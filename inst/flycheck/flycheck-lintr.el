465d085e0ee3838ef16717bf6857a18f8c7f092d
Jim Hester <james.f.hester@gmail.com>
2014-10-21 16:43:06 -0400

    Add non-quite working flycheck code

---
+++ inst/flycheck/flycheck-lintr.el
@@ -0,0 +1,33 @@
+(require 'dash)
+(require 'flycheck)
+(require 'ess)
+
+(flycheck-define-checker r-lintr
+  "A R syntax and type checker using lintr.
+
+See URL `https://github.com/jimhester/lintr'."
+
+ :command
+ ("R" "--slave" "--restore" "--no-save"
+  "-e 'library(lintr);lint_file(commandArgs(TRUE))'" "--args" source)
+
+ :error-patterns
+ ((info line-start (file-name) ":" line ":" column ": " "style:"
+           (message
+             (one-or-more " ") (one-or-more not-newline)
+             line-end)))
+ ((warning line-start (file-name) ":" line ":" column ": " "warning:"
+           (message
+             (one-or-more " ") (one-or-more not-newline)
+             line-end)))
+ ((error line-start (file-name) ":" line ":" column ": " "error:"
+         (message
+           (one-or-more " ") (one-or-more not-newline)
+           line-end)))
+
+ :modes ess-mode
+)
+
+(add-to-list 'flycheck-checkers 'r-lintr)
+
+(provide 'flycheck-lintr)
