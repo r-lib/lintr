07dd23147f0bab8120800c5d4af9ac4cab1ab3e5
Jim Hester <james.f.hester@gmail.com>
2014-10-23 11:48:11 -0400

    Remove unused code from other packages

--- R/style_diff.R
+++
@@ -1,7 +0,0 @@
-style_diff <- function(file, number, message, line) {
-  structure(list(file=file, number=number, message=message, line=line), class="style_diff")
-}
-
-print.style_diff <- function(x, ...) {
-  cat(sep="", x$file, ":", x$number, " ", x$message, ": ", x$line, "\n")
-}
