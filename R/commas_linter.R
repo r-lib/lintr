8af28497c5b274e3e9e00c11292015e90d316b66
Jim Hester <james.f.hester@gmail.com>
2014-11-05 15:53:34 -0500

    Improve speed of commas_linter

--- R/commas_linter.R
+++ R/commas_linter.R
@@ -2,52 +2,75 @@
 #' have spaces before them.
 #' @export
 commas_linter <- function(source_file) {
-  lapply(ids_with_token(source_file, "','"),
 
-    function(id) {
-      parsed <- source_file$parsed_content[id, ]
-      line <- getSrcLines(source_file, parsed$line1, parsed$line1)
-      before_comma <- substr(line, 1L, parsed$col1 - 1L)
-      after_comma <- substr(line, parsed$col1 + 1L, parsed$col1 + 1L)
+  re <- rex(list(one_or_more(" "), ",") %or% list(",", non_space))
 
-      res <- list()
+  res <- re_matches(source_file$content, re, global = TRUE, locations = TRUE)[[1]]
 
-      before_match <-
-        re_matches(before_comma,
-          rex(non_space,
-            spaces,
-            end),
-          locations = TRUE)
+  if (!is.na(res$start[1])) {
+    lapply(seq_len(NROW(res)),
+      function(itr) {
 
-      if (!is.na(before_match$start)) {
+      lints <- list()
 
-        res[[length(res) + 1L]] <-
-          Lint(
-            filename = source_file$filename,
-            line_number = parsed$line1,
-            column_number = before_match$start + 1L,
-            type = "style",
-            message = "Commas should never have a space before.",
-            line = getSrcLines(source_file, parsed$line1, parsed$line1),
-            ranges = list(c(before_match$start + 1L, before_match$end))
-            )
+      line_number <- source_file$find_line(res$start[itr])
+
+      start <- source_file$find_column(res$start[itr])
+      end <- source_file$find_column(res$end[itr])
+      line <- getSrcLines(source_file, line_number, line_number)
+
+      comma_loc <- start + re_matches(substr(line, start, end), rex(","), locations = TRUE)$start - 1L
+
+      space_before <- substr(line, comma_loc - 1L, comma_loc - 1L) %==% " "
+
+      if (space_before) {
+
+        has_token <- any(source_file$parsed_content$line1 == line_number &
+          source_file$parsed_content$col1 == comma_loc &
+          source_file$parsed_content$token == "','")
+
+        start_of_line <- re_matches(line, rex(start, spaces, ","))
+
+        if (has_token && !start_of_line) {
+
+          lints[[length(lints) + 1L]] <-
+            Lint(
+              filename = source_file$filename,
+              line_number = line_number,
+              column_number = comma_loc,
+              type = "style",
+              message = "Commas should never have a space before.",
+              line = line,
+              ranges = list(c(start, end))
+              )
+        }
+      }
+
+      # we still need to check if there is a non-space after
+      non_space_after <- re_matches(substr(line, comma_loc + 1L, comma_loc + 1L), rex(non_space))
+
+      if (non_space_after) {
+
+        has_token <- any(source_file$parsed_content$line1 == line_number &
+          source_file$parsed_content$col1 == comma_loc &
+          source_file$parsed_content$token == "','")
+
+        if (has_token) {
+
+          lints[[length(lints) + 1L]] <-
+            Lint(
+              filename = source_file$filename,
+              line_number = line_number,
+              column_number = comma_loc,
+              type = "style",
+              message = "Commas should always have a space after.",
+              line = line
+              )
+        }
 
       }
 
-      if (re_matches(after_comma, rex(non_spaces))) {
-
-        res[[length(res) + 1L]] <-
-          Lint(
-            filename = source_file$filename,
-            line_number = parsed$line1,
-            column_number = parsed$col1 + 1L,
-            type = "style",
-            message = "Commas should always have a space after.",
-            line = getSrcLines(source_file, parsed$line1, parsed$line1)
-            )
-
-      }
-
-      res
+      lints
     })
+  }
 }
--- tests/testthat/test-r-linters.R
+++ tests/testthat/test-r-linters.R
@@ -117,37 +117,6 @@ test_that("returns the correct linting", {
 
 })
 
-context("r-linter-commas")
-test_that("returns the correct linting", {
-
-  expect_lint("blah", NULL, commas_linter)
-
-  expect_lint("fun(1, 1)", NULL, commas_linter)
-
-  expect_lint("fun(1,\n  1)", NULL, commas_linter)
-
-  expect_lint("fun(1,\n1)", NULL, commas_linter)
-
-  expect_lint("fun(1\n,\n1)", NULL, commas_linter)
-
-  expect_lint("fun(1\n  ,\n1)", NULL, commas_linter)
-
-  expect_lint("fun(1\n,1)",
-    rex("Commas should always have a space after."),
-    commas_linter)
-
-  expect_lint("fun(1,1)",
-    rex("Commas should always have a space after."),
-    commas_linter)
-
-  expect_lint("fun(1 ,1)",
-    list(
-      rex("Commas should never have a space before."),
-      rex("Commas should always have a space after.")
-      ),
-    commas_linter)
-})
-
 context("r-linter-infix_spaces")
 test_that("returns the correct linting", {
   ops <- c(
