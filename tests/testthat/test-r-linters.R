c66d876bb73188f500b545c412af3824b5a9aec6
Jim Hester <james.f.hester@gmail.com>
2014-11-06 12:18:05 -0500

    Move space left parentheses linter to it's own file

--- tests/testthat/test-r-linters.R
+++ tests/testthat/test-r-linters.R
@@ -177,54 +177,6 @@ test_that("returns the correct linting", {
   expect_lint("a[1 + -1]", NULL, infix_spaces_linter)
 })
 
-context("r-linter-spaces-left_parentheses")
-test_that("returns the correct linting", {
-
-  expect_lint("blah", NULL, spaces_left_parentheses_linter)
-
-  expect_lint("print(blah)", NULL, spaces_left_parentheses_linter)
-
-  expect_lint("base::print(blah)", NULL, spaces_left_parentheses_linter)
-
-  expect_lint("base::print(blah, fun(1))",
-    NULL,
-    spaces_left_parentheses_linter)
-
-  expect_lint("blah <- function(blah) { }",
-    NULL,
-    spaces_left_parentheses_linter)
-
-  expect_lint("(1 + 1)", NULL, spaces_left_parentheses_linter)
-
-  expect_lint("( (1 + 1) )", NULL, spaces_left_parentheses_linter)
-
-  expect_lint("if (blah) { }", NULL, spaces_left_parentheses_linter)
-
-  expect_lint("for (i in j) { }", NULL, spaces_left_parentheses_linter)
-
-  expect_lint("1 * (1 + 1)", NULL, spaces_left_parentheses_linter)
-
-  expect_lint("((1 + 1))",
-    rex("Place a space before left parenthesis, except in a function call."),
-    spaces_left_parentheses_linter)
-
-  expect_lint("if(blah) { }",
-    rex("Place a space before left parenthesis, except in a function call."),
-    spaces_left_parentheses_linter)
-
-  expect_lint("for(i in j) { }",
-    rex("Place a space before left parenthesis, except in a function call."),
-    spaces_left_parentheses_linter)
-
-  expect_lint("1*(1 + 1)",
-    rex("Place a space before left parenthesis, except in a function call."),
-    spaces_left_parentheses_linter)
-
-  expect_lint("test <- function(x) { if(1 + 1) 'hi' }",
-    rex("Place a space before left parenthesis, except in a function call."),
-    spaces_left_parentheses_linter)
-})
-
 context("r-linter-open_curly")
 test_that("returns the correct linting", {
 
---
+++ tests/testthat/test-spaces_left_parentheses_linter.R
@@ -0,0 +1,53 @@
+context("spaces_left_parentheses_linter")
+test_that("returns the correct linting", {
+
+  expect_lint("blah", NULL, spaces_left_parentheses_linter)
+
+  expect_lint("print(blah)", NULL, spaces_left_parentheses_linter)
+
+  expect_lint("base::print(blah)", NULL, spaces_left_parentheses_linter)
+
+  expect_lint("base::print(blah, fun(1))",
+    NULL,
+    spaces_left_parentheses_linter)
+
+  expect_lint("blah <- function(blah) { }",
+    NULL,
+    spaces_left_parentheses_linter)
+
+  expect_lint("(1 + 1)", NULL, spaces_left_parentheses_linter)
+
+  expect_lint("( (1 + 1) )", NULL, spaces_left_parentheses_linter)
+
+  expect_lint("if (blah) { }", NULL, spaces_left_parentheses_linter)
+
+  expect_lint("for (i in j) { }", NULL, spaces_left_parentheses_linter)
+
+  expect_lint("1 * (1 + 1)", NULL, spaces_left_parentheses_linter)
+
+  expect_lint("((1 + 1))",
+    rex("Place a space before left parenthesis, except in a function call."),
+    spaces_left_parentheses_linter)
+
+  expect_lint("if(blah) { }",
+    rex("Place a space before left parenthesis, except in a function call."),
+    spaces_left_parentheses_linter)
+
+  expect_lint("for(i in j) { }",
+    rex("Place a space before left parenthesis, except in a function call."),
+    spaces_left_parentheses_linter)
+
+  expect_lint("1*(1 + 1)",
+    rex("Place a space before left parenthesis, except in a function call."),
+    spaces_left_parentheses_linter)
+
+  expect_lint("test <- function(x) { if(1 + 1) 'hi' }",
+    rex("Place a space before left parenthesis, except in a function call."),
+    spaces_left_parentheses_linter)
+
+  expect_lint("\"test <- function(x) { if(1 + 1) 'hi' }\"",
+    NULL,
+    spaces_left_parentheses_linter)
+})
+
+
