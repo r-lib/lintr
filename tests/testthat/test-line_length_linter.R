24168657282a9f3c87fea58de0f083b1d66edf3a
Jim Hester <james.f.hester@gmail.com>
2014-11-10 11:24:35 -0500

    Split line_length_linter tests to own file

---
+++ tests/testthat/test-line_length_linter.R
@@ -0,0 +1,44 @@
+context("line_length_linter")
+test_that("returns the correct linting", {
+
+  expect_lint("blah",
+    NULL,
+    line_length_linter(80))
+
+  expect_lint("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    NULL,
+    line_length_linter(80))
+
+  expect_lint("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "lines should not be more than 80 characters",
+    line_length_linter(80))
+
+  expect_lint(
+    paste0("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n",
+    "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"),
+
+    list(
+      rex("lines should not be more than 80 characters"),
+      rex("lines should not be more than 80 characters")),
+
+    line_length_linter(80))
+
+  expect_lint("aaaaaaaaaaaaaaaaaaaa",
+    NULL,
+    line_length_linter(20))
+
+  expect_lint("aaaaaaaaaaaaaaaaaaaab",
+    rex("lines should not be more than 20 characters"),
+    line_length_linter(20))
+})
+test_that("works with unicode characters", {
+
+  expect_lint("'☂☂☂☂☂☂☂☂☂☂☂☂☂☂☂☂☂'",
+    NULL,
+    line_length_linter(20))
+
+  expect_lint("'☂☂☂☂☂☂☂☂☂☂☂☂☂☂☂☂☂☂☂'",
+    rex("lines should not be more than 20 characters"),
+    line_length_linter(20))
+
+})
