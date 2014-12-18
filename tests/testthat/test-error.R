36984e5255dff9064f1705d893e6e365d881f8af
Jim Hester <james.f.hester@gmail.com>
2014-12-18 16:31:34 -0500

    Add a test to cover get_source_expressions.R#L55-L61
    https://github.com/jimhester/lintr/blob/01849a56cc96e55fb495ef3e259eeed6ea6ffaab/R/get_source_expressions.R#L55-L61

--- tests/testthat/test-error.R
+++ tests/testthat/test-error.R
@@ -11,5 +11,8 @@ test_that("returns the correct linting", {
   expect_lint("\"\\z\"",
     rex("is an unrecognized escape in character string starting")
     )
+  expect_lint("a <- 1
+    function() {
+    b",
+    rex("unexpected end of input"), (function(...) NULL))
 })
-
