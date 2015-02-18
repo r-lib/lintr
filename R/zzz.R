f3651e27629b1070816c4d7c4f6a9fa25259396d
Jim Hester <james.f.hester@gmail.com>
2015-02-18 15:10:37 -0500

    One last try

--- R/zzz.R
+++ R/zzz.R
@@ -90,7 +90,11 @@ default_settings <- list(
   exclude_end = rex::rex("#", any_spaces, "nolint end"),
   exclusions = list(),
   cache_directory = "~/.R/lintr_cache", # nolint
-  comment_token = "565a3dc866ba3997b3c39d070c5cb269034e1b46"
+  comment_token = paste0("7713914a",
+                         "3598997a",
+                         "0887b075",
+                         "dcc71584",
+                         "9bf1a3ce")
 )
 
 settings <- list2env(default_settings, parent = emptyenv())
