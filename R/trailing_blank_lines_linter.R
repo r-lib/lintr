4630752bc35f017137ab62f400c4bbc7b7002b8c
Jim Hester <james.f.hester@gmail.com>
2015-04-06 14:48:14 -0400

    Fix the trailing_blank_lines_linter to handle NA lines

--- R/trailing_blank_lines_linter.R
+++ R/trailing_blank_lines_linter.R
@@ -2,21 +2,22 @@
 #' @export
 trailing_blank_lines_linter <- function(source_file) {
   blanks <- re_matches(source_file$file_lines,
-    rex(start, any_spaces, end),
-    global = TRUE)
+    rex(start, any_spaces, end))
 
   line_number <- length(source_file$file_lines)
   lints <- list()
-  while(line_number > 0L && blanks[[line_number]]) {
-    lints[[length(lints) + 1L]] <-
-      Lint(
-        filename = source_file$filename,
-        line_number = line_number,
-        column_number = 1,
-        type = "style",
-        message = "Trailing blank lines are superfluous.",
-        line = source_file$file_lines[[line_number]]
-        )
+  while(line_number > 0L && (is.na(blanks[[line_number]]) || isTRUE(blanks[[line_number]]))) {
+    if (!is.na(blanks[[line_number]])) {
+      lints[[length(lints) + 1L]] <-
+        Lint(
+          filename = source_file$filename,
+          line_number = line_number,
+          column_number = 1,
+          type = "style",
+          message = "Trailing blank lines are superfluous.",
+          line = source_file$file_lines[[line_number]]
+          )
+    }
     line_number <- line_number - 1L
   }
   lints
