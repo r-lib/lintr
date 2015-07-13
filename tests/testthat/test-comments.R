3d9e70b15559c460bc60e8961dd94cdaca8dc094
Paul Staab <develop@paulstaab.de>
2015-07-13 22:40:28 +0200

    fix CI environment detection

---
+++ tests/testthat/test-comments.R
@@ -0,0 +1,11 @@
+context("comments")
+test_that("it detects CI environments", {
+  org_value <- Sys.getenv("CI")
+  Sys.setenv(CI="true")
+  expect_true(in_ci())
+  Sys.setenv(CI="false")
+  expect_false(in_ci())
+  Sys.setenv(CI="")
+  expect_false(in_ci())
+  Sys.setenv(CI=org_value)
+})
