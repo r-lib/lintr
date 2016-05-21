060022af537c36e3114499eb420e7b20ca18ab6a
Gábor Csárdi <csardi.gabor@gmail.com>
2016-05-21 15:38:26 +0100

    Linter for 1:length(...) and similar expressions

---
+++ tests/testthat/test-seq_linter.R
@@ -0,0 +1,29 @@
+context("seq_linter")
+
+test_that("other : expressions are fine", {
+  expect_lint("function() { 1:10 }", NULL, seq_linter)
+  expect_lint("function(x) { 2:length(x) }", NULL, seq_linter)
+  expect_lint("function(x) { 1:(length(x) || 1) }", NULL, seq_linter)
+})
+
+test_that("finds 1:length(...) expressions", {
+  expect_lint("function(x) { 1:length(x) }",
+    rex("length(...) expressions, use seq_len"),
+    seq_linter)
+
+  expect_lint("function(x) { 1:nrow(x) }",
+    rex("nrow(...) expressions, use seq_len"),
+    seq_linter)
+
+  expect_lint("function(x) { 1:ncol(x) }",
+    rex("ncol(...) expressions, use seq_len"),
+    seq_linter)
+
+  expect_lint("function(x) { 1:NROW(x) }",
+    rex("NROW(...) expressions, use seq_len"),
+    seq_linter)
+
+  expect_lint("function(x) { 1:NCOL(x) }",
+    rex("NCOL(...) expressions, use seq_len"),
+    seq_linter)
+})
