9de2572a59721c513cb794ee3ee58bf99601a238
rmsharp <rmsharp@me.com>
2016-09-18 16:28:00 -0500

    created test script for commas_before_empty_bracket_linter()
    Indicates “a[, 2]” and “a[ , 2] should not be flagged. I am of two
    minds on this. The function commas_before_empty_bracket_linter() was
    created to allow the second, but I am not forcing adherence to that
    style at this point. I am interested in comments from others.

---
+++ tests/testthat/test-commas_before_empty_bracket_linter.R
@@ -0,0 +1,41 @@
+context("commas_before_empty_bracket_linter")
+test_that("returns the correct linting", {
+
+  expect_lint("blah", NULL, commas_before_empty_bracket_linter)
+
+  expect_lint("fun(1, 1)", NULL, commas_before_empty_bracket_linter)
+
+  expect_lint("fun(1,\n  1)", NULL, commas_before_empty_bracket_linter)
+
+  expect_lint("fun(1,\n1)", NULL, commas_before_empty_bracket_linter)
+
+  expect_lint("fun(1\n,\n1)", NULL, commas_before_empty_bracket_linter)
+
+  expect_lint("fun(1\n  ,\n1)", NULL, commas_before_empty_bracket_linter)
+
+  expect_lint("fun(1 ,1)",
+    list(
+      rex("Commas should never have a space before.")
+      ),
+    commas_before_empty_bracket_linter)
+
+  expect_lint("\"fun(1 ,1)\"",
+    NULL,
+    commas_before_empty_bracket_linter)
+
+  expect_lint("a[1, , 2]",
+    NULL,
+    commas_before_empty_bracket_linter)
+
+  expect_lint("a[, 2]",
+              NULL,
+              commas_before_empty_bracket_linter)
+  
+  expect_lint("a[ , 2]",
+                NULL,
+              commas_before_empty_bracket_linter)
+  
+  expect_lint("a[1, , 2, , 3]",
+    NULL,
+    commas_before_empty_bracket_linter)
+})
