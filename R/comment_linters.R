d2924ddc676388e1b9fd1a75a071f133eace311a
Florent Angly <florent.angly@gmail.com>
2017-03-07 11:10:00 +0100

    Relax the commented_code_linter()
    * Do not lint comments within roxygen blocks (this follows the linter's description, strictly speaking). This allows to write comments that show the expected result in @example roxygen blocks, e.g.:
       #' @examples
       #' foo(1)  ## list(1)
    
    * Do not consider "-" an R operator to avoid too many false positives, e.g. in "# Non-existent"

--- R/comment_linters.R
+++ R/comment_linters.R
@@ -1,6 +1,6 @@
 ops <- list(
   "+",
-  "-",
+  #"-",
   "=",
   "==",
   "!=",
@@ -23,14 +23,15 @@ ops <- list(
   "&&",
   rex("%", except_any_of("%"), "%"))
 
-#' @describeIn linters checks that there is no commented code outside roxygen
+#' @describeIn linters Check that there is no commented code outside roxygen
 #' blocks
 #' @export
 commented_code_linter <- function(source_file) {
   res <- re_matches(source_file$file_lines,
                     rex("#", any_spaces,
                         capture(name = "code",
-                          except("'"), anything,
+                          # except("'"),
+                          anything,
                           or(some_of("{}[]"), # code-like parentheses
                             or(ops), # any operator
                             group(graphs, "(", anything, ")"), # a function call
