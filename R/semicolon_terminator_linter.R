f92e855e9b4d9ef4bb44a275047f7d8b97b4c59b
Florent Angly <florent.angly@gmail.com>
2017-03-13 11:00:32 +0100

    Simplified semicolon_terminator_linter() more

--- R/semicolon_terminator_linter.R
+++ R/semicolon_terminator_linter.R
@@ -6,39 +6,36 @@
 #' @export
 semicolon_terminator_linter <- function(type = c("compound", "trailing")) {
   function(source_file) {
-    sc_ids <- ids_with_token(source_file, "';'")
-    if (length(sc_ids)) {
-      sc_tokens <- with_id(source_file, sc_ids)
-      is_trailing <- is_trailing_sc(sc_tokens, source_file)
+    tokens <- with_id(source_file, ids_with_token(source_file, "';'"))
+    is_trailing <- is_trailing_sc(tokens, source_file)
 
-      to_keep <- ( is_trailing & "trailing" %in% type) |
-                 (!is_trailing & "compound" %in% type)
- 
-      sc_tokens <- sc_tokens[to_keep, ]
-      is_trailing <- is_trailing[to_keep]
+    to_keep <- ( is_trailing & "trailing" %in% type) |
+               (!is_trailing & "compound" %in% type)
 
-      lapply(
-        seq_len(nrow(sc_tokens)),
-        function(sc_num) {
-          sc_token <- sc_tokens[sc_num, ]
-          msg <- if (is_trailing[[sc_num]]) {
-            "Trailing semicolons are not needed."
-          } else  {
-            "Compound semicolons are not needed. Replace them by a newline."
-          }
-          Lint(
-            filename = source_file[["filename"]],
-            line_number = sc_token[["line1"]],
-            column_number = sc_token[["col1"]],
-            type = "style",
-            message = msg,
-            line = source_file[["lines"]][[as.character(sc_token[["line1"]])]],
-            ranges = list(c(sc_token[["col1"]], sc_token[["col2"]])),
-            linter = "semicolon_linter"
-          )
+    tokens <- tokens[to_keep, ]
+    are_trailing <- is_trailing[to_keep]
+
+    Map(
+      function(token, is_trailing) {
+        msg <- if (is_trailing) {
+          "Trailing semicolons are not needed."
+        } else  {
+          "Compound semicolons are not needed. Replace them by a newline."
         }
-      )
-    }
+        Lint(
+          filename = source_file[["filename"]],
+          line_number = token[["line1"]],
+          column_number = token[["col1"]],
+          type = "style",
+          message = msg,
+          line = source_file[["lines"]][[as.character(token[["line1"]])]],
+          ranges = list(c(token[["col1"]], token[["col2"]])),
+          linter = "semicolon_linter"
+        )
+      },
+      split(tokens, seq_len(nrow(tokens))),
+      are_trailing
+    )
   }
 }
 
