2b459a762c116084372bcacb5540eec4c01ff9fb
Florent Angly <florent.angly@gmail.com>
2017-03-13 11:50:26 +0100

    Fixes for no_tab_linter
    * improved lint message: does not mention the number of spaces to use anymore
    * took some checks out of loop to vectorize them
    * fixed regex: was not working anymore on comments after changes made earlier in this branch

--- R/no_tab_linter.R
+++ R/no_tab_linter.R
@@ -1,33 +1,41 @@
 #' @describeIn linters check that only spaces are used, never tabs.
 #' @export
 no_tab_linter <- function(source_file) {
-  res <- re_matches(source_file$lines,
-    rex("\t" %>% one_or_more()), locations = TRUE, global = TRUE)
+  all_res <- re_matches(
+    source_file$lines,
+    rex(start, zero_or_more(regex("\\s")), one_or_more("\t")),
+    locations = TRUE,
+    global = TRUE
+  )
+  names(all_res) <- as.character(seq_along(all_res))
 
-  lapply(seq_along(source_file$lines), function(line_number) {
-    mapply(
-      FUN = function(start, end) {
-        if (is.na(start)) {
-          return()
-        }
+  # filter out lines with no matches
+  all_res <- all_res[
+    vapply(all_res, function(line_match) {!is.na(line_match[["start"]][[1L]])}, logical(1L))
+  ]
 
-        Lint(
-          filename = source_file$filename,
-          line_number = line_number,
-          column_number = start,
-          type = "style",
-          message = "Use two spaces to indent, never tabs.",
-          line = source_file$lines[line_number],
+  Map(
+    function(line_res, line_num) {
+      lapply(
+        split(line_res, seq_len(nrow(line_res))),
+        function(res) {
+          Lint(
+            filename = source_file$filename,
+            line_number = line_num,
+            column_number = res[["start"]],
+            type = "style",
+            message = "Use spaces to indent, not tabs.",
+            line = source_file$lines[line_num],
 
-          # R outputs tabs with 8 spaces
-          # TODO: this is incorrect for embedded tabs, I am not going to fix it.
-          ranges = list(c(start, end)),
-          linter = "no_tab_linter"
+            # R outputs tabs with 8 spaces
+            # TODO: this is incorrect for embedded tabs, I am not going to fix it.
+            ranges = list(c(res[["start"]], res[["end"]])),
+            linter = "no_tab_linter"
           )
-      },
-      res[[line_number]]$start,
-      res[[line_number]]$end,
-      SIMPLIFY = FALSE
+        }
       )
-    })
+    },
+    all_res,
+    names(all_res)
+  )
 }
