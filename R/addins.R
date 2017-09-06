5a5ddd7ccbb5b7bab76b6f822cbcf58495bc2a75
JhossePaul <mcromanceva@hotmail.com>
2017-09-06 01:22:34 -0500

    Defensive checks for addins

--- R/addins.R
+++ R/addins.R
@@ -1,8 +1,16 @@
-addin_lint <- function () {
+addin_lint <- function() {
   filename <- rstudioapi::getActiveDocumentContext()
+  if (filename$path == "") {
+    return("Current source has no path. Please save before continue")
+  }
   lintr::lint(filename$path)
 }
 
-addin_lint_package <- function () {
-  lintr::lint_package()
+addin_lint_package <- function() {
+  project <- rstudioapi::getAciveProject()
+  project_path <- if (is.null(project)) getwd() else project
+
+  if (is.null(project)) message("No project found, passing current directory")
+
+  lintr::lint_package(project_path)
 }
