23f84cff8bf9a2e4d63ef37ade22fe78e58e259e
Fabian Scheipl <fabian.scheipl@googlemail.com>
2019-08-19 19:11:54 +0200

    [WIP] linter works

---
+++ R/cyclocomp_linter.R
@@ -0,0 +1,34 @@
+#' @describeIn linters  Report the presence of expressions with overly high complexity.
+#' @param complexity_limit expressions with a cyclomatic complexity higher than
+#'   this are linted, defaults to 15. See \code{\link[cyclocomp]{cyclocomp}}.
+#' @importFrom cyclocomp cyclocomp
+#' @export
+cyclocomp_linter <- function(complexity_limit = 15) {
+  function(source_file) {
+    if (!is.null(source_file[["file_lines"]])) {
+      # abort if source_file is entire file, not just a top level expression.
+      return(NULL)
+    }
+    complexity <- try_silently(
+      cyclocomp::cyclocomp(parse(text = source_file$content))
+      )
+    if (inherits(complexity, "try-error")) return(NULL)
+    if (complexity <= complexity_limit) return(NULL)
+    Lint(
+      filename = source_file[["filename"]],
+      line_number = source_file[["line"]][1],
+      column_number = source_file[["column"]][1],
+      type = "style",
+      message = paste0(
+        "Write short and simple functions and avoid complex conditionals,",
+        "e.g. by encapsulating distinct steps into subfunctions.",
+        "The expression starting on this line has a cyclomatic complexity of ",
+        complexity, ", should be at most ", complexity_limit, "."
+      ),
+      ranges = list(c(NA, NA)),
+      line = source_file$lines[1],
+      linter = "cyclocomp_linter"
+    )
+  }
+}
+# expect_lint(readLines("inst/example/complexity.R"), NULL, cyclocomp_linter(10))
