63d1cce8fa771573be7a41d4c5d7ed6804cc1448
Fabian Scheipl <fabian.scheipl@googlemail.com>
2019-08-23 20:04:22 +0200

    361 cyclocomp linter (#396)
    * [WIP] linter works
    
    * adds linting for cyclomatic complexity.
    
    incurs dependency on cyclocomp.
    closes #361.
    
    * updates NEWS for cyclocomp_linter
    
    * less verbose message for cyclocomp_linter
    
    * update tests to new message

--- NEWS.md
+++ NEWS.md
@@ -1,4 +1,5 @@
 # lintr 1.0.3.9000 # 
+* New cyclocomp_linter() identifies overly complex functions (#361, @fabian-s)
 * Add support for overriding GitHub API Token via `GITHUB_TOKEN` environment
   variable (#63, @mattyb)
 * Changed the default value of the `length` argument to `object_length_linter` to 30 for consistency (#325 @DragosMG) 
---
+++ R/cyclocomp_linter.R
@@ -0,0 +1,32 @@
+#' @describeIn linters Check for overly complicated expressions. See
+#'   \code{\link[cyclocomp]{cyclocomp}}.
+#' @param complexity_limit expressions with a cyclomatic complexity higher than
+#'   this are linted, defaults to 25. See \code{\link[cyclocomp]{cyclocomp}}.
+#' @importFrom cyclocomp cyclocomp
+#' @export
+cyclocomp_linter <- function(complexity_limit = 25) {
+  function(source_file) {
+    if (!is.null(source_file[["file_lines"]])) {
+      # abort if source_file is entire file, not a top level expression.
+      return(NULL)
+    }
+    complexity <- try_silently(
+      cyclocomp::cyclocomp(parse(text = source_file$content))
+    )
+    if (inherits(complexity, "try-error")) return(NULL)
+    if (complexity <= complexity_limit) return(NULL)
+    Lint(
+      filename = source_file[["filename"]],
+      line_number = source_file[["line"]][1],
+      column_number = source_file[["column"]][1],
+      type = "style",
+      message = paste0(
+        "functions should have cyclomatic complexity of less than ",
+        complexity_limit, ", this has ", complexity,"."
+      ),
+      ranges = list(c(NA, NA)),
+      line = source_file$lines[1],
+      linter = "cyclocomp_linter"
+    )
+  }
+}
