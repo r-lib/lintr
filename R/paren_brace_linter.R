bad7edcd989cef6c18369b5ddc0e83c020b030eb
Russell Hyde <russ.hyde.data@gmail.com>
2020-03-12 14:47:21 +0000

    regex-based `paren_brace_linter`

--- R/paren_brace_linter.R
+++ R/paren_brace_linter.R
@@ -1,39 +1,12 @@
 #' @describeIn linters check that there is a space between right
 #' parenthesis and an opening curly brace.
+#' @include   make_linter_from_regex.R
+#'
 #' @export
-paren_brace_linter <- function(source_file) {
-  all_matches <- re_matches(
-    source_file[["lines"]],
-    rex("){"),
-    locations = TRUE,
-    global = TRUE
-  )
-  line_numbers <- as.integer(names(source_file[["lines"]]))
 
-  Map(
-    function(line_matches, line_number) {
-      lapply(
-        split(line_matches, seq_len(nrow(line_matches))),
-        function(.match) {
-          start <- .match[["start"]]
-          if (is.na(start)) {
-            return()
-          }
-          end <- .match[["end"]]
-          Lint(
-            filename = source_file[["filename"]],
-            line_number = line_number,
-            column_number = start,
-            type = "style",
-            message = "There should be a space between right parenthesis and an opening curly brace.",
-            line = source_file[["lines"]][[as.character(line_number)]],
-            ranges = list(c(start, end)),
-            linter = "paren_brace_linter"
-          )
-        }
-      )
-    },
-    all_matches,
-    line_numbers
-  )
-}
+paren_brace_linter <- make_linter_from_regex(
+  regex = rex("){"),
+  lint_name = "paren_brace_linter",
+  lint_type = "style",
+  lint_msg = "There should be a space between right parenthesis and an opening curly brace."
+)
