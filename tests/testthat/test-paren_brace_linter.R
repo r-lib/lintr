b04bce6b93b07f8279c378f8176d085e81394de2
Russell Hyde <russ.hyde.data@gmail.com>
2020-03-12 14:46:14 +0000

    add tests: code-bound linters should ignore strings / comments
    Tests added to ensure paren-brace linter, equals-NA linter and no-tab
    linter do not flag lints in irrelevant sections of code (eg, strings /
    comments).

--- tests/testthat/test-paren_brace_linter.R
+++ tests/testthat/test-paren_brace_linter.R
@@ -18,4 +18,26 @@ test_that("returns the correct linting", {
     msg,
     paren_brace_linter
   )
+
+  # paren_brace_linter should ignore strings and comments, as in regexes:
+  expect_lint(
+    "grepl('(iss){2}', 'Mississippi')",
+    NULL,
+    paren_brace_linter
+  )
+  expect_lint(
+    "x <- 123 # dont flag (paren){brace} if inside a comment",
+    NULL,
+    paren_brace_linter
+  )
+  # paren-brace lint should not be thrown when the brace lies on subsequent line
+  expect_lint(
+    paste(
+      "x <- function()",
+      "               {2}",
+      sep = "\n"
+    ),
+    NULL,
+    paren_brace_linter
+  )
 })
