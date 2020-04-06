ed397a4b222dbed1a3474fc924061b5ec1742625
Russ Hyde <russ.hyde.data@gmail.com>
2020-04-06 10:09:47 +0100

    regex-based linters can filter out strings but not comments

--- R/make_linter_from_regex.R
+++ R/make_linter_from_regex.R
@@ -2,15 +2,12 @@ make_linter_from_regex <- function(regex,
                                    lint_name,
                                    lint_type,
                                    lint_msg,
-                                   ignore_comments = TRUE,
                                    ignore_strings = TRUE) {
 
   # If a regex-based linter is found, only flag those lints that occur within
   # a relevant section of source code
   .in_ignorable_position <- function(source_file, line_number, match) {
-    is.na(match[["start"]]) ||
-      (ignore_comments && in_comment(source_file, line_number, match)) ||
-      (ignore_strings && in_string(source_file, line_number, match))
+    ignore_strings && in_string(source_file, line_number, match)
   }
 
   linter <- function(source_file) {
@@ -28,7 +25,8 @@ make_linter_from_regex <- function(regex,
         lapply(
           split(line_matches, seq_len(nrow(line_matches))),
           function(.match) {
-            if (.in_ignorable_position(source_file, line_number, .match)) {
+            if (is.na(.match[["start"]]) ||
+                .in_ignorable_position(source_file, line_number, .match)) {
               return()
             }
             start <- .match[["start"]]
@@ -89,33 +87,6 @@ is_match_covered <- function(source_file, line_number, match, token_type = NULL)
   }
 
   any_single_line_covers() || any_multi_line_covers()
-}
-
-in_comment <- function(source_file, line_number, match) {
-  # For code of the form "abc <- 123 # some comment", there are two runs
-  # through a given linter: first with a source_file generated from
-  # "abc <- 123", and secondly with a source_file from the comment
-
-  # If a regex matches inside a post-code comment, then the "match" will not
-  # overlap the code that precedes the comment: for now we return TRUE for
-  # these instances
-
-  # TODO: filter out code blocks that do not cover the "match" before running
-  # this function then use is_match_covered(..., "COMMENT")
-
-  # TODO: check this handles "f <- function(x, #\tblah\n    y) {}"
-
-  pc <- source_file[["parsed_content"]]
-
-  non_comment_rows <- which(pc[["token"]] != "COMMENT")
-  pc_no_comments <- pc[non_comment_rows, ]
-
-  if (nrow(pc_no_comments) == 0 ||
-    match[["start"]] > max(pc_no_comments[["col2"]])) {
-    TRUE
-  } else {
-    FALSE
-  }
 }
 
 in_string <- function(source_file, line_number, match) {
