6d753d7e2ff2183297c36fe05861f5aec3a77a40
Michael Chirico <chiricom@google.com>
2020-10-05 11:29:43 -0400

    re-fix the regular expression

--- tests/testthat/test-seq_linter.R
+++ tests/testthat/test-seq_linter.R
@@ -8,42 +8,42 @@ test_that("other : expressions are fine", {
 
 test_that("finds 1:length(...) expressions", {
   expect_lint("function(x) { 1:length(x) }",
-    rex("length(...) .* use seq_len"),
+    rex("length(...)", anything, "use seq_len"),
     seq_linter)
 
   expect_lint("function(x) { 1:nrow(x) }",
-    rex("nrow(...) .* use seq_len"),
+    rex("length(...)", anything, "use seq_len"),
     seq_linter)
 
   expect_lint("function(x) { 1:ncol(x) }",
-    rex("ncol(...) .* use seq_len"),
+    rex("length(...)", anything, "use seq_len"),
     seq_linter)
 
   expect_lint("function(x) { 1:NROW(x) }",
-    rex("NROW(...) .* use seq_len"),
+    rex("length(...)", anything, "use seq_len"),
     seq_linter)
 
   expect_lint("function(x) { 1:NCOL(x) }",
-    rex("NCOL(...) .* use seq_len"),
+    rex("length(...)", anything, "use seq_len"),
     seq_linter)
 
   expect_lint("function(x) { 1:dim(x)[1L] }",
-    rex("dim(...) .* use seq_len"),
+    rex("length(...)", anything, "use seq_len"),
     seq_linter)
   
   expect_lint("function(x) { 1L:dim(x)[[1]] }",
-    rex("dim(...) .* use seq_len"),
+    rex("length(...)", anything, "use seq_len"),
     seq_linter)
 })
 
 test_that("1L is also bad", {
   expect_lint("function(x) { 1L:length(x) }",
-    rex("1L:length(...) .* use seq_len"),
+    rex("length(...)", anything, "use seq_len"),
     seq_linter)
 })
 
 test_that("reverse seq is ok", {
   expect_lint("function(x) { length(x):1 }",
-    rex("length(...):1 .* use seq_len"),
+    rex("length(...)", anything, "use seq_len"),
     seq_linter)
 })
