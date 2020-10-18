0fd7ed7db572f0154f4738f12304f47556871929
Michael Chirico <michaelchirico4@gmail.com>
2020-10-18 00:46:13 -0400

    an xml approach for single_quotes_linte

--- R/single_quotes_linter.R
+++ R/single_quotes_linter.R
@@ -2,20 +2,32 @@
 #' string constants.
 #' @export
 single_quotes_linter <- function(source_file) {
-  lapply(ids_with_token(source_file, "STR_CONST"),
-    function(id) {
-      parsed <- with_id(source_file, id)
-      if (re_matches(parsed$text, rex(start, single_quote, any_non_double_quotes, single_quote, end))) {
-        Lint(
-          filename = source_file$filename,
-          line_number = parsed$line1,
-          column_number = parsed$col1,
-          type = "style",
-          message = "Only use double-quotes.",
-          line = source_file$lines[as.character(parsed$line1)],
-          ranges = list(sort(c(parsed$col1, parsed$col2))),
-          linter = "single_quotes_linter"
-          )
-      }
-    })
+  if (!length(source_file$parsed_content)) return(list())
+
+  xpath <- paste(
+    "//STR_CONST[starts-with(text(), \"'\")",
+    "and not(contains(text(), \'\"\'))]"
+  )
+  squote_str <- xml2::xml_find_all(source_file$xml_parsed_content, xpath)
+  lapply(squote_str, function(node) {
+    attrs <- xml2::xml_attrs(node)
+    line1 <- attrs["line1"]
+    line1_src <- source_file$lines[line1]
+    col1 <- as.integer(attrs["col1"])
+    col2 <- if (line1 == attrs["line2"]) {
+      as.integer(attrs["col2"])
+    } else {
+      nchar(line1_src)
+    }
+    Lint(
+      filename = source_file$filename,
+      line_number = as.integer(line1),
+      column_number = col1,
+      type = "style",
+      message = "Only use double-quotes.",
+      line = line1_src,
+      ranges = list(c(col1, col2)),
+      linter = "single_quotes_linter"
+    )
+  })
 }
