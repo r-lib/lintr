a3f8d3cc7221c0edeb2e096e7dc9856b76e5170e
Michael Chirico <michaelchirico4@gmail.com>
2020-10-19 23:09:43 -0400

    rewrite using full-source vectorized approach

--- R/single_quotes_linter.R
+++ R/single_quotes_linter.R
@@ -2,32 +2,33 @@
 #' string constants.
 #' @export
 single_quotes_linter <- function(source_file) {
-  if (!length(source_file$parsed_content)) return(list())
+  if (is.null(source_file$full_parsed_content)) {
+    return(list())
+  }
 
-  xpath <- paste(
-    "//STR_CONST[starts-with(text(), \"'\")",
-    "and not(contains(text(), \'\"\'))]"
-  )
-  squote_str <- xml2::xml_find_all(source_file$xml_parsed_content, xpath)
-  lapply(squote_str, function(node) {
-    attrs <- xml2::xml_attrs(node)
-    line1 <- attrs["line1"]
-    line1_src <- source_file$lines[line1]
-    col1 <- as.integer(attrs["col1"])
-    col2 <- if (line1 == attrs["line2"]) {
-      as.integer(attrs["col2"])
-    } else {
-      nchar(line1_src)
+  content <- source_file$full_parsed_content
+  str_idx <- which(content$token == "STR_CONST")
+  squote_matches <- which(re_matches(
+    content[str_idx, 'text'],
+    rex(start, single_quote, any_non_double_quotes, single_quote, end)
+  ))
+
+  lapply(squote_matches,
+    function(id) {
+      with(content[str_idx[id], ], {
+        line <- source_file$file_lines[line1]
+        col2 <- if (line1 == line2) col2 else nchar(line)
+        Lint(
+          filename = source_file$filename,
+          line_number = line1,
+          column_number = col1,
+          type = "style",
+          message = "Only use double-quotes.",
+          line = line,
+          ranges = list(c(col1, col2)),
+          linter = "single_quotes_linter"
+        )
+      })
     }
-    Lint(
-      filename = source_file$filename,
-      line_number = as.integer(line1),
-      column_number = col1,
-      type = "style",
-      message = "Only use double-quotes.",
-      line = line1_src,
-      ranges = list(c(col1, col2)),
-      linter = "single_quotes_linter"
-    )
-  })
+  )
 }
