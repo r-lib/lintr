68010da64ea8c097cec1eba28ffb28d1f4eeed09
AshesITR <alexander.rosenstock@web.de>
2020-11-02 20:02:20 +0100

    moves the linters.Rd stub to aaa.R and correct collate order (#555)

--- R/aaa.R
+++ R/aaa.R
@@ -3,6 +3,15 @@
 #' @include utils.R
 NULL
 
+#' Available linters
+#'
+#' @rdname linters
+#' @name linters
+#' @title linters
+#' @param source_file returned by \code{\link{get_source_expressions}}
+#' @param length the length cutoff to use for the given linter.
+NULL
+
 # need to register rex shortcuts as globals to avoid CRAN check errors
 rex::register_shortcuts("lintr")
 
