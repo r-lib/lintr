30c64209c1d1b7f65af7a2cdac920dfcc3234021
Kun Ren <renkun@outlook.com>
2020-11-05 15:56:42 +0800

    Check NULL xml_parsed_content

--- R/missing_package_linter.R
+++ R/missing_package_linter.R
@@ -3,7 +3,7 @@
 #' @export
 missing_package_linter <- function(source_file) {
 
-  if (!length(source_file$parsed_content)) return(list())
+  if (!length(source_file$parsed_content) || is.null(source_file$xml_parsed_content)) return(list())
 
   xml <- source_file$xml_parsed_content
 
