154706d576cb495c7efeb7916cc4d5c0d882f887
Kun Ren <renkun@outlook.com>
2020-11-14 08:51:41 +0800

    Allow exceptions for functions

--- R/missing_argument_linter.R
+++ R/missing_argument_linter.R
@@ -12,22 +12,31 @@ missing_argument_linter <- function(source_file) {
     self::EQ_SUB[following-sibling::*[1][self::OP-RIGHT-PAREN or self::OP-COMMA]]
   ]"
 
+  exceptions <- c("switch", "alist")
+
   missing_args <- xml2::xml_find_all(xml, xpath)
 
   line1 <- as.integer(xml2::xml_attr(missing_args, "line1"))
   col1 <- as.integer(xml2::xml_attr(missing_args, "col1"))
   col2 <- as.integer(xml2::xml_attr(missing_args, "col2"))
 
-  lapply(seq_along(missing_args), function(i) {
-    Lint(
-      filename = source_file$filename,
-      line_number = line1[[i]],
-      column_number = col1[[i]],
-      type = "warning",
-      message = "Missing argument in function call.",
-      line = source_file$lines[line1[[i]]],
-      ranges = list(c(col1[[i]], col2[[i]])),
-      linter = "missing_argument_linter"
-    )
+  result <- lapply(seq_along(missing_args), function(i) {
+    func <- xml2::xml_find_all(missing_args[[i]],
+      "preceding-sibling::expr/SYMBOL_FUNCTION_CALL")
+    func <- xml2::xml_text(func)
+    if (length(func) == 1 && !(func %in% exceptions)) {
+      Lint(
+        filename = source_file$filename,
+        line_number = line1[[i]],
+        column_number = col1[[i]],
+        type = "warning",
+        message = "Missing argument in function call.",
+        line = source_file$lines[line1[[i]]],
+        ranges = list(c(col1[[i]], col2[[i]])),
+        linter = "missing_argument_linter"
+      )
+    }
   })
+
+  result[vapply(result, is.list, logical(1L))]
 }
