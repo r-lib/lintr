1818eaa26c1237c38920c1eaa9ae2f2819c5f161
Kun Ren <renkun@outlook.com>
2020-11-17 20:48:16 +0800

    Add test cases

---
+++ tests/testthat/test-missing_argument_linter.R
@@ -0,0 +1,77 @@
+context("missing_argument_linter")
+test_that("returns the correct linting", {
+  expect_lint("fun(x, a = 1)",
+    NULL,
+    missing_argument_linter())
+
+  expect_lint("fun(x = 1, a = 1)",
+    NULL,
+    missing_argument_linter())
+
+  expect_lint("dt[, 1]",
+    NULL,
+    missing_argument_linter())
+
+  expect_lint("dt[, 'col']",
+    NULL,
+    missing_argument_linter())
+
+  expect_lint("array[, , 1]",
+    NULL,
+    missing_argument_linter())
+
+  expect_lint("switch(a =, b =, c = 1, 0)",
+    NULL,
+    missing_argument_linter())
+
+  expect_lint("alist(a =, b =, c = 1, 0)",
+    NULL,
+    missing_argument_linter())
+
+  expect_lint("test(a =, b =, c = 1, 0)",
+    NULL,
+    missing_argument_linter(c("test")))
+
+  expect_lint("fun(, a = 1)",
+    list(message = rex("Missing argument in function call.")),
+    missing_argument_linter())
+
+  expect_lint("fun(a = 1,, b = 2)",
+    list(message = rex("Missing argument in function call.")),
+    missing_argument_linter())
+
+  expect_lint("fun(a = 1, b =)",
+    list(message = rex("Missing argument in function call.")),
+    missing_argument_linter())
+
+  expect_lint("fun(a = 1,)",
+    list(message = rex("Missing argument in function call.")),
+    missing_argument_linter())
+
+  expect_lint("fun(a = )",
+    list(message = rex("Missing argument in function call.")),
+    missing_argument_linter())
+
+  expect_lint("list(
+    a = 1,
+    b = 2,
+    )",
+    list(message = rex("Missing argument in function call.")),
+    missing_argument_linter())
+
+  expect_lint("stats::median(1:10, na.rm =)",
+    list(message = rex("Missing argument in function call.")),
+    missing_argument_linter())
+
+  expect_lint("env$get(1:10, default =)",
+    list(message = rex("Missing argument in function call.")),
+    missing_argument_linter())
+
+  expect_lint("switch(a =, b = 1, 0)",
+    list(message = rex("Missing argument in function call.")),
+    missing_argument_linter(c()))
+
+  expect_lint("alist(a =)",
+    list(message = rex("Missing argument in function call.")),
+    missing_argument_linter(c()))
+})
