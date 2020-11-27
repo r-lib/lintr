b5fe4131c8ba3a82214ff8a2112fae2a87fab049
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-11-27 12:18:53 -0800

    Make test more concise

--- tests/testthat/test-undesirable_function_linter.R
+++ tests/testthat/test-undesirable_function_linter.R
@@ -19,14 +19,9 @@ test_that("linter returns correct linting", {
 
 test_that("it's possible to NOT lint symbols", {
   linter <- undesirable_function_linter(
-    fun = c("dir" = NA),
+    fun = c("dir" = NA, "log10" = "use log()"),
     symbol_is_undesirable = FALSE
   )
-  expect_lint("dir <- 1:3", NULL, linter)
-
-  linter <- undesirable_function_linter(
-    fun = c("log10" = "use log()"),
-    symbol_is_undesirable = FALSE
-  )
+  expect_lint("dir <- 'path/to/a/directory'", NULL, linter)
   expect_lint("lapply(x, log10)", NULL, linter)
 })
