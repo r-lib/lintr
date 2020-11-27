5e1af20fc3a5b1f699605e2ba24dbba56beb02d1
Michael Chirico <chiricom@google.com>
2020-11-27 20:16:16 +0000

    add missing function

--- R/utils.R
+++ R/utils.R
@@ -210,3 +210,27 @@ unescape <- function(str, q="`") {
   )
   str
 }
+
+# convert an XML match into a Lint
+xml_nodes_to_lint <- function(xml, source_file, message, linter,
+                              type = c("style", "warning", "error")) {
+  type <- match.arg(type, c("style", "warning", "error"))
+  line1 <- xml2::xml_attr(xml, "line1")[1]
+  col1 <- as.integer(xml2::xml_attr(xml, "col1"))
+
+  if (xml2::xml_attr(xml, "line2") == line1) {
+    col2 <- as.integer(xml2::xml_attr(xml, "col2"))
+  } else {
+    col2 <- nchar(source_file$lines[line1])
+  }
+  return(Lint(
+    filename = source_file$filename,
+    line_number = as.integer(line1),
+    column_number = as.integer(col1),
+    type = type,
+    message = message,
+    line = source_file$lines[line1],
+    ranges = list(c(col1, col2)),
+    linter = linter
+  ))
+}
