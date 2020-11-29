16cc938db46044bf849282f71b55d28200a0a4b5
Michael Chirico <michaelchirico4@gmail.com>
2020-11-28 22:37:16 -0500

    reverting equals_na_linter

--- R/equals_na_linter.R
+++ R/equals_na_linter.R
@@ -1,40 +1,38 @@
 #' @describeIn linters that checks for x == NA
-#'
 #' @export
 equals_na_linter <- function(source_file) {
-  if (!is.null(source_file[["file_lines"]])) {
-    # abort if source_file is entire file, not a top level expression.
-    return(NULL)
-  }
-
-  xml <- source_file[["xml_parsed_content"]]
-
-  # match on the '=='
-  xpath <- paste0(
-    "//EQ[",
-    "text() = '==' ",
-    "and following-sibling::*/NUM_CONST[text() = 'NA']",
-    "]"
+  all_matches <- re_matches(
+    source_file$lines,
+    rex("==", zero_or_more(" "), "NA"),
+    locations = TRUE,
+    global = TRUE
   )
+  line_numbers <- as.integer(names(source_file$lines))
 
-  eq_na_exprs <- xml2::xml_find_all(xml, xpath)
-
-  lapply(
-    eq_na_exprs,
-    function(expr) {
-      x <- as_list(expr)
-      line_num <- x@line1
-      line <- source_file[["lines"]][[as.character(line_num)]]
-      Lint(
-        filename = source_file$filename,
-        line_number = line_num,
-        column_number = x@col1,
-        type = "warning",
-        message = "Use is.na rather than == NA.",
-        line = line,
-        ranges = list(as.numeric(c(x@col1, x@col2))),
-        "equals_na_linter"
+  Map(
+    function(line_matches, line_number) {
+      lapply(
+        split(line_matches, seq_len(nrow(line_matches))),
+        function(match) {
+          start <- match[["start"]]
+          if (is.na(start)) {
+            return()
+          }
+          end <- match[["end"]]
+          Lint(
+            filename = source_file$filename,
+            line_number = line_number,
+            column_number = start,
+            type = "warning",
+            message = "Use is.na rather than == NA.",
+            line = source_file$lines[[as.character(line_number)]],
+            ranges = list(c(start, end)),
+            linter = "equals_na_linter"
+          )
+        }
       )
-    }
+    },
+    all_matches,
+    line_numbers
   )
 }
