aef1fe8af8e2a996842d26b44b3ee71d6e7932ee
Michael Chirico <michaelchirico4@gmail.com>
2020-11-30 21:17:26 -0500

    attempt to set LINTR_COMMENT_BOT env for test

--- tests/testthat/test-ci.R
+++ tests/testthat/test-ci.R
@@ -12,21 +12,18 @@ test_that("GitHub Actions functionality works", {
   })
 })
 
-# test_that("Printing works for Travis", {
-#   withr::with_envvar(c(GITHUB_ACTIONS = "false", TRAVIS_REPO_SLUG = "test/repo"), {
-#     old = options(lintr.rstudio_source_markers = FALSE)
-#     on.exit(options(old), add = TRUE)
-#
-#     # set comment_bot to TRUE ?
-#
-#
-#     writeLines("x <- 1:nrow(y)", tmp <- tempfile())
-#     on.exit(unlink(tmp))
-#
-#     l <- lint(tmp)
-#
-#     with_mock(github_comment = function(x, ...) cat(x, "\n"), {
-#       expect_output(print(l), "*warning:*", fixed = TRUE)
-#     })
-#   })
-# })
+test_that("Printing works for Travis", {
+  withr::with_envvar(c(GITHUB_ACTIONS = "false", TRAVIS_REPO_SLUG = "test/repo", LINTR_COMMENT_BOT = "true"), {
+    old = options(lintr.rstudio_source_markers = FALSE)
+    on.exit(options(old), add = TRUE)
+
+    writeLines("x <- 1:nrow(y)", tmp <- tempfile())
+    on.exit(unlink(tmp))
+
+    l <- lint(tmp)
+
+    with_mock(github_comment = function(x, ...) cat(x, "\n"), {
+      expect_output(print(l), "*warning:*", fixed = TRUE)
+    })
+  })
+})
