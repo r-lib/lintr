eadeb392a34ba9333468cbfbe0ef055fd52b8c97
Michael Chirico <michaelchirico4@gmail.com>
2020-11-30 21:13:10 -0500

    uncovr rot

--- R/utils.R
+++ R/utils.R
@@ -134,13 +134,11 @@ logical_env <- function(x) {
 }
 
 # from ?chartr
-# nocov start used in .onLoad which is nocov
 rot <- function(ch, k = 13) {
   p0 <- function(...) paste(c(...), collapse = "")
   A <- c(letters, LETTERS, " '")
   I <- seq_len(k); chartr(p0(A), p0(c(A[-I], A[I])), ch)
 }
-# nocov end
 
 trim_ws <- function(x) {
   sub("^\\s+", "", sub("\\s+$", "", x))
