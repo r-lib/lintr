8c95b03a4b8a1b034796c3bb5a5a02abd4e98c9e
Alexander Rosenstock <alexander.rosenstock@web.de>
2020-12-04 18:10:27 +0100

    improve resilience by generating expected error message directly

--- tests/testthat/test-error.R
+++ tests/testthat/test-error.R
@@ -39,10 +39,25 @@ test_that("returns the correct linting", {
     equals_na_linter
   )
 
+  # Error message changed in R-devel as of 2020/12
+  old_lang <- Sys.getenv("LANG")
+  Sys.setenv(LANG = "en")
+  on.exit(Sys.setenv(LANG = old_lang), add = TRUE)
+  expected_message <- tryCatch(parse(text = "\\"), error = function(e) {
+    rex::re_substitutes(
+      data = conditionMessage(e),
+      pattern = rex::rex(
+        list(start, "<text>:1:1: ") %or%
+          list(newline, anything, newline, anything, end)
+      ),
+      replacement = "",
+      global = TRUE
+    )
+  })
+
   expect_lint(
     "\\",
-    # Error message changed in R-devel as of 2020/12
-    rex("unexpected input" %or% "unexpected end of line")
+    rex(expected_message)
   )
 
   expect_lint("``",
