6bdf7810794a3e3e16cfcb1652dce1031c4ee367
AshesITR <alexander.rosenstock@web.de>
2020-12-05 12:05:20 +0100

    de-lint test-ci.R

--- tests/testthat/test-ci.R
+++ tests/testthat/test-ci.R
@@ -1,7 +1,7 @@
 test_that("GitHub Actions functionality works", {
   # imitate being on GHA whether or not we are
   withr::with_envvar(c(GITHUB_ACTIONS = "true"), {
-    old = options(lintr.rstudio_source_markers = FALSE)
+    old <- options(lintr.rstudio_source_markers = FALSE)
     on.exit(options(old), add = TRUE)
 
     writeLines("x <- 1:nrow(y)", tmp <- tempfile())
@@ -14,7 +14,7 @@ test_that("GitHub Actions functionality works", {
 
 test_that("Printing works for Travis", {
   withr::with_envvar(c(GITHUB_ACTIONS = "false", TRAVIS_REPO_SLUG = "test/repo", LINTR_COMMENT_BOT = "true"), {
-    old = options(lintr.rstudio_source_markers = FALSE)
+    old <- options(lintr.rstudio_source_markers = FALSE)
     on.exit(options(old), add = TRUE)
 
     writeLines("x <- 1:nrow(y)", tmp <- tempfile())
@@ -30,7 +30,7 @@ test_that("Printing works for Travis", {
 
 test_that("Printing works for Wercker", {
   withr::with_envvar(c(GITHUB_ACTIONS = "false", WERCKER_GIT_BRANCH = "test/repo", LINTR_COMMENT_BOT = "true"), {
-    old = options(lintr.rstudio_source_markers = FALSE)
+    old <- options(lintr.rstudio_source_markers = FALSE)
     on.exit(options(old), add = TRUE)
 
     writeLines("x <- 1:nrow(y)", tmp <- tempfile())
