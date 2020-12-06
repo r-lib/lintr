abf92a4db1ef9949e79c2c1bc6488eaba2a39e2e
AshesITR <alexander.rosenstock@web.de>
2020-12-06 19:06:19 +0100

    fix delint conflicts (#687)
    * de-lint test-absolute_path_linter.R
    
    * de-lint test-exclusions.R
    
    * de-lint test-extraction_operator_linter.R
    
    * de-lint test-get_source_expressions.R
    
    * de-lint test-implicit_integer_linter.R
    
    * de-lint test-nonportable_path_linter.R
    
    * de-lint test-object_name_linter.R
    
    * de-lint test-pipe_continuation_linter.R
    
    * de-lint test-semicolon_terminator_linter.R
    
    * de-lint test-settings.R
    
    * de-lint test-T_and_F_symbol_linter.R
    
    * de-lint test-todo_comment_linter.R
    
    * de-lint test-undesirable_function_linter.R
    
    * de-lint test-undesirable_operator_linter.R
    
    * de-lint test-unneeded_concatenation_linter.R
    
    * bump cyclocomp limit up to 30 for lint()
    
    caused by #673
    
    * fix test failure on non-english locale
    
    * fix test failure on non-english locale
    
    * remove most context() calls in touched files
    
    test-exclusions.R was not touched since it contains 3 calls (and will be split by #660)

--- tests/testthat/test-undesirable_function_linter.R
+++ tests/testthat/test-undesirable_function_linter.R
@@ -1,18 +1,16 @@
-context("undesirable_function_linter")
-
 test_that("linter returns correct linting", {
-  linter <- undesirable_function_linter(fun=c("return"=NA, "log10"="use log()"))
-  msgR <- "Function \"return\" is undesirable.$"
-  msgL <- "Function \"log10\" is undesirable. As an alternative, use log\\(\\)."
+  linter <- undesirable_function_linter(fun = c("return" = NA, "log10" = "use log()"))
+  msg_return <- "Function \"return\" is undesirable.$"
+  msg_log10 <- "Function \"log10\" is undesirable. As an alternative, use log\\(\\)."
 
   expect_lint("x <- options()", NULL, linter)
   expect_lint("cat(\"Try to return\")", NULL, linter)
-  expect_lint("lapply(x, log10)", list(message=msgL, line_number=1L, column_number=11L), linter)
-  expect_lint("return()", list(message=msgR, line_number=1L, column_number=1L), linter)
+  expect_lint("lapply(x, log10)", list(message = msg_log10, line_number = 1L, column_number = 11L), linter)
+  expect_lint("return()", list(message = msg_return, line_number = 1L, column_number = 1L), linter)
   expect_lint("function(x) {\nprint(options())\ny <- log10(x)\nreturn(y)\n}",
               list(
-                list(message=msgL, line_number=3L, column_number=6L),
-                list(message=msgR, line_number=4L, column_number=1L)
+                list(message = msg_log10, line_number = 3L, column_number = 6L),
+                list(message = msg_return, line_number = 4L, column_number = 1L)
               ),
               linter)
 })
