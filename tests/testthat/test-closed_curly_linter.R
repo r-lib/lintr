2405a5cfb354aa010f2029a11b4ebabdadcba687
Russ Hyde <russ.hyde.data@gmail.com>
2021-01-28 16:38:44 +0000

    refac: replace a duplicated linter-message regex

--- tests/testthat/test-closed_curly_linter.R
+++ tests/testthat/test-closed_curly_linter.R
@@ -1,3 +1,10 @@
+closed_curly_message_regex <- rex(
+  paste(
+    "Closing curly-braces should always be on their own line,",
+    "unless it's followed by an else."
+  )
+)
+
 test_that("returns the correct linting", {
 
   expect_lint("blah",
@@ -9,11 +16,11 @@ test_that("returns the correct linting", {
     closed_curly_linter())
 
   expect_lint("a <- function() { 1 }",
-    rex("Closing curly-braces should always be on their own line, unless it's followed by an else."),
+    closed_curly_message_regex,
     closed_curly_linter())
 
   expect_lint("a <- function() { 1 }",
-    rex("Closing curly-braces should always be on their own line, unless it's followed by an else."),
+    closed_curly_message_regex,
     closed_curly_linter())
 
   expect_lint("a <- function() { 1 }",
@@ -21,18 +28,18 @@ test_that("returns the correct linting", {
               closed_curly_linter(allow_single_line = TRUE))
 
   expect_lint("a <- if(1) {\n 1} else {\n 2\n}",
-    rex("Closing curly-braces should always be on their own line, unless it's followed by an else."),
+    closed_curly_message_regex,
     closed_curly_linter())
 
   expect_lint("a <- if(1) {\n 1\n} else {\n 2}",
-    rex("Closing curly-braces should always be on their own line, unless it's followed by an else."),
+    closed_curly_message_regex,
     closed_curly_linter())
 
   expect_lint("a <- if(1) {\n 1} else {\n 2}",
     list(
-      rex("Closing curly-braces should always be on their own line, unless it's followed by an else."),
-      rex("Closing curly-braces should always be on their own line, unless it's followed by an else.")
-      ),
+      closed_curly_message_regex,
+      closed_curly_message_regex
+    ),
     closed_curly_linter())
 
   expect_lint("eval(bquote({...}))",
