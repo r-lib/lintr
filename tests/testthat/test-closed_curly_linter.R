8acc4eaf67f1a2c048103214fa91f3dba5965126
Russ Hyde <russ.hyde.data@gmail.com>
2021-01-28 16:41:07 +0000

    refac: indentation consistency in tests

--- tests/testthat/test-closed_curly_linter.R
+++ tests/testthat/test-closed_curly_linter.R
@@ -24,8 +24,8 @@ test_that("returns the correct linting", {
     closed_curly_linter())
 
   expect_lint("a <- function() { 1 }",
-              NULL,
-              closed_curly_linter(allow_single_line = TRUE))
+    NULL,
+    closed_curly_linter(allow_single_line = TRUE))
 
   expect_lint("a <- if(1) {\n 1} else {\n 2\n}",
     closed_curly_message_regex,
@@ -43,16 +43,16 @@ test_that("returns the correct linting", {
     closed_curly_linter())
 
   expect_lint("eval(bquote({...}))",
-              NULL,
-              closed_curly_linter())
+    NULL,
+    closed_curly_linter())
 
   expect_lint("fun({\n  statements\n}, param)",
-              NULL,
-              closed_curly_linter())
+    NULL,
+    closed_curly_linter())
 
   expect_lint("out <- lapply(stuff, function(i) {\n  do_something(i)\n}) %>% unlist",
-              NULL,
-              closed_curly_linter())
+    NULL,
+    closed_curly_linter())
 
   expect_lint("{{x}}", NULL, closed_curly_linter())
 })
