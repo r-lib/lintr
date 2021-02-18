611aa1e65d3ad9071dda4f2b60baab2bc8ffddba
Alexander Rosenstock <alexander.rosenstock@web.de>
2021-02-18 09:38:42 +0100

    add comments

--- R/object_usage_linter.R
+++ R/object_usage_linter.R
@@ -61,6 +61,7 @@ object_usage_linter <- function() {
 
           location <- re_matches(line, rex(boundary, row$name, boundary), locations = TRUE)
 
+          # Handle multi-line lints where name occurs on subsequent lines (#507)
           if (is.na(location$start) && nzchar(row$line2) && row$line2 != row$line1) {
             lines <- source_file$content[org_line_num:(as.integer(row$line2) + info$line1 - 1L)]
             locations <- re_matches(lines, rex(boundary, row$name, boundary), locations = TRUE)
@@ -72,6 +73,7 @@ object_usage_linter <- function() {
             line <- lines[matching_row]
           }
 
+          # Fallback if name isn't found anywhere: lint the first line
           if (is.na(location$start)) {
             location$start <- 1L
             location$end <- nchar(line)
