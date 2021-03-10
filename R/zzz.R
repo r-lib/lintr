03bde3c36d9d070fe271ae8dab5b2cb36fb8d0aa
Michael Chirico <michaelchirico4@gmail.com>
2021-03-09 18:45:38 -0800

    with_defaults removes newlines before processing names

--- R/zzz.R
+++ R/zzz.R
@@ -44,7 +44,12 @@ with_defaults <- function(..., default = default_linters) {
     # var[["foo"]]    => "foo"
     nms[missing] <- re_substitutes(
       re_substitutes(
-        re_substitutes(args, rex("(", anything), ""),
+        re_substitutes(
+          # Very long input might have newlines which are not caught
+          #  by . in a perl regex; see #774
+          gsub("\n", "", args, fixed = TRUE),
+          rex("(", anything), ""
+        ),
         rex(start, anything, '["'),
         ""
       ),
