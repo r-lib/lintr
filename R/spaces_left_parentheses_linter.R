32a04d317503fbbbc390cfa7323d96044ae4a2ed
Michael Chirico <michaelchirico4@gmail.com>
2021-03-11 01:00:27 -0800

    tweaks for neatness

--- R/spaces_left_parentheses_linter.R
+++ R/spaces_left_parentheses_linter.R
@@ -8,12 +8,18 @@ spaces_left_parentheses_linter <- function() {
 
     xml <- source_file$xml_parsed_content
 
-    # see infix_spaces_linter.R; preceding-sibling::* is for unary operators like -(a)
-    infix_selves <- paste0("self::", names(infix_tokens), "[preceding-sibling::*]", collapse = " or ")
+    # apply the lint by requiring a gap in three cases:
+    #   (1) if/while loop conditions, e.g. 'if(x>2) { }'
+    #   (2) for loop conditions, e.g. 'for(i in 1:5) { }' [very similar to (1) in code but different in XML]
+    #   (3) non-unary infix operators, e.g. 'x&(y | z)'
+
     # -2 on LHS because, when RHS matches nothing, +2 tricks the condition into returning true...
     #   while @start will always be there, the @end may not be
     if_while_cond <- "@start - 2 != preceding-sibling::*[self::IF or self::WHILE]/@end"
     for_cond <- "@start - 2 != parent::forcond/preceding-sibling::FOR/@end"
+
+    # see infix_spaces_linter.R; preceding-sibling::* is for unary operators like -(a)
+    infix_selves <- paste0("self::", names(infix_tokens), "[preceding-sibling::*]", collapse = " or ")
     # preceding-symbol::* catches (1) function definitions and (2) function calls
     infix_cond <- sprintf(
       "not(preceding-sibling::*) and (@start - 2 != parent::expr/preceding-sibling::*[%s]/@end)",
