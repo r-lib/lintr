54a8bb7d887f7444d993acb73bb3b210e0826229
Michael Chirico <michaelchirico4@gmail.com>
2021-03-11 11:31:50 -0800

    regression test for unary ~

--- tests/testthat/test-spaces_left_parentheses_linter.R
+++ tests/testthat/test-spaces_left_parentheses_linter.R
@@ -47,6 +47,7 @@ test_that("returns the correct linting", {
 
   expect_lint("'[[<-.data.frame'(object, y)", NULL, linter)
   expect_lint("object@data@get('input')", NULL, linter)
+  expect_lint("x <- ~(. + y)", NULL, linter)
   # these don't violate the linter, even if they are strange coding practice
   expect_lint("for (ii in 1:10) next()", NULL, linter)
   expect_lint("for (ii in 1:10) break()", NULL, linter)
