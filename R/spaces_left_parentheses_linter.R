a286648d88d2eb6d9807ac5857e5c5fac9acdf22
Michael Chirico <michaelchirico4@gmail.com>
2021-03-11 01:35:39 -0800

    use affirmative instead of negative condition

--- R/spaces_left_parentheses_linter.R
+++ R/spaces_left_parentheses_linter.R
@@ -13,16 +13,16 @@ spaces_left_parentheses_linter <- function() {
     #   (2) for loop conditions, e.g. 'for(i in 1:5) { }' [very similar to (1) in code but different in XML]
     #   (3) non-unary infix operators, e.g. 'x&(y | z)'
 
-    # -2 on LHS because, when RHS matches nothing, +2 tricks the condition into returning true...
-    #   while @start will always be there, the @end may not be
-    if_while_cond <- "@start - 2 != preceding-sibling::*[self::IF or self::WHILE]/@end"
-    for_cond <- "@start - 2 != parent::forcond/preceding-sibling::FOR/@end"
+    # -1 on LHS because, when RHS matches nothing, +1 tricks the condition into returning true...
+    #   while @start will always be there, the @end may not be.
+    if_while_cond <- "@start - 1 = preceding-sibling::*[self::IF or self::WHILE]/@end"
+    for_cond <- "@start - 1 = parent::forcond/preceding-sibling::FOR/@end"
 
     # see infix_spaces_linter.R; preceding-sibling::* is for unary operators like -(a)
     infix_selves <- paste0("self::", names(infix_tokens), "[preceding-sibling::*]", collapse = " or ")
     # preceding-symbol::* catches (1) function definitions and (2) function calls
     infix_cond <- sprintf(
-      "not(preceding-sibling::*) and (@start - 2 != parent::expr/preceding-sibling::*[%s]/@end)",
+      "not(preceding-sibling::*) and (@start - 1 = parent::expr/preceding-sibling::*[%s]/@end)",
       infix_selves
     )
     xpath <- sprintf(
