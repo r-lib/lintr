0dcb701d7ffb3a455ac330a33ff4861392ff6bea
Michael Chirico <chiricom@google.com>
2021-05-18 23:59:23 -0700

    add tests

---
+++ tests/testthat/test-pipe_call_linter.R
@@ -0,0 +1,48 @@
+test_that("pipe_call_linter skips allowed usages", {
+  expect_lint("a %>% foo()", NULL, pipe_call_linter)
+  expect_lint("a %>% foo(x)", NULL, pipe_call_linter)
+  expect_lint("b %>% { foo(., ., .) }", NULL, pipe_call_linter)
+  expect_lint("a %>% foo() %>% bar()", NULL, pipe_call_linter)
+
+  # ensure it works across lines too
+  lines <- trim_some("
+  a %>%
+    foo() %>%
+    bar()
+  ")
+  expect_lint(lines, NULL, pipe_call_linter)
+
+  # symbol extraction is OK (don't force extract2(), e.g.)
+  expect_lint("a %>% .$y %>% mean()", NULL, pipe_call_linter)
+})
+
+test_that("pipe_call_linter blocks simple disallowed usages", {
+  expect_lint(
+    "x %>% foo",
+    "Use explicit calls in magrittr pipes",
+    pipe_call_linter
+  )
+
+  expect_lint(
+    "x %>% foo() %>% bar",
+    "Use explicit calls in magrittr pipes",
+    pipe_call_linter
+  )
+
+  expect_lint(
+    "x %>% foo %>% bar()",
+    "Use explicit calls in magrittr pipes",
+    pipe_call_linter
+  )
+
+  lines <- trim_some("
+  a %>%
+    foo %>%
+    bar()
+  ")
+  expect_lint(
+    lines,
+    "Use explicit calls in magrittr pipes",
+    pipe_call_linter
+  )
+})
