761d6259dd5fcbc42854d35a86c36e706bc3a9ba
Alexander Rosenstock <alexander.rosenstock@web.de>
2021-07-02 18:42:39 +0200

    remove pointless NULL check and make sure result is not named

--- R/settings.R
+++ R/settings.R
@@ -132,16 +132,14 @@ get_encoding_from_dcf <- function(file) {
   if (is.null(file)) return(NULL)
 
   encodings <- tryCatch(
-    drop(read.dcf(file, "Encoding")),
+    unname(drop(read.dcf(file, "Encoding"))),
     error = function(e) NULL,
     warning = function(e) NULL
   )
 
-  if (!is.null(encodings)) {
-    encodings <- encodings[!is.na(encodings)]
-    if (length(encodings) > 0L) {
-      return(encodings[1L])
-    }
+  encodings <- encodings[!is.na(encodings)]
+  if (length(encodings) > 0L) {
+    return(encodings[1L])
   }
 
   NULL
