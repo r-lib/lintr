0397cb777e42ac25fcda8f7245edaadbf56f6fc0
AshesITR <alexander.rosenstock@web.de>
2021-07-02 19:44:08 +0200

    capture specifically generic names  (#755)
    * capture specifically generic names
    
    fixes #737
    
    * add NEWS bullet
    
    * add regression test file
    
    * add anchors to regex
    
    remove empty string "generics"
    
    * add .lintr file to not exclude regression test file
    
    * improve generic detection
    
    * fix vapply
    
    * make s3 detection more robust
    
    * backwards compatible base s3 generics
    
    * try fixing compatibility issues with R 3.3 and 3.4
    
    * typo
    
    * fix R CMD check warning (by wrapping)
    
    * remove dead code
    
    * de-lint
    
    * use nzchar
    
    * fix typo, improve style, fix compare_branches.R (forgot the fallback to UTF-8)
    
    Co-authored-by: Michael Chirico <chiricom@google.com>
    Co-authored-by: Michael Chirico <michaelchirico4@gmail.com>

--- R/namespace.R
+++ R/namespace.R
@@ -16,5 +16,43 @@ namespace_imports <- function(path = find_package()) {
   data.frame(
     pkg = unlist(lapply(imports, function(x) rep(x[[1L]], length(x[[2L]])))),
     fun = unlist(lapply(imports, `[[`, 2L)),
-    stringsAsFactors = FALSE)
+    stringsAsFactors = FALSE
+  )
 }
+
+# filter namespace_imports() for S3 generics
+# this loads all imported namespaces
+imported_s3_generics <- function(ns_imports) {
+  is_generic <- vapply(
+    seq_len(nrow(ns_imports)),
+    function(i) {
+      fun_obj <- get(ns_imports$fun[i], envir = asNamespace(ns_imports$pkg[i]))
+      is.function(fun_obj) && is_s3_generic(fun_obj)
+    },
+    logical(1L)
+  )
+
+  ns_imports[is_generic, ]
+}
+
+is_s3_generic <- function(fun) {
+  if (getRversion() >= "3.5.0") {
+    # Available in 3.4.0, but bugged there in multiple ways that cause errors
+    # throughout many base functions, e.g. `-`, `as.null.default`, `dontCheck`
+    utils::isS3stdGeneric(fun)
+  } else {
+    is.function(fun) # nocov
+  }
+}
+
+.base_s3_generics <- c(
+  names(.knownS3Generics),
+  .S3PrimitiveGenerics,
+  if (getRversion() >= "3.5.0") {
+    .S3_methods_table[, 1L]
+  } else {
+    # R < 3.5.0 doesn't provide .S3_methods_table
+    # fallback: search baseenv() for generic methods
+    imported_s3_generics(data.frame(pkg = "base", fun = ls(baseenv()), stringsAsFactors = FALSE))$fun
+  }
+)
