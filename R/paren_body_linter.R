d129dfd768187ecdc52b3a022df0caf20cf6546a
Konrad Pagacz <konrad.pagacz@contractors.roche.com>
2021-07-09 16:08:27 +0200

    Updated after review no 1

--- R/paren_body_linter.R
+++ R/paren_body_linter.R
@@ -6,28 +6,31 @@
 paren_body_linter <- function() {
   Linter(function(source_file) {
     if (is.null(source_file$xml_parsed_content)) return(NULL)
-    
+
     xpath <- paste(
       "//expr[",
       "@line1 = preceding-sibling::FUNCTION/@line1",
+      "|",
+      "preceding-sibling::IF/@line1",
+      "|",
+      "preceding-sibling::WHILE/@line1",
       "and",
       "@col1 = preceding-sibling::OP-RIGHT-PAREN/@col1 + 1",
+      "]",
+      "|",
+      "//expr[",
+      "@line1 = preceding-sibling::forcond/@line1",
+      "and",
+      "@col1 = preceding-sibling::forcond/OP-RIGHT-PAREN/@col1 + 1",
       "]"
     )
     matched_expressions <- xml2::xml_find_all(source_file$xml_parsed_content, xpath)
-    
-    lapply(matched_expressions, get_lint_from_expression, source_file = source_file)
-  })
-}
 
-get_lint_from_expression <- function(expression, source_file) {
-  expression <- xml2::as_list(expression)
-  Lint(
-    filename = source_file$filename,
-    message = "There should be a space between the right parenthesis and the function body.",
-    column_number = expression@col1,
-    line_number = expression@line1,
-    line = source_file$lines[[as.character(expression@line1)]],
-    ranges = list(as.integer(c(expression@col1, expression@col2)))
-  )
+    lapply(
+      matched_expressions,
+      xml_nodes_to_lint,
+      source_file = source_file,
+      message = "There should be a space between right parenthesis and a body expression."
+    )
+  })
 }
--- tests/testthat/test-paren_body_linter.R
+++ tests/testthat/test-paren_body_linter.R
@@ -1,41 +1,45 @@
 testthat::test_that("paren_body_linter returns correct lints", {
   linter <- paren_body_linter()
-  msg <- "There should be a space between the right parenthesis and the function body."
-  
+  msg <- "There should be a space between right parenthesis and a body expression."
+
   # No space after the closing parenthesis prompts a lint
   expect_lint("function()test", msg, linter)
   expect_lint("print('hello')\nx <- function(x)NULL\nprint('hello')", msg, linter)
-  
+  expect_lint("if (TRUE)test", msg, linter)
+  expect_lint("while (TRUE)test", msg, linter)
+  expect_lint("for (i in seq_along(1))test", msg, linter)
+
   # A space after the closing parenthesis does not prompt a lint
   expect_lint("function() test", NULL, linter)
-  
-  # Symbols after the closing parentheses of a function call do not prompt a lint
+
+  # Symbols after the closing parenthesis of a function call do not prompt a lint
   expect_lint("head(mtcars)$cyl", NULL, linter)
-  
-  # paren_body_linter returns the correct column number
-  expect_lint("function()test", list(column_number = 11L), linter)
-  
+
   # paren_body_linter returns the correct line number
-  expect_lint("function()test", list(line_number = 1L), linter)
   expect_lint(
     "print('hello')\nx <- function(x)NULL\nprint('hello')",
     list(line_number = 2L),
     linter
   )
-  
-  # paren_body_linter returns the correct type
-  expect_lint("function()test", list(type = "style"), linter)
-  
-  # paren_body_linter returns the correct line
-  expect_lint("function()test", list(line = "function()test"), linter)
-  
-  # paren_body_linter returns the correct range
-  expect_lint("function()test", list(ranges = list(c(11L, 14L))), linter)
-  
-  # paren_body_linter does not lint when the function body is defined
-  # on a new line
+
+  expect_lint(
+    "function()test",
+    list(
+      line_number = 1L,
+      column_number = 11L,
+      type = "style",
+      line = c("1" = "function()test"),
+      ranges = list(c(11L, 14L))
+    ),
+    linter
+  )
+
+  # paren_body_linter does not lint when the function body is defined on a new line
   expect_lint("function()\n  test", NULL, linter)
-  
+
   # paren_body_linter does not lint comments
   expect_lint("#function()test", NULL, linter)
+
+  # multiple lints on the same line
+  expect_lint("function()if(TRUE)while(TRUE)test", list(msg, msg, msg), linter)
 })
