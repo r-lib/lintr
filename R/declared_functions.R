2cc538009edb3da801dc5490dad8e797c58400bd
Jonathan Keane <jkeane@gmail.com>
2021-07-23 16:10:57 -0500

    Add test for when a package is not in a directory of its own name

--- R/declared_functions.R
+++ R/declared_functions.R
@@ -16,22 +16,3 @@ declared_s3_generics <- function(x) {
 
   as.character(xml2::xml_find_all(x, xpath))
 }
-
-declared_r6_generics <- function(x) {
-  xpath <- paste0(
-    # Top level expression which
-    "/exprlist/expr",
-
-    # Assigns to a symbol
-    "[./LEFT_ASSIGN|EQ_ASSIGN]",
-    "[./expr/SYMBOL]",
-
-    # Is an R6 class (contains call to R6Class)
-    "[.//SYMBOL_FUNCTION_CALL[text()='R6Class']]",
-
-    # Retrieve assigned name of the function
-    "/expr/SYMBOL/text()")
-
-  as.character(xml2::xml_find_all(x, xpath))
-}
-
