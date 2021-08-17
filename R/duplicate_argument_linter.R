4a3b2e676bb854c22f6c1756eadbff5f2dcde1f5
Kun Ren <renkun@outlook.com>
2021-08-17 15:15:07 +0800

    Implement duplicate_argument_linter

---
+++ R/duplicate_argument_linter.R
@@ -0,0 +1,43 @@
+#' @describeIn linters checks for duplicate arguments in function calls.
+#' @param except a character vector of function names as exceptions.
+#' @export
+duplicate_argument_linter <- function(except = character()) {
+  Linter(function(source_file) {
+
+    if (is.null(source_file$full_xml_parsed_content)) return(list())
+
+    xml <- source_file$full_xml_parsed_content
+
+    xpath <- "//expr[expr[SYMBOL_FUNCTION_CALL] and EQ_SUB]"
+
+    calls <- xml2::xml_find_all(xml, xpath)
+
+    result <- lapply(calls, function(call) {
+      args <- xml2::xml_find_all(call, "EQ_SUB/preceding-sibling::*[1]")
+      args_text <- vapply(
+        parse(text = xml2::xml_text(args), keep.source = FALSE),
+        as.character, character(1L)
+      )
+      is_dup_arg <- duplicated(args_text)
+      dup_args <- args[is_dup_arg]
+      if (length(dup_args)) {
+        line1 <- as.integer(xml2::xml_attr(dup_args, "line1"))
+        col1 <- as.integer(xml2::xml_attr(dup_args, "col1"))
+        col2 <- as.integer(xml2::xml_attr(dup_args, "col2"))
+        lapply(seq_along(dup_args), function(i) {
+          Lint(
+            filename = source_file$filename,
+            line_number = line1[[i]],
+            column_number = col1[[i]],
+            type = "warning",
+            message = "Duplicate arguments in function call.",
+            line = source_file$file_lines[line1[[i]]],
+            ranges = list(c(col1[[i]], col2[[i]]))
+          )
+        })
+      }
+    })
+
+    unlist(result, recursive = FALSE, use.names = FALSE)
+  })
+}
