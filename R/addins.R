1de6f8557be791c3bdcfbff106b5737575dca888
Michael Chirico <chiricom@google.com>
2021-09-21 06:20:07 -0700

    move httr to Suggests (#863)
    * move httr to Suggests
    
    * move more packages to Suggests
    
    * NEWS
    
    * remove @importFrom
    
    * removing from NAMESPACE
    
    * restore dependencies of linters from with_defaults
    
    * sorting for no diff
    
    * restore NAMESPACE
    
    * adjust NEWS
    
    * roll back in cyclocomp_linter
    
    * rollback in object_usage_linter
    
    * nocov

--- R/addins.R
+++ R/addins.R
@@ -1,5 +1,8 @@
 # nocov start
 addin_lint <- function() {
+  if (!requireNamespace("rstudioapi", quietly = TRUE)) {
+    stop("'rstudioapi' is required for add-ins.") # nocov
+  }
   filename <- rstudioapi::getSourceEditorContext()
   if (filename$path == "") {
     return("Current source has no path. Please save before continue")
@@ -23,6 +26,9 @@ addin_lint <- function() {
 }
 
 addin_lint_package <- function() {
+  if (!requireNamespace("rstudioapi", quietly = TRUE)) {
+    stop("'rstudioapi' is required for add-ins.") # nocov
+  }
   project <- rstudioapi::getActiveProject()
   project_path <- if (is.null(project)) getwd() else project
 
