dd1ea2dc9dd8c80730bd91af8031337150258a8f
Konrad Pagacz <konrad.pagacz@gmail.com>
2021-09-27 09:55:25 +0200

    Fixed nocov annotations

--- R/expect_lint.R
+++ R/expect_lint.R
@@ -41,9 +41,9 @@
 #' @export
 expect_lint <- function(content, checks, ..., file = NULL, language = "en") {
   if (!requireNamespace("testthat", quietly = TRUE)) {
-    stop(
+    stop( # nocov start
       "'expect_lint' is designed to work within the 'testthat' testing framework, but 'testthat' is not installed."
-    ) # nocov
+    ) # nocov end
   }
   old_lang <- set_lang(language)
   on.exit(reset_lang(old_lang))
