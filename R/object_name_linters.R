8340014de1041037e59c46fa53d15f8a98dcea6c
Alexander Rosenstock <alexander.rosenstock@web.de>
2021-10-09 10:36:47 +0200

    Refactor `object_length_linter` to properly handle generics
    Fixes #871

--- R/object_name_linters.R
+++ R/object_name_linters.R
@@ -1,3 +1,40 @@
+object_name_xpath <- paste0(
+  # assignments
+  "//SYMBOL[",
+  " not(preceding-sibling::OP-DOLLAR)",
+  " and ancestor::expr[",
+  "  following-sibling::LEFT_ASSIGN",
+  "  or preceding-sibling::RIGHT_ASSIGN",
+  "  or following-sibling::EQ_ASSIGN",
+  " ]",
+  " and not(ancestor::expr[",
+  "  preceding-sibling::OP-LEFT-BRACKET",
+  "  or preceding-sibling::LBB",
+  " ])",
+  "]",
+
+  " | ",
+
+  "//STR_CONST[",
+  " not(preceding-sibling::OP-DOLLAR)",
+  " and ancestor::expr[",
+  "  following-sibling::LEFT_ASSIGN",
+  "  or preceding-sibling::RIGHT_ASSIGN",
+  "  or following-sibling::EQ_ASSIGN",
+  " ]",
+  " and not(ancestor::expr[",
+  "  preceding-sibling::OP-LEFT-BRACKET",
+  "  or preceding-sibling::LBB",
+  " ])",
+  "]",
+
+  # Or
+  " | ",
+
+  # Formal argument names
+  "//SYMBOL_FORMALS"
+)
+
 #' Object name linter
 #'
 #' Check that object names conform to a naming style.
@@ -34,44 +71,7 @@ object_name_linter <- function(styles = c("snake_case", "symbols")) {
 
     xml <- source_file$full_xml_parsed_content
 
-    xpath <- paste0(
-      # assignments
-      "//SYMBOL[",
-      " not(preceding-sibling::OP-DOLLAR)",
-      " and ancestor::expr[",
-      "  following-sibling::LEFT_ASSIGN",
-      "  or preceding-sibling::RIGHT_ASSIGN",
-      "  or following-sibling::EQ_ASSIGN",
-      " ]",
-      " and not(ancestor::expr[",
-      "  preceding-sibling::OP-LEFT-BRACKET",
-      "  or preceding-sibling::LBB",
-      " ])",
-      "]",
-
-      " | ",
-
-      "//STR_CONST[",
-      " not(preceding-sibling::OP-DOLLAR)",
-      " and ancestor::expr[",
-      "  following-sibling::LEFT_ASSIGN",
-      "  or preceding-sibling::RIGHT_ASSIGN",
-      "  or following-sibling::EQ_ASSIGN",
-      " ]",
-      " and not(ancestor::expr[",
-      "  preceding-sibling::OP-LEFT-BRACKET",
-      "  or preceding-sibling::LBB",
-      " ])",
-      "]",
-
-      # Or
-      " | ",
-
-      # Formal argument names
-      "//SYMBOL_FORMALS"
-    )
-
-    assignments <- xml2::xml_find_all(xml, xpath)
+    assignments <- xml2::xml_find_all(xml, object_name_xpath)
 
     # Retrieve assigned name
     nms <- strip_names(
@@ -93,7 +93,7 @@ object_name_linter <- function(styles = c("snake_case", "symbols")) {
 
     lapply(
       assignments[!matches_a_style],
-      object_lint2,
+      object_lint,
       source_file,
       lint_msg
     )
@@ -131,7 +131,7 @@ strip_names <- function(x) {
   x
 }
 
-object_lint2 <- function(expr, source_file, message) {
+object_lint <- function(expr, source_file, message) {
   symbol <- xml2::as_list(expr)
   Lint(
     filename = source_file$filename,
@@ -142,135 +142,6 @@ object_lint2 <- function(expr, source_file, message) {
     line = source_file$file_lines[as.numeric(symbol@line1)],
     ranges = list(as.numeric(c(symbol@col1, symbol@col2))),
   )
-}
-
-object_linter_factory <- function(fun, name = linter_auto_name()) {
-  force(name)
-  Linter(function(source_file) {
-
-    token_nums <- ids_with_token(
-      source_file, rex(start, "SYMBOL" %if_next_isnt% "_SUB"), fun = re_matches
-    )
-    if (length(token_nums) == 0) {
-      return(list())
-    }
-    tokens <- with_id(source_file, token_nums)
-    names <- unquote(tokens[["text"]]) # remove surrounding backticks
-
-    keep_indices <- which(
-      !is_operator(names) &
-        !is_known_generic(names) &
-        !is_base_function(names)
-    )
-
-    lapply(
-      keep_indices,
-      function(i) {
-        token <- tokens[i, ]
-        if (is_declared_here(token, source_file) &&
-            !is_external_reference(source_file, token[["id"]])) {
-          fun(source_file, token)
-        }
-      }
-    )
-  }, name = name)
-}
-
-known_generic_regex <- rex(
-  start,
-  or(
-    unique(
-      # Clean up "as.data.frame" to "as", "names<-" to "names", etc
-      re_substitutes(c(names(.knownS3Generics), .S3PrimitiveGenerics),
-                     rex(or(dot, "<-"), anything, end), "")
-    )
-  ),
-  dot
-)
-
-is_known_generic <- function(name) {
-  re_matches(name, known_generic_regex)
-}
-
-is_declared_here <- function(token, source_file) {
-  # The object was declared here if one of the following is true:
-  #   * its name precedes a left assign ("<-" or "<<-") or equal assign ("=")
-  #   * its name follows a right assign ("->" or "->>")
-  #   * its name is not "..." and its first sibling token is a function definition
-  filt <- filter_out_token_type(source_file[["parsed_content"]], "expr")
-  assign_regex <- rex(start, or("EQ_ASSIGN", "LEFT_ASSIGN"), end)
-  l <- which(filt[, "id"] == token[["id"]])
-  if ((l + 1L <= dim(filt)[[1L]] && re_matches(filt[l + 1L, "token"], assign_regex)) ||
-       (l >= 2L  &&  filt[l - 1L, "token"] == "RIGHT_ASSIGN")) {
-    # assigned variable or function parameter
-    TRUE
-  } else {
-    sibling_ids <- siblings(source_file[["parsed_content"]], token[["id"]], 1L)
-    if (token[["text"]] != "..." &&
-        length(sibling_ids) &&
-        with_id(source_file, sibling_ids[[1L]])[["text"]] == "function") {
-      # parameter in function definition
-      TRUE
-    } else {
-      FALSE
-    }
-  }
-}
-
-is_operator <- function(name) {
-  name != make.names(name)
-}
-
-is_external_reference <- function(source_file, id) {
-  sibling_tokens <- with_id(source_file, siblings(source_file$parsed_content, id, 1))$token
-  any(sibling_tokens %in% c("NS_GET", "NS_GET_INT"))
-}
-
-# via unlist(tools:::.get_standard_package_names(), use.names = FALSE)
-base_pkgs <- c(
-  "base",
-  "tools",
-  "utils",
-  "grDevices",
-  "graphics",
-  "stats",
-  "datasets",
-  "methods",
-  "grid",
-  "splines",
-  "stats4",
-  "compiler",
-  "parallel",
-  "MASS",
-  "lattice",
-  "Matrix",
-  "nlme",
-  "survival",
-  "boot",
-  "cluster",
-  "codetools",
-  "foreign",
-  "KernSmooth",
-  "rpart",
-  "class",
-  "nnet",
-  "spatial",
-  "mgcv"
-)
-
-# some duplicates such as .onLoad appear in multiple packages; sort for efficiency
-base_funs <- sort(unique(unlist(lapply(
-  base_pkgs,
-  function(x) {
-    name <- try_silently(getNamespace(x))
-    if (!inherits(name, "try-error")) {
-      ls(name, all.names = TRUE)
-    }
-  }
-))))
-
-is_base_function <- function(x) {
-  x %in% base_funs
 }
 
 # see ?".onLoad", ?Startup, and ?quit. Remove leading dot to match behavior of strip_names().
@@ -291,19 +162,6 @@ special_funs <- c(
 is_special_function <- function(x) {
   x %in% special_funs
 }
-
-object_lint <- function(source_file, token, message) {
-  Lint(
-    filename = source_file$filename,
-    line_number = token$line1,
-    column_number = token$col1,
-    type = "style",
-    message = message,
-    line = source_file$lines[as.character(token$line1)],
-    ranges = list(c(token$col1, token$col2))
-  )
-}
-
 
 loweralnum <- rex(one_of(lower, digit))
 upperalnum <- rex(one_of(upper, digit))
@@ -330,13 +188,38 @@ regexes_rd <- toString(paste0("\\sQuote{", names(style_regexes), "}"))
 #' @seealso [linters] for a complete list of linters available in lintr.
 #' @export
 object_length_linter <- function(length = 30L) {
-  object_linter_factory(function(source_file, token) {
-    if (nchar(token$text) > length) {
-        object_lint(
-          source_file,
-          token,
-          paste0("Variable and function names should not be longer than ", length, " characters.")
-        )
-      }
+  lint_msg <- paste0("Variable and function names should not be longer than ", length, " characters.")
+
+  Linter(function(source_file) {
+    if (is.null(source_file$full_xml_parsed_content)) return(list())
+
+    xml <- source_file$full_xml_parsed_content
+
+    assignments <- xml2::xml_find_all(xml, object_name_xpath)
+
+    # Retrieve assigned name
+    nms <- strip_names(
+      xml2::xml_text(assignments)
+    )
+
+    generics <- strip_names(c(
+      declared_s3_generics(xml),
+      imported_s3_generics(namespace_imports(find_package(source_file$filename)))$fun,
+      .base_s3_generics
+    ))
+    generics <- unique(generics[nzchar(generics)])
+
+    # Remove generic function names from generic implementations
+    # This only lints S3 implementations if the class names are too long, still lints generics if they are too long.
+    nms_stripped <- re_substitutes(nms, rex(start, or(generics), "."), "")
+
+    too_long <- nchar(nms_stripped) > length
+
+    lapply(
+      assignments[too_long],
+      object_lint,
+      source_file,
+      lint_msg
+    )
   })
 }
--- tests/testthat/test-object_length_linter.R
+++ tests/testthat/test-object_length_linter.R
@@ -1,15 +1,37 @@
 test_that("returns the correct linting", {
 
-  expect_lint("blah",
-    NULL,
-    object_length_linter())
+  linter <- object_length_linter()
+  lint_msg <- rex("Variable and function names should not be longer than 30 characters.")
 
-  expect_lint("very_very_very_very_long_variable_names_are_not_ideal <- 1",
-    rex("Variable and function names should not be longer than 30 characters."),
-    object_length_linter())
+  expect_lint("blah", NULL, linter)
 
-  expect_lint("very_very_very_very_long_variable_names_are_not_ideal <<- 'foo'",
+  expect_lint("very_very_very_very_long_variable_names_are_not_ideal <- 1", lint_msg, linter)
+
+  expect_lint(
+    "very_very_very_very_long_variable_names_are_not_ideal <<- 'foo'",
     rex("Variable and function names should not be longer than 40 characters."),
-    object_length_linter(length = 40))
+    object_length_linter(length = 40)
+  )
 
+  # Regression tests for #871
+
+  expect_lint("print.very_very_long_class_name <- 1", NULL, linter)
+  expect_lint("print.very_very_very_very_long_class_name <- 1", lint_msg, linter)
+
+  expect_lint(trim_some("
+    very_very_very_long_generic_name <- function(x, ...) {
+      UseMethod(\"very_very_very_long_generic_name\")
+    }
+
+    very_very_very_long_generic_name.short_class <- function(x, ...) {
+      42L
+    }
+
+    very_very_very_long_generic_name.very_very_very_very_long_class_name <- function(x, ...) {
+      2L
+    }
+  "), list(
+    list(line_number = 1),
+    list(line_number = 9)
+  ), linter)
 })
