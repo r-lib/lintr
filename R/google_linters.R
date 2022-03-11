76f1e692f84399397ef45ea467d96de20998d24e
Michael Chirico <chiricom@google.com>
2022-03-11 21:15:33 +0000

    remove google_linters page

--- R/google_linters.R
+++
@@ -1,9 +0,0 @@
-#' Linters for the Google extension to the tidyverse style guide
-#'
-#' Google uses a fork of the tidyverse style guide, pursuant to which
-#'   its R users have written a bundle of linters, many of which
-#'   are generally useful for writing good R code. These linters
-#'   help encourage code consistency, readability, and best practices.
-#'
-#' @name google-linters
-NULL
