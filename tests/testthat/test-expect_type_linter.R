d303fca8fd721655db1817c6700d310b58f3b4f4
Michael Chirico <chiricom@google.com>
2022-03-12 22:30:26 +0000

    fix linter-as-call issue

--- tests/testthat/test-expect_type_linter.R
+++ tests/testthat/test-expect_type_linter.R
@@ -8,28 +8,28 @@ test_that("expect_type_linter skips allowed usages", {
   expect_lint("expect_true(is.data.frame(x))", NULL, expect_type_linter())
 
   # expect_type(x, ...) cannot be cleanly used here:
-  expect_lint("expect_true(typeof(x) %in% c('builtin', 'closure'))", NULL, expect_type_linter)
+  expect_lint("expect_true(typeof(x) %in% c('builtin', 'closure'))", NULL, expect_type_linter())
 })
 
 test_that("expect_type_linter blocks simple disallowed usages", {
   expect_lint(
     "expect_equal(typeof(x), 'double')",
     rex::rex("expect_type(x, t) is better than expect_equal(typeof(x), t)"),
-    expect_type_linter
+    expect_type_linter()
   )
 
   # expect_identical is treated the same as expect_equal
   expect_lint(
     "testthat::expect_identical(typeof(x), 'language')",
     rex::rex("expect_type(x, t) is better than expect_identical(typeof(x), t)"),
-    expect_type_linter
+    expect_type_linter()
   )
 
   # different equivalent usage
   expect_lint(
     "expect_true(is.complex(foo(x)))",
     rex::rex("expect_type(x, t) is better than expect_true(is.<t>(x))"),
-    expect_type_linter
+    expect_type_linter()
   )
 })
 
@@ -46,7 +46,7 @@ local({
     expect_lint(
       sprintf("expect_true(is.%s(x))", is_type),
       rex::rex("expect_type(x, t) is better than expect_true(is.<t>(x))"),
-      expect_type_linter
+      expect_type_linter()
     ),
     .test_name = is_types,
     is_type = is_types
