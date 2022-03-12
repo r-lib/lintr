f3623eb7bb4931e10c40b82476e3b81a8e4a91b5
Michael Chirico <chiricom@google.com>
2022-03-12 22:00:37 +0000

    tweak tests

--- tests/testthat/test-expect_type_linter.R
+++ tests/testthat/test-expect_type_linter.R
@@ -2,41 +2,33 @@ test_that("expect_type_linter skips allowed usages", {
   # expect_type doesn't have an inverted version
   expect_lint("expect_true(!is.numeric(x))", NULL, expect_type_linter())
   # NB: also applies to tinytest, but it's sufficient to test testthat
-  expect_lint(
-    "testthat::expect_true(!is.numeric(x))",
-    NULL,
-    expect_type_linter
-  )
+  expect_lint("testthat::expect_true(!is.numeric(x))", NULL, expect_type_linter)
 
   # other is.<x> calls are not suitable for expect_type in particular
   expect_lint("expect_true(is.data.frame(x))", NULL, expect_type_linter())
 
   # expect_type(x, ...) cannot be cleanly used here:
-  expect_lint(
-    "expect_true(typeof(x) %in% c('builtin', 'closure'))",
-    NULL,
-    expect_type_linter
-  )
+  expect_lint("expect_true(typeof(x) %in% c('builtin', 'closure'))", NULL, expect_type_linter)
 })
 
 test_that("expect_type_linter blocks simple disallowed usages", {
   expect_lint(
     "expect_equal(typeof(x), 'double')",
-    "expect_type\\(x, t\\) is better than expect_equal\\(typeof\\(x\\), t\\)",
+    rex::rex("expect_type(x, t) is better than expect_equal(typeof(x), t)"),
     expect_type_linter
   )
 
   # expect_identical is treated the same as expect_equal
   expect_lint(
     "testthat::expect_identical(typeof(x), 'language')",
-    "expect_type\\(x, t\\) is better than expect_equal\\(typeof\\(x\\), t\\)",
+    rex::rex("expect_type(x, t) is better than expect_equal(typeof(x), t)"),
     expect_type_linter
   )
 
   # different equivalent usage
   expect_lint(
     "expect_true(is.complex(foo(x)))",
-    "expect_type\\(x, t\\) is better than expect_equal\\(typeof\\(x\\), t\\)",
+    rex::rex("expect_type(x, t) is better than expect_equal(typeof(x), t)"),
     expect_type_linter
   )
 })
@@ -53,7 +45,7 @@ local({
     "expect_type_linter catches expect_true(is.<base type>)",
     expect_lint(
       sprintf("expect_true(is.%s(x))", is_type),
-      "expect_type\\(x, t\\) is better than expect_equal\\(typeof\\(x\\),",
+      rex::rex("expect_type(x, t) is better than expect_equal(typeof(x), t)"),
       expect_type_linter
     ),
     .test_name = is_types,
