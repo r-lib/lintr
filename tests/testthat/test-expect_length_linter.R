878dacfc12a39d3e7a130b0c66fc14084ea3d9d6
Michael Chirico <michaelchirico4@gmail.com>
2022-03-16 14:20:47 -0700

    New expect_length_linter (#950)

---
+++ tests/testthat/test-expect_length_linter.R
@@ -0,0 +1,44 @@
+test_that("expect_length_linter skips allowed usages", {
+  expect_lint("expect_equal(nrow(x), 4L)", NULL, expect_length_linter())
+  # NB: also applies to tinytest, but it's sufficient to test testthat
+  expect_lint("testthat::expect_equal(nrow(x), 4L)", NULL, expect_length_linter())
+
+  # only check the first argument. yoda tests in the second argument will be
+  #   missed, but there are legitimate uses of length() in argument 2
+  expect_lint("expect_equal(nrow(x), length(y))", NULL, expect_length_linter())
+})
+
+test_that("expect_length_linter blocks simple disallowed usages", {
+  expect_lint(
+    "expect_equal(length(x), 2L)",
+    rex::rex("expect_length(x, n) is better than expect_equal(length(x), n)"),
+    expect_length_linter()
+  )
+
+  expect_lint(
+    "testthat::expect_equal(length(DF), length(old))",
+    rex::rex("expect_length(x, n) is better than expect_equal(length(x), n)"),
+    expect_length_linter()
+  )
+
+  # yoda test cases
+  expect_lint(
+    "expect_equal(2, length(x))",
+    rex::rex("expect_length(x, n) is better than expect_equal(length(x), n)"),
+    expect_length_linter()
+  )
+
+  expect_lint(
+    "expect_equal(2L, length(x))",
+    rex::rex("expect_length(x, n) is better than expect_equal(length(x), n)"),
+    expect_length_linter()
+  )
+})
+
+test_that("expect_length_linter blocks expect_identical usage as well", {
+  expect_lint(
+    "expect_identical(length(x), 2L)",
+    rex::rex("expect_length(x, n) is better than expect_identical(length(x), n)"),
+    expect_length_linter()
+  )
+})
