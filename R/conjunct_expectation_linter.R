9a5f0bb647da08352d97f9f2faaa5b642253d236
Michael Chirico <chiricom@google.com>
2022-03-19 19:27:00 +0000

    correct logic error re: expect_false(); NEWS

--- R/conjunct_expectation_linter.R
+++ R/conjunct_expectation_linter.R
@@ -2,7 +2,8 @@
 #'
 #' For readability of test outputs, testing only one thing per call to
 #'   [testthat::expect_true()] is preferable, i.e.,
-#'   `expect_true(A); expect_true(B)` is better than `expect_true(A && B)`.
+#'   `expect_true(A); expect_true(B)` is better than `expect_true(A && B)`, and
+#'   `expect_false(A); expect_false(B)` is better than `expect_false(A || B)`.
 #'
 #' @evalRd rd_tags("expect_true_false_and_condition_linter")
 #' @seealso [linters] for a complete list of linters available in lintr.
@@ -16,26 +17,26 @@ conjunct_expectation_linter <- function() {
 
     xml <- source_file$full_xml_parsed_content
 
-    xpath <- "
-    //expr[SYMBOL_FUNCTION_CALL[text() = 'expect_true' or text() = 'expect_false']]
-    /following-sibling::expr
-    /AND2
-    "
+    xpath <- "//expr[
+      (
+        expr[SYMBOL_FUNCTION_CALL[text() = 'expect_true']]
+        and expr[2][AND2]
+      ) or (
+        expr[SYMBOL_FUNCTION_CALL[text() = 'expect_false']]
+        and expr[2][OR2]
+      )
+    ]"
 
     bad_expr <- xml2::xml_find_all(xml, xpath)
 
-    return(lapply(
-      bad_expr,
-      xml_nodes_to_lint,
-      source_file = source_file,
-      message = paste(
-        "Write multiple && conditions in expect_true()/expect_false() as",
-        "individual unit tests, e.g. expect_true(x && y) becomes",
-        "expect_true(x) and expect_true(y). The latter will produce",
-        "better error messages in the case of failure."
-      ),
-      type = "warning",
-      global = TRUE
-    ))
+    return(lapply(bad_expr, gen_conjunct_expectation_lint, source_file))
   })
+}
+
+gen_conjunct_expectation_lint <- function(expr, source_file) {
+  matched_fun <- xml2::xml_text(xml2::xml_find_first(expr, "expr/SYMBOL_FUNCTION_CALL"))
+  op = if (matched_fun == "expect_true") "&&" else "||"
+  message <- sprintf("Instead of %1$s(A %2$s B), write multiple unit tests like %1$s(A) and %1$s(B)", matched_fun, op)
+  message <- paste(message, "The latter will produce better error messages in the case of failure.")
+  xml_nodes_to_lint(expr, source_file, message, type = "warning", global = TRUE)
 }
--- tests/testthat/test-conjunct_expectation_linter.R
+++ tests/testthat/test-conjunct_expectation_linter.R
@@ -5,20 +5,34 @@ test_that("conjunct_expectation_linter skips allowed usages", {
   # more complicated expression
   expect_lint("expect_true(x || (y && z))", NULL, conjunct_expectation_linter())
   # the same by operator precedence, though not obvious a priori
-  expect_lint("expect_false(x || y && z)", NULL, conjunct_expectation_linter())
+  expect_lint("expect_true(x || y && z)", NULL, conjunct_expectation_linter())
   expect_lint("expect_true(x && y || z)", NULL, conjunct_expectation_linter())
 })
 
-test_that("conjunct_expectation_linter blocks simple disallowed usages", {
+test_that("conjunct_expectation_linter blocks && conditions with expect_true()", {
   expect_lint(
-    "expect_false(x && y)",
-    rex::rex("Write multiple && conditions in expect_true()/expect_false() as"),
+    "expect_true(x && y)",
+    rex::rex("Instead of expect_true(A && B), write multiple unit tests"),
     conjunct_expectation_linter()
   )
 
   expect_lint(
     "expect_true(x && y && z)",
-    rex::rex("Write multiple && conditions in expect_true()/expect_false() as"),
+    rex::rex("Instead of expect_true(A && B), write multiple unit tests"),
+    conjunct_expectation_linter()
+  )
+})
+
+test_that("conjunct_expectation_linter blocks || conditions with expect_false()", {
+  expect_lint(
+    "expect_false(x || y)",
+    rex::rex("Instead of expect_false(A || B), write multiple unit tests"),
+    conjunct_expectation_linter()
+  )
+
+  expect_lint(
+    "expect_false(x || y || z)",
+    rex::rex("Instead of expect_false(A || B), write multiple unit tests"),
     conjunct_expectation_linter()
   )
 })
