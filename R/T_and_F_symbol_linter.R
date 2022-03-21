363b6230e2c0e3823f7c0cb9855b5cf98db4817e
Michael Chirico <chiricom@google.com>
2022-03-21 20:45:27 +0000

    remove extraneous usage

--- R/T_and_F_symbol_linter.R
+++ R/T_and_F_symbol_linter.R
@@ -1,16 +1,17 @@
-#' @include aaa.R
+#' `T` and `F` symbol linter
 #'
-#' @describeIn linters  Avoid the symbols \code{T} and \code{F} (for \code{TRUE} and \code{FALSE}).
+#' Avoid the symbols `T` and `F` (for `TRUE` and `FALSE`).
+#'
+#' @evalRd rd_tags("T_and_F_symbol_linter")
+#' @seealso [linters] for a complete list of linters available in lintr.
 #' @export
 T_and_F_symbol_linter <- function() { # nolint: object_name_linter.
   Linter(function(source_file) {
     if (is.null(source_file$xml_parsed_content)) return(list())
 
-    text_is_t_f <- sprintf("(%s)", xp_text_in_table(c("T", "F")))
-
     xpath <- paste0(
       "//SYMBOL[",
-      text_is_t_f, # T or F symbol
+      "(text() = 'T' or text() = 'F')", # T or F symbol
       " and not(preceding-sibling::OP-DOLLAR)", # not part of a $-subset expression
       " and not(parent::expr[",
       "  following-sibling::LEFT_ASSIGN", # not target of left assignment
@@ -22,7 +23,7 @@ T_and_F_symbol_linter <- function() { # nolint: object_name_linter.
 
     xpath_assignment <- paste0(
       "//SYMBOL[",
-      text_is_t_f, # T or F symbol
+      "(text() = 'T' or text() = 'F')", # T or F symbol
       " and not(preceding-sibling::OP-DOLLAR)", # not part of a $-subset expression
       " and parent::expr[", #, but ...
       "  following-sibling::LEFT_ASSIGN", # target of left assignment
@@ -35,14 +36,15 @@ T_and_F_symbol_linter <- function() { # nolint: object_name_linter.
     bad_exprs <- xml2::xml_find_all(source_file$xml_parsed_content, xpath)
     bad_assigns <- xml2::xml_find_all(source_file$xml_parsed_content, xpath_assignment)
 
+    replacement_map <- c(T = "TRUE", F = "FALSE")
     make_lint <- function(expr, fmt) {
-      symbol <- xml2::xml_text(expr)
-      replacement <- switch(symbol, "T" = "TRUE", "F" = "FALSE")
-      message <- sprintf(fmt, replacement, symbol)
       xml_nodes_to_lint(
         xml = expr,
         source_file = source_file,
-        message = message,
+        lint_message = function(expr) {
+          symbol <- xml2::xml_text(expr)
+          sprintf(fmt, replacement_map[[symbol]], symbol)
+        },
         type = "style",
         offset = 1L
       )
