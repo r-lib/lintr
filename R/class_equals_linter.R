e7980167ba709b26c489941f211a536f14a233bf
Michael Chirico <chiricom@google.com>
2022-03-23 18:27:07 +0000

    New class_equals_linter

---
+++ R/class_equals_linter.R
@@ -0,0 +1,41 @@
+#' Block comparison of class with ==
+#'
+#' Usage like `class(x) == "character"` is prone to error since class in R
+#'   is in general a vector. The correct version for S3 classes is [inherits()]:
+#'   `inherits(x, "character")`. Often, class `k` will have an `is.` equivalent,
+#'   for example [is.character()] or [is.data.frame()].
+#'
+#' Similar reasoning applies for `class(x) %in% "character"`
+#'
+#' @evalRd rd_tags("class_equals_linter")
+#' @seealso [linters] for a complete list of linters available in lintr.
+#' @export
+class_equals_linter <- function() {
+  Linter(function(source_file) {
+    if (length(source_file$parsed_content) == 0L) {
+      return(list())
+    }
+
+    xml <- source_file$xml_parsed_content
+
+    xpath <- "//expr[
+      not(preceding-sibling::OP-LEFT-BRACKET)
+      and expr[expr[SYMBOL_FUNCTION_CALL[text() = 'class']]]
+      and (EQ or NE or SPECIAL[text() = '%in%'])
+    ]"
+
+    bad_expr <- xml2::xml_find_all(xml, xpath)
+
+    return(lapply(
+      bad_expr,
+      xml_nodes_to_lint,
+      source_file = source_file,
+      lint_message = function(expr) {
+        op <- xml2::xml_text(xml2::xml_find_first(expr, "*[2]"))
+        message <- sprintf("Instead of comparing class(x) with %s,", op)
+        paste(message, "use inherits(x, 'class-name') or is.<class> or is(x, 'class')")
+      },
+      type = "warning"
+    ))
+  })
+}
