77f6ef89f1d700915681766abf8a63e2c64d8408
Michael Chirico <chiricom@google.com>
2022-03-25 22:04:27 +0000

    xml_parsed_content

--- R/literal_coercion_linter.R
+++ R/literal_coercion_linter.R
@@ -13,7 +13,7 @@
 #' @export
 literal_coercion_linter <- function() {
   Linter(function(source_file) {
-    if (length(source_file$parsed_content) == 0L) {
+    if (length(source_file$xml_parsed_content) == 0L) {
       return(list())
     }
 
