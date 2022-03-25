f349f35778199de656d20d22b88997085a20e528
Michael Chirico <chiricom@google.com>
2022-03-25 00:12:00 -0700

    wording

--- R/nested_ifelse_linter.R
+++ R/nested_ifelse_linter.R
@@ -37,8 +37,8 @@ nested_ifelse_linter <- function() {
       source_file = source_file,
       lint_message = function(expr) {
         matched_call <- xml2::xml_text(xml2::xml_find_first(expr, "expr/SYMBOL_FUNCTION_CALL"))
-        lint_message <- sprintf("Don't use nested %s() calls; instead, try", matched_call)
-        paste(lint_message, "(1) data.table::fcase; (2) dplyr::case_when; or (3) merging to a lookup table.")
+        lint_message <- sprintf("Don't use nested %s() calls;", matched_call)
+        paste(lint_message, "instead, try (1) data.table::fcase; (2) dplyr::case_when; or (3) using a lookup table.")
       },
       type = "warning"
     ))
