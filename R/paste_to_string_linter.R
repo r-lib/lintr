ab5e80e184eeb2834e2184fff17cf29d07b12548
Michael Chirico <chiricom@google.com>
2022-03-28 21:28:40 +0000

    improve xpath readability

--- R/paste_to_string_linter.R
+++ R/paste_to_string_linter.R
@@ -14,16 +14,14 @@ paste_to_string_linter <- function() {
 
     xml <- source_file$xml_parsed_content
 
-    paste_cond_fmt <- "expr[SYMBOL_FUNCTION_CALL[%s]]"
-    collapse_cond_fmt <- "SYMBOL_SUB[text() = 'collapse']/following-sibling::expr[1][STR_CONST[%s]]"
-
     # 3 expr: the function call, the argument, and collapse=
     # TODO(michaelchirico): catch raw-string equivalents
-    xpath <- sprintf(
-      "//expr[count(expr) = 3 and %s and %s]",
-      sprintf(collapse_cond_fmt, xp_text_in_table(c("', '", '", "'))),
-      sprintf(paste_cond_fmt, xp_text_in_table(c("paste", "paste0")))
-    )
+    seps <- c("', '", '", "')
+    xpath <- glue::glue("//expr[
+      expr[SYMBOL_FUNCTION_CALL[text() = 'paste' or text() = 'paste0']]
+      and count(expr) = 3
+      and SYMBOL_SUB[text() = 'collapse']/following-sibling::expr[1][STR_CONST[ {xp_text_in_table(seps)} ]]
+    ]")
     bad_expr <- xml2::xml_find_all(xml, xpath)
 
     return(lapply(
