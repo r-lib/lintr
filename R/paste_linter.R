7dd6dff8a743b9fd1d7cce53a2c7eb48fdef2661
Michael Chirico <michaelchirico4@gmail.com>
2022-04-02 06:13:33 -0700

    Merge paste_sep_linter and paste_to_string_linter into paste_linter (#1031)
    * rename paste_sep_linter to paste_linter
    
    * ingest paste_to_string_linter to new paste_linter
    
    * update linter DB, roxygenize
    
    * merge NEWS
    
    * fix tag in doc
    
    * document parameter
    
    * merge + roxy
    
    Co-authored-by: AshesITR <alexander.rosenstock@web.de>

---
+++ R/paste_linter.R
@@ -0,0 +1,74 @@
+#' Raise lints for several common poor usages of paste()
+#'
+#' The following issues are linted by default by this linter
+#'   (and each can be turned off optionally):
+#'
+#'  1. Block usage of [paste()] with `sep = ""`. [paste0()] is a faster, more concise alternative.
+#'  2. Block usage of `paste()` or `paste0()` with `collapse = ", "`. [toString()] is a direct
+#'     wrapper for this, and alternatives like [glue::glue_collapse()] might give better messages for humans.
+#'
+#' @evalRd rd_tags("paste_linter")
+#' @param allow_empty_sep Logical, default `FALSE`. If `TRUE`, usage of
+#'   `paste()` with `sep = ""` is not linted.
+#' @param allow_to_string Logical, default `FALSE`. If `TRUE`, usage of
+#'   `paste()` and `paste0()` with `collapse = ", "` is not linted.
+#' @seealso [linters] for a complete list of linters available in lintr.
+#' @export
+paste_linter <- function(allow_empty_sep = FALSE, allow_to_string = FALSE) {
+  Linter(function(source_file) {
+    if (length(source_file$xml_parsed_content) == 0L) {
+      return(list())
+    }
+
+    xml <- source_file$xml_parsed_content
+
+    if (allow_empty_sep) {
+      empty_sep_lints <- NULL
+    } else {
+      # NB: string-length()=2 means '' or "" (*not* 'ab' or 'cd' which have length 4)
+      # TODO: adapt this for R>4.0 raw strings
+      empty_sep_xpath <- "//expr[
+        expr[SYMBOL_FUNCTION_CALL[text() = 'paste']]
+        and SYMBOL_SUB[text() = 'sep']/following-sibling::expr[1][STR_CONST[string-length(text()) = 2]]
+      ]"
+
+      empty_sep_expr <- xml2::xml_find_all(xml, empty_sep_xpath)
+
+      empty_sep_lints <- lapply(
+        empty_sep_expr,
+        xml_nodes_to_lint,
+        source_file = source_file,
+        lint_message = 'paste0(...) is better than paste(..., sep = "").',
+        type = "warning"
+      )
+    }
+
+    if (allow_to_string) {
+      to_string_lints <- NULL
+    } else {
+      # 3 expr: the function call, the argument, and collapse=
+      # TODO(michaelchirico): catch raw-string equivalents
+      seps <- c("', '", '", "')
+      to_string_xpath <- glue::glue("//expr[
+      expr[SYMBOL_FUNCTION_CALL[text() = 'paste' or text() = 'paste0']]
+        and count(expr) = 3
+        and SYMBOL_SUB[text() = 'collapse']/following-sibling::expr[1][STR_CONST[ {xp_text_in_table(seps)} ]]
+      ]")
+      to_string_expr <- xml2::xml_find_all(xml, to_string_xpath)
+
+      to_string_lints <- lapply(
+        to_string_expr,
+        xml_nodes_to_lint,
+        source_file = source_file,
+        lint_message = paste(
+          'toString(.) is more expressive than paste(., collapse = ", ").',
+          "Note also glue::glue_collapse() and and::and()",
+          "for constructing human-readable / translation-friendly lists"
+        ),
+        type = "warning"
+      )
+    }
+
+    c(empty_sep_lints, to_string_lints)
+  })
+}
