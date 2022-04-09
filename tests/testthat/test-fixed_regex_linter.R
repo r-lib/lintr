0943393ffa409dac3fb3f01b7b94c0d436030ddb
Michael Chirico <michaelchirico4@gmail.com>
2022-04-09 11:49:56 -0700

    copy tests over from R branch

--- tests/testthat/test-fixed_regex_linter.R
+++ tests/testthat/test-fixed_regex_linter.R
@@ -14,6 +14,7 @@ test_that("fixed_regex_linter skips allowed usages", {
   expect_lint("grep('0+1', x, perl = TRUE)", NULL, fixed_regex_linter())
   expect_lint("grep('1*2', x)", NULL, fixed_regex_linter())
   expect_lint("grep('a|b', x)", NULL, fixed_regex_linter())
+  expect_lint("grep('\\\\[|\\\\]', x)", NULL, fixed_regex_linter())
 
   # if fixed=TRUE is already set, regex patterns don't matter
   expect_lint("gsub('\\\\.', '', y, fixed = TRUE)", NULL, fixed_regex_linter())
@@ -35,7 +36,6 @@ test_that("fixed_regex_linter blocks simple disallowed usages", {
     fixed_regex_linter()
   )
 
-
   expect_lint(
     "gregexpr('a-z', y)",
     rex::rex("For static regular expression patterns, set `fixed = TRUE`."),
@@ -50,6 +50,13 @@ test_that("fixed_regex_linter blocks simple disallowed usages", {
 
   expect_lint(
     "grep('\n', x)",
+    rex::rex("For static regular expression patterns, set `fixed = TRUE`."),
+    fixed_regex_linter()
+  )
+
+  # naming the argument doesn't matter (if it's still used positionally)
+  expect_lint(
+    "gregexpr(pattern = 'a-z', y)",
     rex::rex("For static regular expression patterns, set `fixed = TRUE`."),
     fixed_regex_linter()
   )
@@ -69,7 +76,7 @@ test_that("fixed_regex_linter catches regex like [.] or [$]", {
   )
 })
 
-test_that("fixed_regex_linter catches calls to strsplit as well", {
+test_that("fixed_regex_linter catches null calls to strsplit as well", {
   expect_lint("strsplit(x, '^x')", NULL, fixed_regex_linter())
   expect_lint("tstrsplit(x, '[a-zA-Z]')", NULL, fixed_regex_linter())
   expect_lint("tstrsplit(x, fmt)", NULL, fixed_regex_linter())
@@ -81,6 +88,7 @@ test_that("fixed_regex_linter catches calls to strsplit as well", {
 
   # if fixed=TRUE is already set, regex patterns don't matter
   expect_lint("strsplit(x, '\\\\.', fixed = TRUE)", NULL, fixed_regex_linter())
+  expect_lint("strsplit(x, '\\\\.', fixed = T)", NULL, fixed_regex_linter())
 })
 
 test_that("fixed_regex_linter catches calls to strsplit as well", {
@@ -121,14 +129,14 @@ test_that("fixed_regex_linter skips allowed usages", {
   expect_lint("str_extract_all(y, '\\\\s')", NULL, fixed_regex_linter())
   expect_lint("str_which(x, '1*2')", NULL, fixed_regex_linter())
 
-  # if fixed=TRUE is already set, regex patterns don't matter
+  # if fixed() is already set, regex patterns don't matter
   expect_lint("str_replace(y, fixed('\\\\.'), '')", NULL, fixed_regex_linter())
 
   # namespace qualification doesn't matter
   expect_lint("stringr::str_replace(y, stringr::fixed('abcdefg'), '')", NULL, fixed_regex_linter())
 })
 
-test_that("fixed_regex_linter blocks simple disallowed usages", {
+test_that("fixed_regex_linter blocks simple disallowed usages of stringr functions", {
   expect_lint(
     "str_replace_all(x, '\\\\.', '')",
     rex::rex("For static regular expression patterns, set `fixed = TRUE`."),
@@ -155,6 +163,19 @@ test_that("fixed_regex_linter blocks simple disallowed usages", {
 
   expect_lint(
     "str_which(x, '\n')",
+    rex::rex("For static regular expression patterns, set `fixed = TRUE`."),
+    fixed_regex_linter()
+  )
+
+  # named, positional arguments are still caught
+  expect_lint(
+    "str_locate(y, pattern = 'a-z')",
+    rex::rex("For static regular expression patterns, set `fixed = TRUE`."),
+    fixed_regex_linter()
+  )
+  # nor do other named arguments throw things off
+  expect_lint(
+    "str_starts(x, '\\\\.', negate = TRUE)",
     rex::rex("For static regular expression patterns, set `fixed = TRUE`."),
     fixed_regex_linter()
   )
@@ -185,6 +206,11 @@ test_that("str_replace_all's multi-replacement version is handled", {
   #   a pipeline where each step does one of the replacements and fixed() is used, this is overkill.
   #   Instead, ensure that no lint is returned for this case
   expect_lint('str_replace_all(x, c("one" = "1", "two" = "2", "three" = "3"))', NULL, fixed_regex_linter())
+  expect_lint('grepl(c("a" = "b"), x)', NULL, fixed_regex_linter())
+})
+
+test_that("1- or 2-width octal escape sequences are handled", {
+  expect_lint('strsplit(x, "\\1")', rex::rex("For static regular expression patterns, set `fixed = TRUE`."), fixed_regex_linter())
 })
 
 # TODO(michaelchirico): one difference for stringr functions vs. base is that
