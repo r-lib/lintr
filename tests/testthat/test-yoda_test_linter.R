964a88b359dfd6292f37d34ff1caf9d45a18c394
Michael Chirico <michaelchirico4@gmail.com>
2022-04-12 15:19:43 -0700

    customize yoda_test_linter message for placeholder tests

--- tests/testthat/test-yoda_test_linter.R
+++ tests/testthat/test-yoda_test_linter.R
@@ -43,6 +43,14 @@ test_that("yoda_test_linter ignores usage in pipelines", {
   expect_lint("bar() |> expect_equal('a')", NULL, yoda_test_linter())
 })
 
+test_that("yoda_test_linter throws a special message for placeholder tests", {
+  expect_lint(
+    "expect_equal(1, 1)",
+    rex::rex("Avoid storing placeholder tests like expect_equal(1, 1)"),
+    yoda_test_linter()
+  )
+})
+
 # TODO(michaelchirico): Should this be extended to RUnit tests? It seems yes,
 #   but the argument names in RUnit (inherited from base all.equal()) are a bit
 #   confusing, e.g. `checkEqual(target=, current=)`. From the name, one might
