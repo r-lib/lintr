fc6cb377839ffb6862601e72c0e1b86f7ef00015
Michael Chirico <michaelchirico4@gmail.com>
2022-04-16 08:36:49 -0700

    unreachable_code_linter() doesnt lint after process$stop() (#1060)
    Co-authored-by: AshesITR <alexander.rosenstock@web.de>

--- R/unreachable_code_linter.R
+++ R/unreachable_code_linter.R
@@ -20,13 +20,14 @@ unreachable_code_linter <- function() {
     #  - * returns all children, including the terminal }, so the position
     #    is not last(), but last()-1. If there's no }, this linter doesn't apply.
     #    this is also why we need /* and not /expr -- position() must include all nodes
+    #  - use not(OP-DOLLAR) to prevent matching process$stop(), #1051
     #  - land on the culprit expression
     xpath <- "
     //FUNCTION
     /following-sibling::expr
     /*[
       self::expr
-      and expr[SYMBOL_FUNCTION_CALL[text() = 'return' or text() = 'stop']]
+      and expr[not(OP-DOLLAR) and SYMBOL_FUNCTION_CALL[text() = 'return' or text() = 'stop']]
       and (position() != last() - 1 or not(following-sibling::OP-RIGHT-BRACE))
       and @line2 < following-sibling::*[1]/@line2
     ]
