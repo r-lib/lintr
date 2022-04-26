85fd37503dbf2d98ef369590cb9c95d5becc640f
AshesITR <alexander.rosenstock@web.de>
2022-04-26 21:24:32 +0200

    delete function_brace_linter and merge it into brace_linter (#1094)
    * delete function_brace_linter and merge it into brace_linter
    
    * delete if_else_match_braces_linter and merge it into brace_linter (#1095)
    
    * delete if_else_match_braces_linter and merge it into brace_linter
    
    * deprecate open_curly_linter and merge it into brace_linter (#1096)
    
    * deprecate open_curly_linter
    
     - remove open_curly_linter from defaults
     - refactor to XPath based approach
     - no longer lint trailing whitespace (there's a separate linter for that)
    
    * merge paren_brace_linter into brace_linter (#1097)
    
    * deprecate paren_brace_linter
    
     - remove paren_brace_linter from defaults
     - extend to else{ and repeat{
    
    * `code`
    
    Co-authored-by: Michael Chirico <michaelchirico4@gmail.com>
    
    * add explicit test for different behaviour compared to closed_curly_linter
    
    Co-authored-by: Michael Chirico <michaelchirico4@gmail.com>
    
    Co-authored-by: Michael Chirico <michaelchirico4@gmail.com>
    
    Co-authored-by: Michael Chirico <michaelchirico4@gmail.com>

--- R/brace_linter.R
+++ R/brace_linter.R
@@ -2,13 +2,20 @@
 #'
 #' Perform various style checks related to placement and spacing of curly braces:
 #'
-#'  - Curly braces are on their own line unless they are followed by an `else`.
+#'  - Opening curly braces are never on their own line and are always followed by a newline.
+#'  - Opening curly braces have a space before them.
+#'  - Closing curly braces are on their own line unless they are followed by an `else`.
 #'  - Closing curly braces in `if` conditions are on the same line as the corresponding `else`.
+#'  - Either both or neither branch in `if`/`else` use curly braces, i.e., either both branches use `{...}` or neither
+#'    does.
+#'  - Functions spanning multiple lines use curly braces.
 #'
 #' @param allow_single_line if `TRUE`, allow an open and closed curly pair on the same line.
 #'
 #' @evalRd rd_tags("brace_linter")
-#' @seealso [linters] for a complete list of linters available in lintr.
+#' @seealso [linters] for a complete list of linters available in lintr. \cr
+#'   <https://style.tidyverse.org/syntax.html#indenting> \cr
+#'   <https://style.tidyverse.org/syntax.html#if-statements>
 #' @export
 brace_linter <- function(allow_single_line = FALSE) {
   Linter(function(source_expression) {
@@ -17,6 +24,51 @@ brace_linter <- function(allow_single_line = FALSE) {
     }
 
     lints <- list()
+
+    xp_cond_open <- xp_and(c(
+      # matching } is on same line
+      if (isTRUE(allow_single_line)) {
+        "(@line1 != following-sibling::OP-LEFT-BRACE/@line1)"
+      },
+      # double curly
+      "not(
+        (@line1 = parent::expr/preceding-sibling::OP-LEFT-BRACE/@line1) or
+        (@line1 = following-sibling::expr/OP-LEFT-BRACE/@line1)
+      )"
+    ))
+
+    # TODO (AshesITR): if c_style_braces is TRUE, invert the preceding-sibling condition
+    xp_open_curly <- glue::glue("//OP-LEFT-BRACE[
+      { xp_cond_open } and (
+        not(@line1 = parent::expr/preceding-sibling::*/@line2) or
+        @line1 = following-sibling::*[1][not(self::COMMENT)]/@line1
+      )
+    ]")
+
+    lints <- c(lints, lapply(
+      xml2::xml_find_all(source_expression$xml_parsed_content, xp_open_curly),
+      xml_nodes_to_lint,
+      source_file = source_expression,
+      lint_message = paste(
+        "Opening curly braces should never go on their own line and",
+        "should always be followed by a new line."
+      )
+    ))
+
+    xp_open_preceding <- "parent::expr/preceding-sibling::*[1][self::OP-RIGHT-PAREN or self::ELSE or self::REPEAT]"
+
+    xp_paren_brace <- glue::glue("//OP-LEFT-BRACE[
+      @line1 = { xp_open_preceding }/@line1
+      and
+      @col1 = { xp_open_preceding }/@col2 + 1
+    ]")
+
+    lints <- c(lints, lapply(
+      xml2::xml_find_all(source_expression$xml_parsed_content, xp_paren_brace),
+      xml_nodes_to_lint,
+      source_file = source_expression,
+      lint_message = "There should be a space before an opening curly brace."
+    ))
 
     xp_cond_closed <- xp_and(c(
       # matching { is on same line
@@ -65,6 +117,41 @@ brace_linter <- function(allow_single_line = FALSE) {
       xml_nodes_to_lint,
       source_file = source_expression,
       lint_message = "`else` should come on the same line as the previous `}`."
+    ))
+
+    xp_function_brace <- "//expr[FUNCTION and @line1 != @line2 and not(expr[OP-LEFT-BRACE])]"
+
+    lints <- c(lints, lapply(
+      xml2::xml_find_all(source_expression$xml_parsed_content, xp_function_brace),
+      xml_nodes_to_lint,
+      source_file = source_expression,
+      lint_message = "Any function spanning multiple lines should use curly braces."
+    ))
+
+    # if (x) { ... } else if (y) { ... } else { ... } is OK; fully exact pairing
+    #   of if/else would require this to be
+    #   if (x) { ... } else { if (y) { ... } else { ... } } since there's no
+    #   elif operator/token in R, which is pretty unseemly
+    xp_if_else_match_brace <- "
+    //IF[
+      following-sibling::expr[2][OP-LEFT-BRACE]
+      and following-sibling::ELSE
+          /following-sibling::expr[1][not(OP-LEFT-BRACE or IF/following-sibling::expr[2][OP-LEFT-BRACE])]
+    ]
+
+    |
+
+    //ELSE[
+      following-sibling::expr[1][OP-LEFT-BRACE]
+      and preceding-sibling::IF/following-sibling::expr[2][not(OP-LEFT-BRACE)]
+    ]
+    "
+
+    lints <- c(lints, lapply(
+      xml2::xml_find_all(source_expression$xml_parsed_content, xp_if_else_match_brace),
+      xml_nodes_to_lint,
+      source_file = source_expression,
+      lint_message = "Either both or neither branch in `if`/`else` should use curly braces."
     ))
 
     lints
--- tests/testthat/test-brace_linter.R
+++ tests/testthat/test-brace_linter.R
@@ -1,4 +1,7 @@
-test_that("brace_linter lints closed braces correctly", {
+test_that("brace_linter lints braces correctly", {
+  open_curly_msg <- rex::rex(
+    "Opening curly braces should never go on their own line and should always be followed by a new line."
+  )
   closed_curly_msg <- rex::rex(paste(
     "Closing curly-braces should always be on their own line,",
     "unless they are followed by an else."
@@ -7,8 +10,9 @@ test_that("brace_linter lints closed braces correctly", {
   linter <- brace_linter()
   expect_lint("blah", NULL, linter)
   expect_lint("a <- function() {\n}", NULL, linter)
+  expect_lint("a <- function() {  \n}", NULL, linter)
 
-  expect_lint("a <- function() { 1 }", closed_curly_msg, linter)
+  expect_lint("a <- function() { 1 }", list(open_curly_msg, closed_curly_msg), linter)
   # allowed by allow_single_line
   expect_lint("a <- function() { 1 }", NULL, brace_linter(allow_single_line = TRUE))
 
@@ -47,9 +51,9 @@ test_that("brace_linter lints closed braces correctly", {
   )
 
   # }) is allowed
-  expect_lint("eval(bquote({...}))", NULL, linter)
+  expect_lint("eval(bquote({\n...\n}))", NULL, linter)
   # }] is too
-  expect_lint("df[, {...}]", NULL, linter)
+  expect_lint("df[, {\n...\n}]", NULL, linter)
 
   # }, is allowed
   expect_lint(
@@ -79,8 +83,97 @@ test_that("brace_linter lints closed braces correctly", {
     linter
   )
 
-  # }} is allowed
+  # {{ }} is allowed
   expect_lint("{{ x }}", NULL, linter)
+
+  expect_lint(
+    trim_some("
+      pkg_name <- function(path = find_package()) {
+        if (is.null(path)) {
+          return(NULL)
+        } else {
+          read.dcf(file.path(path, \"DESCRIPTION\"), fields = \"Package\")[1]
+        }
+      }
+    "),
+    NULL,
+    linter
+  )
+
+  expect_lint("a <- function()\n{\n  1 \n}", open_curly_msg, linter)
+  expect_lint("a <- function()\n    {\n  1 \n}", open_curly_msg, linter)
+  expect_lint("a <- function()\n\t{\n  1 \n}", open_curly_msg, linter)
+
+  # trailing comments are allowed
+  expect_lint(
+    trim_some('
+      if ("P" != "NP") { # what most people expect
+        print("Cryptomania is possible")
+      }
+    '),
+    NULL,
+    linter
+  )
+})
+
+test_that("brace_linter lints spaces before open braces", {
+  linter <- brace_linter()
+  msg <- rex::rex("There should be a space before an opening curly brace.")
+
+  expect_lint(
+    "blah <- function(){\n}",
+    list(
+      message = msg,
+      column_number = 19L
+    ),
+    linter
+  )
+
+  expect_lint(
+    "\nblah <- function(){\n\n\n}",
+    list(
+      message = msg,
+      column_number = 19L
+    ),
+    linter
+  )
+
+  # should also lint if/else
+  expect_lint(
+    "a <- if (a){\n} else{\n}",
+    list(
+      list(message = msg, line_number = 1L, column_number = 12L),
+      list(message = msg, line_number = 2L, column_number = 7L)
+    ),
+    linter
+  )
+
+  # should lint repeat{
+  expect_lint(
+    "repeat{\nblah\n}",
+    list(message = msg, line_number = 1L, column_number = 7L),
+    linter
+  )
+
+  # should ignore strings and comments, as in regexes:
+  expect_lint("grepl('(iss){2}', 'Mississippi')", NULL, linter)
+  expect_lint(
+    "x <- 123 # dont flag (paren){brace} if inside a comment",
+    NULL,
+    linter
+  )
+  # should not be thrown when the brace lies on subsequent line
+  expect_lint(
+    trim_some("
+      x <- function()
+                     {2}
+    "),
+    list(
+      rex::rex("Opening curly braces should never go on their own line and should always be followed by a new line."),
+      rex::rex("Closing curly-braces should always be on their own line, unless they are followed by an else.")
+    ), #, but not msg
+    linter
+  )
 })
 
 test_that("brace_linter lints else correctly", {
@@ -125,3 +218,88 @@ test_that("brace_linter lints else correctly", {
   )
 })
 
+test_that("brace_linter lints function expressions correctly", {
+  linter <- brace_linter()
+  expect_lint("function(x) 4", NULL, linter)
+
+  lines <- trim_some("
+    function(x) {
+      x + 4
+    }
+  ")
+  expect_lint(lines, NULL, linter)
+
+  lines <- trim_some("
+    function(x)
+      x+4
+  ")
+  expect_lint(
+    lines,
+    rex::rex("Any function spanning multiple lines should use curly braces."),
+    linter
+  )
+})
+
+test_that("brace_linter lints if/else matching braces correctly", {
+  linter <- brace_linter()
+  expect_lint("if (TRUE) 1 else 2", NULL, linter)
+  expect_lint("if (TRUE) 1", NULL, linter)
+
+  lines_brace <- trim_some("
+    if (TRUE) {
+      1
+    } else {
+      2
+    }
+  ")
+  expect_lint(lines_brace, NULL, linter)
+
+  # such usage is also not allowed by the style guide, but test anyway
+  lines_unbrace <- trim_some("
+    foo <- function(x) {
+      if (TRUE)
+        1
+      else
+        2
+    }
+  ")
+  expect_lint(lines_unbrace, NULL, linter)
+
+  # else if is OK
+  lines_else_if <- trim_some("
+    if (x) {
+     1
+    } else if (y) {
+     2
+    } else {
+     3
+    }
+  ")
+  expect_lint(lines_else_if, NULL, linter)
+
+  lines_if <- trim_some("
+    foo <- function(x) {
+      if (x) {
+        1
+      } else 2
+    }
+  ")
+  expect_lint(
+    lines_if,
+    rex::rex("Either both or neither branch in `if`/`else` should use curly braces."),
+    linter
+  )
+
+  lines_else <- trim_some("
+    foo <- function(x) {
+      if (x) 1 else {
+        2
+      }
+    }
+  ")
+  expect_lint(
+    lines_else,
+    rex::rex("Either both or neither branch in `if`/`else` should use curly braces."),
+    linter
+  )
+})
