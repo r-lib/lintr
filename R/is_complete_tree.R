4ae3c632088746cad0e0c53ec8eacd6fb7eee4b6
Michael Chirico <michaelchirico4@gmail.com>
2022-04-28 17:03:18 -0700

    is_lint_level

--- R/is_complete_tree.R
+++ R/is_complete_tree.R
@@ -1,9 +1,10 @@
-#' Does this object contain the full file tree?
+#' Is this an expression- or a file-level source object?
 #'
-#' Helper for determining whether the current expression contains the full file tree
+#' Helper for determining whether the current `source_expression` contains
+#'   the full file tree, or is just a single expression.
 #'
 #' @export
-is_tree_level <- function(source_file, level, require_xml = FALSE) {
+is_lint_level <- function(source_file, level, require_xml = FALSE) {
   level <- match.arg(level, level = c("expression", "complete"))
   required_key <- paste0(if (level == "complete") "full_", if (require_xml), "xml_", "parsed_content")
   !is.null(source_file[[required_key]])
