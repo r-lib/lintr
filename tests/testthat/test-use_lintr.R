f5a45605233780235d3526d0ec6bc71c4471cf7d
Alexander Rosenstock <alexander.rosenstock@web.de>
2022-05-02 19:45:52 +0200

    test type = "full" as well

--- tests/testthat/test-use_lintr.R
+++ tests/testthat/test-use_lintr.R
@@ -5,8 +5,17 @@ test_that("use_lintr works as expected", {
 
   lintr_file <- use_lintr(path = tmp)
   expect_true(file.exists(lintr_file))
+
+  # can't generate if a .lintr already exists
   expect_error(use_lintr(path = tmp), "already exists")
-  expect_silent(read.dcf(lintr_file))
+
+  # read_settings() works with the generated file
+  expect_silent(read_settings(tmp))
+  read_settings(NULL)
+
+  file.remove(lintr_file)
+  # type = "full" also works with read_settings()
+  use_lintr(path = tmp, type = "full")
   expect_silent(read_settings(tmp))
   read_settings(NULL)
 
