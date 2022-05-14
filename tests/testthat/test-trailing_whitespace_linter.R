535e9e7d4bdb04b363ab704b51e0cd156a3f7e13
AshesITR <alexander.rosenstock@web.de>
2022-05-15 01:12:38 +0200

    Ignore trailing whitespace in strings by default (#1119)
    * Ignore trailing whitespace in strings by default
    
    * improve tests
    
    * document()
    
    * fix implicit integer lints

--- tests/testthat/test-trailing_whitespace_linter.R
+++ tests/testthat/test-trailing_whitespace_linter.R
@@ -5,7 +5,7 @@ test_that("returns the correct linting", {
 
   expect_lint(
     "blah <- 1  ",
-    list(message = rex("Trailing whitespace is superfluous."), column_number = 10),
+    list(message = rex("Trailing whitespace is superfluous."), column_number = 10L),
     linter
   )
 
@@ -17,7 +17,7 @@ test_that("returns the correct linting", {
 
   expect_lint(
     "blah <- 1\n'hi'\na <- 2  ",
-    list(message = rex("Trailing whitespace is superfluous."), line_number = 3),
+    list(message = rex("Trailing whitespace is superfluous."), line_number = 3L),
     linter
   )
 })
@@ -27,13 +27,13 @@ test_that("also handles completely empty lines per allow_empty_lines argument", 
 
   expect_lint(
     "blah <- 1\n  \n'hi'\na <- 2",
-    list(message = rex("Trailing whitespace is superfluous."), line_number = 2),
+    list(message = rex("Trailing whitespace is superfluous."), line_number = 2L),
     linter
   )
 
   expect_lint(
     "blah <- 1  ",
-    list(message = rex("Trailing whitespace is superfluous."), column_number = 10),
+    list(message = rex("Trailing whitespace is superfluous."), column_number = 10L),
     trailing_whitespace_linter(allow_empty_lines = TRUE)
   )
 
@@ -41,5 +41,32 @@ test_that("also handles completely empty lines per allow_empty_lines argument", 
     "blah <- 1\n  \n'hi'\na <- 2",
     NULL,
     trailing_whitespace_linter(allow_empty_lines = TRUE)
+  )
+})
+
+test_that("also handles trailing whitespace in string constants", {
+  linter <- trailing_whitespace_linter()
+  msg <- rex::rex("Trailing whitespace is superfluous.")
+
+  expect_lint("blah <- '  \n  \n'", NULL, linter)
+  # Don't exclude past the end of string
+  expect_lint(
+    "blah <- '  \n  \n'  ",
+    list(message = msg, line_number = 3L),
+    linter
+  )
+  # can be enabled with allow_in_strings = FALSE
+  expect_lint(
+    "blah <- '  \n  \n'",
+    list(message = msg, line_number = 1L),
+    trailing_whitespace_linter(allow_empty_lines = TRUE, allow_in_strings = FALSE)
+  )
+  expect_lint(
+    "blah <- '  \n  \n'",
+    list(
+      list(message = msg, line_number = 1L),
+      list(message = msg, line_number = 2L)
+    ),
+    trailing_whitespace_linter(allow_in_strings = FALSE)
   )
 })
