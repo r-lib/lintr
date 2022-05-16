2031c55e2df0c719db0771773a3a1bd3f5a7ae3c
Michael Chirico <michaelchirico4@gmail.com>
2022-05-15 23:54:11 -0700

    amend test of earlier private version

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,11 +1,8 @@
-# TODO: delete this test when we have any lints using xml_nodes_to_lint where the
-#   target node can span >1 line (e.g. an <expr>); until then, this test is here
-#   for coverage
 test_that("xml_nodes_to_lint handles >1-line nodes correctly", {
   xml <- xml2::read_xml('<x line1="1" col1="1" line2="2" col2="10" start="8" end="25"></x>')
   source_expression <- list(filename = "test", lines = c(`1` = "abcdefg", `2` = "hijklmnopq"))
   expect_equal(
-    lintr:::xml_nodes_to_lint(xml, source_expression, "foo"),
+    xml_nodes_to_lint(xml, source_expression, "foo"),
     Lint(
       filename = "test",
       line_number = 1L, column_number = 1L,
