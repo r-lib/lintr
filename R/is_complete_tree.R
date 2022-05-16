33781ca6fe260478e90314eb0a0100a3f77b4d63
Michael Chirico <michaelchirico4@gmail.com>
2022-05-15 20:19:33 -0700

    design alignment

--- R/is_complete_tree.R
+++ R/is_complete_tree.R
@@ -3,9 +3,19 @@
 #' Helper for determining whether the current `source_expression` contains
 #'   the full file tree, or is just a single expression.
 #'
+#' @param source_expresssion A parsed expression object, i.e., an element
+#'   of the object returned by [get_source_expressions()].
+#' @param level Which level of expression is being tested? `"expression"`
+#'   means an individual expression, while `"file"` means the full
+#'   source tree for this file.
+#' @param require_xml If `TRUE`, the XML equivalent of the parse tree
+#'   is required. Use this whenever writing a linter based on logic
+#'   expressed via Xpath.
+#'
 #' @export
-is_lint_level <- function(source_file, level, require_xml = FALSE) {
-  level <- match.arg(level, level = c("expression", "complete"))
+is_lint_level <- function(source_expression, level = c("expression", "file"), require_xml = FALSE) {
+  stopifnot(!missing(level))
+  level <- match.arg(level)
   required_key <- paste0(if (level == "complete") "full_", if (require_xml), "xml_", "parsed_content")
-  !is.null(source_file[[required_key]])
+  !is.null(source_expression[[required_key]])
 }
