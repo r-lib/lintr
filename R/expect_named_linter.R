c65c81ad2c64ef8308b34f8851f5423d41798f3a
Michael Chirico <chiricom@google.com>
2022-05-20 23:42:27 +0000

    wrong depth

--- R/expect_named_linter.R
+++ R/expect_named_linter.R
@@ -28,7 +28,7 @@ expect_named_linter <- function() {
       bad_expr,
       source_expression = source_expression,
       lint_message = function(expr) {
-        matched_function <- xp_call_name(expr)
+        matched_function <- xp_call_name(expr, depth = 0L)
         sprintf("expect_named(x, n) is better than %s(names(x), n)", matched_function)
       },
       type = "warning"
