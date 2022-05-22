4d6a6e6b2bb5fa112e89f5944804f36b30b16bdc
Michael Chirico <chiricom@google.com>
2022-05-22 05:47:47 +0000

    convert undesirable_operator_linter to xpath

--- R/undesirable_operator_linter.R
+++ R/undesirable_operator_linter.R
@@ -19,33 +19,42 @@ op_types <- c(
 #' @seealso [linters] for a complete list of linters available in lintr.
 #' @export
 undesirable_operator_linter <- function(op = default_undesirable_operators) {
-  Linter(function(source_expression) {
-    lapply(
-      ids_with_token(source_expression, op_types, fun = `%in%`),
-      function(id) {
-        token <- with_id(source_expression, id)
-        op_name <- token[["text"]]
-        if (op_name %in% names(op)) {
-          line_num <- token[["line1"]]
-          start_col_num <- token[["col1"]]
-          end_col_num <- token[["col2"]]
-          msg <- sprintf("Operator `%s` is undesirable.", op_name)
-          alt_op <- op[[op_name]]
-          if (!is.na(alt_op)) {
-            msg <- paste(msg, alt_op)
-          }
+  undesirable_operator_metadata <-
+    merge(infix_metadata, infix_overload, by = "xml_tag", all.x = TRUE)
 
-          Lint(
-            filename = source_expression[["filename"]],
-            line_number = line_num,
-            column_number = start_col_num,
-            type = "warning",
-            message = paste0(msg, collapse = " "),
-            line = source_expression[["lines"]][[as.character(line_num)]],
-            ranges = list(c(start_col_num, end_col_num))
-          )
+  included_operators <- undesirable_operator_metadata$string_value %in% names(op) |
+    undesirable_operator_metadata$exact_string_value %in% names(op)
+  operator_nodes <- undesirable_operator_metadata$xml_tag[included_operators]
+  needs_exact_string <- !is.na(undesirable_operator_metadata$exact_string_value[included_operators])
+  operator_nodes[needs_exact_string] <- sprintf(
+    "%s[text() = '%s']",
+    operator_nodes[needs_exact_string],
+    undesirable_operator_metadata$exact_string_value[included_operators][needs_exact_string]
+  )
+
+  Linter(function(source_expression) {
+    if (!is_lint_level(source_expression, "expression")) {
+      return(list())
+    }
+
+    xml <- source_expression$xml_parsed_content
+
+    xpath <- glue::glue("//*[ { paste0('self::', operator_nodes, collapse = ' or ') } ]")
+    bad_op <- xml2::xml_find_all(xml, xpath)
+
+    xml_nodes_to_lints(
+      bad_op,
+      source_expression = source_expression,
+      lint_message = function(expr) {
+        op_name <- xml2::xml_text(expr)
+        msg <- sprintf("Operator `%s` is undesirable.", op_name)
+        alt_op <- op[[op_name]]
+        if (!is.na(alt_op)) {
+          msg <- paste(msg, alt_op)
         }
-      }
+        msg
+      },
+      type = "warning"
     )
   })
 }
