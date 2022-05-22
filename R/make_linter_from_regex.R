a21ea0b454022b040e06261ee191d15d84a7aebb
Michael Chirico <chiricom@google.com>
2022-05-22 04:59:58 +0000

    flatten_lints() version

--- R/make_linter_from_regex.R
+++ R/make_linter_from_regex.R
@@ -11,6 +11,10 @@ make_linter_from_regex <- function(regex,
 
   function() {
     Linter(function(source_expression) {
+      if (!is_lint_level(source_expression, "expression")) {
+        return(list())
+      }
+
       all_matches <- re_matches(
         source_expression[["lines"]],
         regex,
@@ -20,7 +24,8 @@ make_linter_from_regex <- function(regex,
 
       line_numbers <- as.integer(names(source_expression[["lines"]]))
 
-      Map(
+      # flatten_lints() to ensure this returns list() when there's no match
+      flatten_lints(Map(
         function(line_matches, line_number) {
           lapply(
             split(line_matches, seq_len(nrow(line_matches))),
@@ -45,7 +50,7 @@ make_linter_from_regex <- function(regex,
         },
         all_matches,
         line_numbers
-      )
+      ))
     })
   }
 }
