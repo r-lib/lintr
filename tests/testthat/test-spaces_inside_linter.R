57fd7f444211e59ac6f2e1c5336fd025c3289f12
Michael Chirico <chiricom@google.com>
2022-05-23 23:19:51 -0700

    refactor spaces_inside_linter to use xpath (#1169)
    * refactor spaces_inside_linter to use xpath
    
    * improve location of lints
    
    * fix tests
    
    Co-authored-by: AshesITR <alexander.rosenstock@web.de>

--- tests/testthat/test-spaces_inside_linter.R
+++ tests/testthat/test-spaces_inside_linter.R
@@ -1,31 +1,31 @@
 test_that("returns the correct linting", {
   linter <- spaces_inside_linter()
-  msg <- rex::rex("Do not place spaces around code in parentheses or square brackets.")
 
   expect_lint("blah", NULL, linter)
   expect_lint("print(blah)", NULL, linter)
   expect_lint("base::print(blah)", NULL, linter)
-  expect_lint("a[, ]", NULL, linter)
   expect_lint("a[, ]", NULL, linter)
   expect_lint("a[1]", NULL, linter)
   expect_lint("fun(\na[1]\n  )", NULL, linter)
 
   expect_lint(
     "a[1 ]",
-    list(message = msg, line_number = 1L, column_number = 4L, type = "style"),
+    list(message = "Do not place spaces before square brackets", line_number = 1L, column_number = 5L, type = "style"),
     linter
   )
 
   expect_lint(
     "\n\na[ 1]",
-    list(message = msg, line_number = 3L, column_number = 3L, type = "style"),
+    list(message = "Do not place spaces after square brackets", line_number = 3L, column_number = 3L, type = "style"),
     linter
   )
 
   expect_lint(
     "a[ 1 ]",
-    list(list(message = msg, line_number = 1L, column_number = 3L, type = "style"),
-         list(message = msg, line_number = 1L, column_number = 5L, type = "style")),
+    list(
+      list(message = "Do not place spaces after square brackets", line_number = 1L, column_number = 3L, type = "style"),
+      list(message = "Do not place spaces before square brackets", line_number = 1L, column_number = 6L, type = "style")
+    ),
     linter
   )
 
@@ -37,32 +37,46 @@ test_that("returns the correct linting", {
 
   expect_lint(
     "a(1 )",
-    list(message = msg, line_number = 1L, column_number = 4L, type = "style"),
+    list(message = "Do not place spaces before parentheses", line_number = 1L, column_number = 5L, type = "style"),
     linter
   )
 
   expect_lint(
     "a( 1)",
-    list(message = msg, line_number = 1L, column_number = 3L, type = "style"),
+    list(message = "Do not place spaces after parentheses", line_number = 1L, column_number = 3L, type = "style"),
     linter
   )
 
   expect_lint(
     "a( 1 )",
-    list(list(message = msg, line_number = 1L, column_number = 3L, type = "style"),
-         list(message = msg, line_number = 1L, column_number = 5L, type = "style")),
+    list(
+      list(message = "Do not place spaces after parentheses", line_number = 1L, column_number = 3L, type = "style"),
+      list(message = "Do not place spaces before parentheses", line_number = 1L, column_number = 6L, type = "style")
+    ),
     linter
   )
 
-  expect_lint("\"a( 1 )\"", NULL, linter)
+  expect_lint('"a( 1 )"', NULL, linter)
 
   # trailing comments are OK (#636)
-  expect_lint("or( #code\n  x, y\n)", NULL, linter)
+  expect_lint(
+    trim_some("
+      or( #code
+        x, y
+      )
+    "),
+    NULL,
+    linter
+  )
 
-  expect_lint(trim_some("
-    fun(      # this is another comment
-      a = 42, # because 42 is always the answer
-      b = Inf
-    )
-  "), NULL, linter)
+  expect_lint(
+    trim_some("
+      fun(      # this is another comment
+        a = 42, # because 42 is always the answer
+        b = Inf
+      )
+    "),
+    NULL,
+    linter
+  )
 })
