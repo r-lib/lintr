6a0023aa7f4f220af92302b7e1545a24ef560cf7
Michael Chirico <michaelchirico4@gmail.com>
2022-05-23 21:43:54 -0700

    improve location of lints

--- R/spaces_inside_linter.R
+++ R/spaces_inside_linter.R
@@ -10,6 +10,20 @@
 #'   <https://style.tidyverse.org/syntax.html#parentheses>
 #' @export
 spaces_inside_linter <- function() {
+  left_xpath_condition <- "
+    not(following-sibling::*[1][self::COMMENT])
+    and @end != following-sibling::*[1]/@start - 1
+    and @line1 = following-sibling::*[1]/@line2
+  "
+  left_xpath <- glue::glue("//OP-LEFT-BRACKET[{left_xpath_condition}] | //OP-LEFT-PAREN[{left_xpath_condition}]")
+
+  right_xpath_condition <- "
+    not(preceding-sibling::*[1][self::OP-COMMA])
+    and @start != preceding-sibling::*[1]/@end + 1
+    and @line1 = preceding-sibling::*[1]/@line2
+  "
+  right_xpath <- glue::glue("//OP-RIGHT-BRACKET[{right_xpath_condition}] | //OP-RIGHT-PAREN[{right_xpath_condition}]")
+
   Linter(function(source_expression) {
     if (!is_lint_level(source_expression, "file")) {
       return(list())
@@ -17,25 +31,35 @@ spaces_inside_linter <- function() {
 
     xml <- source_expression$full_xml_parsed_content
 
-    xpath <- "//*[
-      (
-        (self::OP-RIGHT-BRACKET or self::OP-RIGHT-PAREN)
-        and not(preceding-sibling::*[1][self::OP-COMMA])
-        and @start != preceding-sibling::*[1]/@end + 1
-        and @line1 = preceding-sibling::*[1]/@line2
-      ) or (
-        (self::OP-LEFT-BRACKET or self::OP-LEFT-PAREN)
-        and not(following-sibling::*[1][self::COMMENT])
-        and @end != following-sibling::*[1]/@start - 1
-        and @line1 = following-sibling::*[1]/@line2
-      )
-    ]"
-    bad_expr <- xml2::xml_find_all(xml, xpath)
-    xml_nodes_to_lints(
-      bad_expr,
+    left_expr <- xml2::xml_find_all(xml, left_xpath)
+    left_lints <- xml_nodes_to_lints(
+      left_expr,
       source_expression = source_expression,
-      lint_message = "Do not place spaces around code in parentheses or square brackets.",
+      lint_message = function(expr) {
+        switch(
+          xml2::xml_text(expr),
+          `[` = "Do not place spaces after square brackets.",
+          `(` = "Do not place spaces after parentheses."
+        )
+      },
+      type = "style",
+      match_after_end = TRUE
+    )
+
+    right_expr <- xml2::xml_find_all(xml, right_xpath)
+    right_lints <- xml_nodes_to_lints(
+      right_expr,
+      source_expression = source_expression,
+      lint_message = function(expr) {
+        switch(
+          xml2::xml_text(expr),
+          `]` = "Do not place spaces before square brackets.",
+          `)` = "Do not place spaces before parentheses."
+        )
+      },
       type = "style"
     )
+
+    c(left_lints, right_lints)
   })
 }
