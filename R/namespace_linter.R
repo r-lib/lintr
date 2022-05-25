c4fce7798002d1c4b2e4b257e6ef0c942bf1ec29
Michael Chirico <chiricom@google.com>
2022-05-25 12:00:48 -0700

    use helpers in namespace_linter (#1222)
    * use helpers in namespace_linter
    
    * more holistic refactor -- proceed in chunks
    
    * delete helpers, use xml_nodes_to_lints
    
    * rename vestigial variable
    
    * clarifying comments, some tidying
    
    * fix vestigial variable
    
    Co-authored-by: AshesITR <alexander.rosenstock@web.de>

--- R/namespace_linter.R
+++ R/namespace_linter.R
@@ -17,113 +17,151 @@ namespace_linter <- function(check_exports = TRUE, check_nonexports = TRUE) {
 
     xml <- source_expression$full_xml_parsed_content
 
-    ns_nodes <- xml2::xml_find_all(xml, "//expr/*[self::NS_GET or self::NS_GET_INT]")
+    ns_nodes <- xml2::xml_find_all(xml, "//NS_GET | //NS_GET_INT")
 
-    get_node_text <- function(nodes) {
-      text <- xml2::xml_text(nodes)
-      expr <- parse(text = text, keep.source = FALSE)
-      vapply(expr, as.character, character(1L))
+    if (length(ns_nodes) == 0L) {
+      return(list())
     }
 
-    pkg_nodes <- xml2::xml_find_all(ns_nodes, "preceding-sibling::*[1]")
-    sym_nodes <- xml2::xml_find_all(ns_nodes, "following-sibling::*[1]")
-    pkgs <- get_node_text(pkg_nodes)
-    ops <- xml2::xml_text(ns_nodes)
-    syms <- get_node_text(sym_nodes)
+    ## Case 1: pkg is uninstalled in pkg::foo
+
+    package_nodes <- xml2::xml_find_all(ns_nodes, "preceding-sibling::*[1]")
+    packages <- get_r_string(package_nodes)
+
+    lints <- list()
 
     installed_packages <- .packages(all.available = TRUE)
+    installed <- packages %in% installed_packages
 
-    results <- lapply(seq_along(ns_nodes), function(i) {
-      if (pkgs[[i]] %in% installed_packages) {
-        if (check_exports || check_nonexports) {
-          ns <- tryCatch(getNamespace(pkgs[[i]]), error = function(e) e)
+    if (!all(installed)) {
+      lints <- c(lints, xml_nodes_to_lints(
+        package_nodes[!installed],
+        source_expression = source_expression,
+        lint_message = sprintf("Package '%s' is not installed.", packages[!installed]),
+        type = "warning"
+      ))
 
-          if (isNamespace(ns)) {
-            if (check_exports && ops[[i]] == "::") {
-              exports <- getNamespaceExports(ns)
-              lazydata <- names(.getNamespaceInfo(ns, "lazydata"))
-              if (!(syms[[i]] %in% c(exports, lazydata))) {
-                line1 <- as.integer(xml2::xml_attr(sym_nodes[[i]], "line1"))
-                col1 <- as.integer(xml2::xml_attr(sym_nodes[[i]], "col1"))
-                col2 <- as.integer(xml2::xml_attr(sym_nodes[[i]], "col2"))
-                return(Lint(
-                  filename = source_expression$filename,
-                  line_number = line1,
-                  column_number = col1,
-                  type = "warning",
-                  message = sprintf("'%s' is not exported from {%s}.", syms[[i]], pkgs[[i]]),
-                  line = source_expression$file_lines[[line1]],
-                  ranges = list(c(col1, col2))
-                ))
-              }
-            }
+      ns_nodes <- ns_nodes[installed]
+      packages <- packages[installed]
+      package_nodes <- package_nodes[installed]
+    }
 
-            if (check_nonexports && ops[[i]] == ":::") {
-              if (exists(syms[[i]], ns, inherits = FALSE)) {
-                exports <- getNamespaceExports(ns)
-                if (syms[[i]] %in% exports) {
-                  line1 <- as.integer(xml2::xml_attr(sym_nodes[[i]], "line1"))
-                  col1 <- as.integer(xml2::xml_attr(sym_nodes[[i]], "col1"))
-                  col2 <- as.integer(xml2::xml_attr(sym_nodes[[i]], "col2"))
-                  return(Lint(
-                    filename = source_expression$filename,
-                    line_number = line1,
-                    column_number = col1,
-                    type = "style",
-                    message = sprintf("'%s' is exported from {%s}. Use %s::%s instead.",
-                      syms[[i]], pkgs[[i]], pkgs[[i]], syms[[i]]),
-                    line = source_expression$file_lines[[line1]],
-                    ranges = list(c(col1, col2))
-                  ))
-                }
-              } else {
-                line1 <- as.integer(xml2::xml_attr(sym_nodes[[i]], "line1"))
-                col1 <- as.integer(xml2::xml_attr(sym_nodes[[i]], "col1"))
-                col2 <- as.integer(xml2::xml_attr(sym_nodes[[i]], "col2"))
-                return(Lint(
-                  filename = source_expression$filename,
-                  line_number = line1,
-                  column_number = col1,
-                  type = "warning",
-                  message = sprintf("'%s' does not exist in {%s}.", syms[[i]], pkgs[[i]]),
-                  line = source_expression$file_lines[[line1]],
-                  ranges = list(c(col1, col2))
-                ))
-              }
-            }
-          } else {
-            # nocov start: need getNamespace to fail. Could be done with {mockery} in principle
-            line1 <- as.integer(xml2::xml_attr(pkg_nodes[[i]], "line1"))
-            col1 <- as.integer(xml2::xml_attr(pkg_nodes[[i]], "col1"))
-            col2 <- as.integer(xml2::xml_attr(pkg_nodes[[i]], "col2"))
-            return(Lint(
-              filename = source_expression$filename,
-              line_number = line1,
-              column_number = col1,
-              type = "warning",
-              message = conditionMessage(ns),
-              line = source_expression$file_lines[[line1]],
-              ranges = list(c(col1, col2))
-            ))
-            # nocov end
-          }
-        }
-      } else {
-        line1 <- as.integer(xml2::xml_attr(pkg_nodes[[i]], "line1"))
-        col1 <- as.integer(xml2::xml_attr(pkg_nodes[[i]], "col1"))
-        col2 <- as.integer(xml2::xml_attr(pkg_nodes[[i]], "col2"))
-        return(Lint(
-          filename = source_expression$filename,
-          line_number = line1,
-          column_number = col1,
-          type = "warning",
-          message = sprintf("Package '%s' is not installed.", pkgs[[i]]),
-          line = source_expression$file_lines[[line1]],
-          ranges = list(c(col1, col2))
-        ))
-      }
-    })
+    if (!check_exports && !check_nonexports) {
+      return(lints)
+    }
 
-    results[!vapply(results, is.null, logical(1L))]
+    ## Case 2 (rare?): pkg namespace is broken in pkg::foo
+
+    namespaces <- lapply(packages, function(package) tryCatch(getNamespace(package), error = identity))
+    failed_namespace <- vapply(namespaces, inherits, "condition", FUN.VALUE = logical(1L))
+
+    if (any(failed_namespace)) {
+      lints <- c(lints, xml_nodes_to_lints(
+        package_nodes[failed_namespace],
+        source_expression = source_expression,
+        lint_message = vapply(namespaces[failed_namespace], conditionMessage, character(1L)),
+        type = "warning"
+      ))
+
+      ns_nodes <- ns_nodes[!failed_namespace]
+      packages <- packages[!failed_namespace]
+      namespaces <- namespaces[!failed_namespace]
+    }
+
+    ## Case 3/4/5: problems with foo in pkg::foo / pkg:::foo
+
+    ns_get <- xml2::xml_text(ns_nodes) == "::"
+    symbol_nodes <- xml2::xml_find_all(ns_nodes, "following-sibling::*[1]")
+    symbols <- get_r_string(symbol_nodes)
+
+    if (check_nonexports) {
+      lints <- c(lints, build_ns_get_int_lints(
+        packages[!ns_get],
+        symbols[!ns_get],
+        symbol_nodes[!ns_get],
+        namespaces[!ns_get],
+        source_expression
+      ))
+    }
+
+    if (check_exports) {
+      lints <- c(lints, build_ns_get_lints(
+        packages[ns_get],
+        symbols[ns_get],
+        symbol_nodes[ns_get],
+        namespaces[ns_get],
+        source_expression
+      ))
+    }
+
+    lints
   })
+}
+
+get_all_exports <- function(namespace) {
+  c(getNamespaceExports(namespace), names(.getNamespaceInfo(namespace, "lazydata")))
+}
+
+build_ns_get_int_lints <- function(packages, symbols, symbol_nodes, namespaces, source_expression) {
+  lints <- list()
+
+  ## Case 3: foo does not exist in pkg:::foo
+
+  non_symbols <- !vapply(
+    seq_along(symbols),
+    function(ii) symbols[[ii]] %in% ls(namespaces[[ii]], all = TRUE),
+    logical(1L)
+  )
+  if (any(non_symbols)) {
+    lints <- c(lints, xml_nodes_to_lints(
+      symbol_nodes[non_symbols],
+      source_expression = source_expression,
+      lint_message = sprintf("'%s' does not exist in {%s}.", symbols[non_symbols], packages[non_symbols]),
+      type = "warning"
+    ))
+
+    packages <- packages[!non_symbols]
+    symbols <- symbols[!non_symbols]
+    symbol_nodes <- symbol_nodes[!non_symbols]
+  }
+
+  ## Case 4: foo is already exported pkg:::foo
+
+  exported <- vapply(
+    seq_along(symbols),
+    function(ii) symbols[[ii]] %in% get_all_exports(namespaces[[ii]]),
+    logical(1L)
+  )
+  if (any(exported)) {
+    lints <- c(lints, xml_nodes_to_lints(
+      symbol_nodes[exported],
+      source_expression = source_expression,
+      lint_message =
+        sprintf("'%1$s' is exported from {%2$s}. Use %2$s::%1$s instead.", symbols[exported], packages[exported]),
+      type = "warning"
+    ))
+  }
+
+  lints
+}
+
+build_ns_get_lints <- function(packages, symbols, symbol_nodes, namespaces, source_expression) {
+  lints <- list()
+
+  ## Case 5: foo is not an export in pkg::foo
+
+  unexported <- !vapply(
+    seq_along(symbols),
+    function(ii) symbols[[ii]] %in% get_all_exports(namespaces[[ii]]),
+    logical(1L)
+  )
+  if (any(unexported)) {
+    lints <- c(lints, xml_nodes_to_lints(
+      symbol_nodes[unexported],
+      source_expression = source_expression,
+      lint_message = sprintf("'%s' is not exported from {%s}.", symbols[unexported], packages[unexported]),
+      type = "warning"
+    ))
+  }
+  lints
 }
