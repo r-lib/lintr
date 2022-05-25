efd37575fa365c577c11a7d499965ce0a43ce344
Alexander Rosenstock <alexander.rosenstock@web.de>
2022-05-25 22:10:55 +0200

    refactor spaces_inside_linter() to use xml_nodes_to_lints()

--- R/spaces_inside_linter.R
+++ R/spaces_inside_linter.R
@@ -35,42 +35,37 @@ spaces_inside_linter <- function() {
     xml <- source_expression$full_xml_parsed_content
 
     left_expr <- xml2::xml_find_all(xml, left_xpath)
-    left_lints <- lapply(left_expr, function(expr) {
-      line1 <- as.integer(xml2::xml_attr(expr, "line1"))
-      col1 <- as.integer(xml2::xml_attr(expr, "col1")) + 1L
-      col2 <- as.integer(xml2::xml_find_num(expr, "number(./following-sibling::*[1]/@col1)")) - 1L
-      Lint(
-        filename = source_expression$filename,
-        line_number = line1,
-        column_number = col1,
-        message = switch(
+
+    left_lints <- xml_nodes_to_lints(
+      left_expr,
+      source_expression = source_expression,
+      lint_message = function(expr) {
+        switch(
           xml2::xml_text(expr),
           `[` = "Do not place spaces after square brackets.",
           `(` = "Do not place spaces after parentheses."
-        ),
-        line = source_expression$file_lines[[line1]],
-        ranges = list(c(col1, col2))
-      )
-    })
+        )
+      },
+      column_number_xpath = "number(./@col1 + 1)",
+      range_start_xpath = "number(./@col1 + 1)",
+      range_end_xpath = "number(./following-sibling::*[1]/@col1 - 1)"
+    )
 
     right_expr <- xml2::xml_find_all(xml, right_xpath)
-    right_lints <- lapply(right_expr, function(expr) {
-      line1 <- as.integer(xml2::xml_attr(expr, "line1"))
-      col1 <- as.integer(xml2::xml_attr(expr, "col2")) + 1L
-      col2 <- as.integer(xml2::xml_find_num(expr, "number(./following-sibling::*[1]/@col1)")) - 1L
-      Lint(
-        filename = source_expression$filename,
-        line_number = line1,
-        column_number = col1,
-        message = switch(
+    right_lints <- xml_nodes_to_lints(
+      right_expr,
+      source_expression = source_expression,
+      lint_message = function(expr) {
+        switch(
           xml2::xml_find_chr(expr, "string(./following-sibling::*[1])"),
           `]` = "Do not place spaces before square brackets.",
           `)` = "Do not place spaces before parentheses."
-        ),
-        line = source_expression$file_lines[[line1]],
-        ranges = list(c(col1, col2))
-      )
-    })
+        )
+      },
+      column_number_xpath = "number(./@col2 + 1)",
+      range_start_xpath = "number(./@col2 + 1)",
+      range_end_xpath = "number(./following-sibling::*[1]/@col1 - 1)"
+    )
 
     c(left_lints, right_lints)
   })
