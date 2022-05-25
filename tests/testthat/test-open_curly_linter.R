620dc9db6ea9baaac9319f916bbf0f140f62d417
Michael Chirico <michaelchirico4@gmail.com>
2022-05-24 18:31:44 -0700

    simplify open_curly_linter using xpath

--- tests/testthat/test-open_curly_linter.R
+++ tests/testthat/test-open_curly_linter.R
@@ -1,6 +1,4 @@
 test_that("returns the correct linting", {
-  msg <- rex("Opening curly braces should never go on their own line and should always be followed by a new line.")
-
   expect_warning(
     linter <- open_curly_linter(),
     "Linter open_curly_linter was deprecated",
@@ -9,48 +7,96 @@ test_that("returns the correct linting", {
 
   expect_lint("blah", NULL, linter)
 
-  expect_lint("a <- function() {\n}", NULL, linter)
-
   expect_lint(
-    "pkg_name <- function(path = find_package()) {
-      if (is.null(path)) {
-        return(NULL)
-      } else {
-        read.dcf(file.path(path, \"DESCRIPTION\"), fields = \"Package\")[1]
+    trim_some("
+      a <- function() {
       }
-    }", NULL, linter)
-
-  expect_lint("a <- function()\n{\n  1 \n}",
-              msg,
-              linter)
-
-  expect_lint("a <- function()\n    {\n  1 \n}",
-              msg,
-              linter)
-
-  expect_lint("a <- function()\n\t{\n  1 \n}",
-              msg,
-              linter)
-
-  expect_lint("a <- function() {  \n}",
-              msg,
-              linter)
-
-  expect_lint("a <- function() { 1 }",
-              msg,
-              linter)
-
-  expect_lint("a <- function() { 1 }",
+    "),
     NULL,
-    suppressWarnings(open_curly_linter(allow_single_line = TRUE)))
+    linter
+  )
 
   expect_lint(
-'if ("P" != "NP") { # what most people expect
-    print("Cryptomania is possible")
-}',
+    trim_some('
+      pkg_name <- function(path = find_package()) {
+        if (is.null(path)) {
+          return(NULL)
+        } else {
+          read.dcf(file.path(path, "DESCRIPTION"), fields = "Package")[1]
+        }
+      }
+    '),
     NULL,
-linter
+    linter
+  )
+
+  expect_lint(
+    trim_some("
+      a <- function()
+      {
+        1
+      }
+    "),
+    rex::rex("Opening curly braces should never go on their own line."),
+    linter
+  )
+
+  expect_lint(
+    trim_some("
+      a <- function()
+          {
+        1
+      }
+    "),
+    rex::rex("Opening curly braces should never go on their own line."),
+    linter
+  )
+
+  expect_lint(
+    trim_some("
+      a <- function()
+      \t{
+        1
+      }
+    "),
+    rex::rex("Opening curly braces should never go on their own line"),
+    linter
+  )
+
+  expect_lint(
+    "a <- function() { 1 }",
+    rex::rex("Opening curly braces should always be followed by a new line"),
+    linter
+  )
+
+  expect_lint(
+    trim_some('
+      if ("P" != "NP") { # what most people expect
+        print("Cryptomania is possible")
+      }
+    '),
+    NULL,
+    linter
   )
 
   expect_lint("{{x}}", NULL, linter)
+})
+
+test_that("allow_single_line=TRUE works", {
+  expect_warning(
+    linter <- open_curly_linter(allow_single_line = TRUE),
+    "Linter open_curly_linter was deprecated",
+    fixed = TRUE
+  )
+
+  expect_lint("a <- function() { 1 }", NULL, linter)
+
+  expect_lint(
+    trim_some("
+      a <- function() { 1
+        2 }
+    "),
+    rex::rex("Opening curly braces should always be followed by a new line unless"),
+    linter
+  )
 })
