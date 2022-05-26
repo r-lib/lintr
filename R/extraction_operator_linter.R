fd3b613aab412defebcd1a5f7743bf98db5ffc28
Alexander Rosenstock <alexander.rosenstock@web.de>
2022-05-26 12:15:21 +0200

    feedback

--- R/extraction_operator_linter.R
+++ R/extraction_operator_linter.R
@@ -7,11 +7,13 @@
 #' @seealso [linters] for a complete list of linters available in lintr.
 #' @export
 extraction_operator_linter <- function() {
+  constant_nodes_in_brackets <- paste0("self::", c("expr", "OP-PLUS", "NUM_CONST", "STR_CONST", "NULL_CONST"))
   xpath <- "
     //OP-DOLLAR[not(preceding-sibling::expr[1]/SYMBOL[text() = 'self' or text() = '.self'])] |
-    //OP-LEFT-BRACKET[not(following-sibling::expr[1]/descendant::*[not(
-      self::expr or self::OP-PLUS or self::NUM_CONST or self::STR_CONST or self::NULL_CONST
-    )]) and not(following-sibling::OP-COMMA)]
+    //OP-LEFT-BRACKET[
+      not(following-sibling::expr[1]/descendant::*[not({xp_or(constant_nodes_in_brackets)})]) and
+      not(following-sibling::OP-COMMA)
+    ]
   "
 
   Linter(function(source_expression) {
