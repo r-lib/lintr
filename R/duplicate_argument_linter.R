3d60306fc2e22f12af0e80536c4f4680ab317a6f
Alexander Rosenstock <alexander.rosenstock@web.de>
2022-05-26 14:07:18 +0200

    refactor duplicate_argument_linter() to use xml_nodes_to_lints()
    fixes #1275

--- R/duplicate_argument_linter.R
+++ R/duplicate_argument_linter.R
@@ -7,6 +7,9 @@
 #' @seealso [linters] for a complete list of linters available in lintr.
 #' @export
 duplicate_argument_linter <- function(except = character()) {
+  xpath_call_with_args <- "//expr[EQ_SUB]"
+  xpath_arg_name <- "./EQ_SUB/preceding-sibling::*[1]"
+
   Linter(function(source_expression) {
 
     if (!is_lint_level(source_expression, "file")) {
@@ -15,41 +18,24 @@ duplicate_argument_linter <- function(except = character()) {
 
     xml <- source_expression$full_xml_parsed_content
 
-    xpath <- "//expr[EQ_SUB]"
-
-    calls <- xml2::xml_find_all(xml, xpath)
+    calls <- xml2::xml_find_all(xml, xpath_call_with_args)
 
     if (length(except)) {
-      calls_text <- get_r_string(calls, xpath = "expr[1][count(*)=1]/*[1]")
+      calls_text <- get_r_string(xp_call_name(calls))
       calls <- calls[!(calls_text %in% except)]
     }
 
-    # TODO(michaelchirico): Use xml_nodes_to_lints(), and better vectorize
-    result <- lapply(calls, function(call) {
-      args <- xml2::xml_find_all(call, "EQ_SUB/preceding-sibling::*[1]")
-      args_text <- get_r_string(args)
-      is_dup_arg <- duplicated(args_text)
-      dup_args <- args[is_dup_arg]
-      if (length(dup_args)) {
-        line1 <- as.integer(xml2::xml_attr(dup_args, "line1"))
-        line2 <- as.integer(xml2::xml_attr(dup_args, "line2"))
-        col1 <- as.integer(xml2::xml_attr(dup_args, "col1"))
-        col2 <- as.integer(xml2::xml_attr(dup_args, "col2"))
-        col2[line2 > line1] <- nchar(source_expression$file_lines[line1[line2 > line1]])
-        lapply(seq_along(dup_args), function(i) {
-          Lint(
-            filename = source_expression$filename,
-            line_number = line1[[i]],
-            column_number = col1[[i]],
-            type = "warning",
-            message = "Duplicate arguments in function call.",
-            line = source_expression$file_lines[line1[[i]]],
-            ranges = list(c(col1[[i]], col2[[i]]))
-          )
-        })
-      }
+    all_arg_nodes <- lapply(calls, function(call_node) {
+      xml2::xml_find_all(call_node, xpath_arg_name)
     })
+    arg_names <- lapply(all_arg_nodes, get_r_string)
+    is_duplicated <- lapply(arg_names, duplicated)
 
-    unlist(result, recursive = FALSE, use.names = FALSE)
+    xml_nodes_to_lints(
+      unlist(all_arg_nodes, recursive = FALSE)[unlist(is_duplicated)],
+      source_expression = source_expression,
+      lint_message = "Duplicate arguments in function call.",
+      type = "warning"
+    )
   })
 }
