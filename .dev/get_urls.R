9f3fbbc0c3205b00bd2fea54df53ecd9486893cc
Michael Chirico <chiricom@google.com>
2022-05-29 23:21:34 +0000

    <- for assignment

--- .dev/get_urls.R
+++ .dev/get_urls.R
@@ -2,34 +2,34 @@
 # Script to get URL keys from CRAN
 #   of packags that Suggest or Import lintr
 
-cran_db = as.data.frame(available.packages())
-lintr_re = "\\blintr\\b"
-lintr_pkg = cran_db[
+cran_db <- as.data.frame(available.packages())
+lintr_re <- "\\blintr\\b"
+lintr_pkg <- cran_db[
   with(cran_db, grepl(lintr_re, Suggests) | grepl(lintr_re, Imports)),
   "Package"
 ]
 
 # simple retry mechanism
-desc_url_fmt = "https://cran.r-project.org/web/packages/%s/DESCRIPTION"
-extract_url = function(pkg, initial_sleep = 1, retry_sleep = 60) {
-  desc_url = sprintf(desc_url_fmt, pkg)
-  retry = function(cond) {
+desc_url_fmt <- "https://cran.r-project.org/web/packages/%s/DESCRIPTION"
+extract_url <- function(pkg, initial_sleep = 1, retry_sleep = 60) {
+  desc_url <- sprintf(desc_url_fmt, pkg)
+  retry <- function(cond) {
     message("Sleepy... 😴")
     Sys.sleep(retry_sleep)
     url(desc_url, open = "r")
   }
   Sys.sleep(initial_sleep)
-  url_conn = tryCatch(url(desc_url, open = "r"), error = retry, warning = retry)
-  pkg_data = read.dcf(url_conn, "URL")
+  url_conn <- tryCatch(url(desc_url, open = "r"), error = retry, warning = retry)
+  pkg_data <- read.dcf(url_conn, "URL")
   drop(pkg_data)
 }
 
-extract_github_repo = function(urls) {
-  matches = regexpr("https?://git(hub|lab).com/[a-zA-Z0-9_-]+/[a-zA-Z0-9._-]+", urls)
-  matched = !is.na(matches) & matches > 0L
+extract_github_repo <- function(urls) {
+  matches <- regexpr("https?://git(hub|lab).com/[a-zA-Z0-9_-]+/[a-zA-Z0-9._-]+", urls)
+  matched <- !is.na(matches) & matches > 0L
 
-  out = character(length(urls))
-  out[matched] = substr(
+  out <- character(length(urls))
+  out[matched] <- substr(
     urls[matched],
     matches[matched],
     matches[matched] + attr(matches, "match.length")[matched] - 1L
@@ -37,13 +37,13 @@ extract_github_repo = function(urls) {
   out
 }
 
-urls = character(length(lintr_pkg))
+urls <- character(length(lintr_pkg))
 # for loop makes debugging easier
 for (ii in seq_along(urls)) {
   urls[ii] <- extract_url(lintr_pkg[ii])
 }
 
-git_urls = extract_github_repo(urls)
+git_urls <- extract_github_repo(urls)
 unmatched <- is.na(git_urls)
 
 writeLines(lintr_pkg[unmatched], "reverse-imports-no-repos")
