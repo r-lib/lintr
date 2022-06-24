479f3235d6e2f30de4dd9ff3d0044fe9f885fbb1
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-06-24 09:04:16 +0200

    Update test-ci.R

--- tests/testthat/test-ci.R
+++ tests/testthat/test-ci.R
@@ -26,14 +26,16 @@ test_that("GitHub Actions functionality works in a subdirectory", {
   )
 })
 
-test_that("GitHub Actions functionality works", {
+test_that("GitHub Actions - linting on error works", {
   # imitate being on GHA whether or not we are
   withr::local_envvar(list(GITHUB_ACTIONS = "true", LINTR_ERROR_ON_LINT = "true"))
   withr::local_options(lintr.rstudio_source_markers = FALSE)
   tmp <- withr::local_tempfile(lines = "x <- 1:nrow(y)")
 
-  mockery::stub(lint, "quit", function() "bye bye")
-  expect_output(lint(tmp), "bye bye", fixed = TRUE)
+  l <- lint(tmp)
+
+  mockery::stub(print.lints, "base::quit", function(...) { cat("Tried to quit.\n") })
+  expect_output(print(l), "::warning file", fixed = TRUE)
 })
 
 test_that("Printing works for Travis", {
