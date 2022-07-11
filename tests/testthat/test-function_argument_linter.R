a3716fce6027c31714fb1e4522753ed4bb18f43a
Alexander Rosenstock <alexander.rosenstock@web.de>
2022-07-11 14:52:23 +0200

    add test for required argument after ...

--- tests/testthat/test-function_argument_linter.R
+++ tests/testthat/test-function_argument_linter.R
@@ -6,6 +6,9 @@ test_that("function_argument_linter returns the correct linting", {
   expect_lint("function(x, y = 1, z, w = 2) {}", list(message = msg, column_number = 20L), linter)
   expect_lint("function(x, y = 1, z = 2) {}", NULL, linter)
   expect_lint("function(x, y, z = 1) {}", NULL, linter)
+
+  # ... handled correctly
+  expect_lint("function(x, y, z = 1, ..., w) {}", list(message = msg, column_number = 28L), linter)
   expect_lint("function(x, y, z = 1, ...) {}", NULL, linter)
   expect_lint("function(x, y, ..., z = 1) {}", NULL, linter)
 
