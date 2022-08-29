866a34aab29776a396676db42aae86e3db4d44bb
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-08-29 14:00:42 +0200

    DRY in tests for `consecutive_stopifnot_linter()`

--- tests/testthat/test-consecutive_stopifnot_linter.R
+++ tests/testthat/test-consecutive_stopifnot_linter.R
@@ -1,9 +1,12 @@
 test_that("consecutive_stopifnot_linter skips allowed usages", {
-  expect_lint("stopifnot(x)", NULL, consecutive_stopifnot_linter())
-  expect_lint("stopifnot(x, y, z)", NULL, consecutive_stopifnot_linter())
+  linter <- consecutive_stopifnot_linter()
+  msg <- rex::rex("Unify consecutive calls to stopifnot().")
+
+  expect_lint("stopifnot(x)", NULL, linter)
+  expect_lint("stopifnot(x, y, z)", NULL, linter)
 
   # intervening expression
-  expect_lint("stopifnot(x); y; stopifnot(z)", NULL, consecutive_stopifnot_linter())
+  expect_lint("stopifnot(x); y; stopifnot(z)", NULL, linter)
 
   # inline or potentially with gaps don't matter
   lines <- trim_some("
@@ -12,46 +15,33 @@ test_that("consecutive_stopifnot_linter skips allowed usages", {
 
     stopifnot(z)
   ")
-  expect_lint(lines, NULL, consecutive_stopifnot_linter())
+  expect_lint(lines, NULL, linter)
 })
 
 test_that("consecutive_stopifnot_linter blocks simple disallowed usages", {
+  linter <- consecutive_stopifnot_linter()
+  msg <- rex::rex("Unify consecutive calls to stopifnot().")
+
   # one test of inline usage
-  expect_lint(
-    "stopifnot(x); stopifnot(y)",
-    rex::rex("Unify consecutive calls to stopifnot()."),
-    consecutive_stopifnot_linter()
-  )
+  expect_lint("stopifnot(x); stopifnot(y)", msg, linter)
 
   lines_gap <- trim_some("
     stopifnot(x)
 
     stopifnot(y, z)
   ")
-  expect_lint(
-    lines_gap,
-    rex::rex("Unify consecutive calls to stopifnot()."),
-    consecutive_stopifnot_linter()
-  )
+  expect_lint(lines_gap, msg, linter)
 
   lines_consecutive <- trim_some("
     stopifnot(x)
     stopifnot(y)
   ")
-  expect_lint(
-    lines_consecutive,
-    rex::rex("Unify consecutive calls to stopifnot()."),
-    consecutive_stopifnot_linter()
-  )
+  expect_lint(lines_consecutive, msg, linter)
 
   lines_comment <- trim_some("
     stopifnot(x)
     # a comment on y
     stopifnot(y)
   ")
-  expect_lint(
-    lines_comment,
-    rex::rex("Unify consecutive calls to stopifnot()."),
-    consecutive_stopifnot_linter()
-  )
+  expect_lint(lines_comment, msg, linter)
 })
