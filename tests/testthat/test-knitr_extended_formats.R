631d98c6c94ae55a09564ef46583567f77d1f465
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-09-26 19:29:15 +0200

    address review comment

--- tests/testthat/test-knitr_extended_formats.R
+++ tests/testthat/test-knitr_extended_formats.R
@@ -1,16 +1,8 @@
-regexes <- list(
-  assign = rex("Use <-, not =, for assignment."),
-  local_var = rex("local variable"),
-  quotes = rex("Only use double-quotes."),
-  trailing = rex("Trailing blank lines are superfluous."),
-  trailws = rex("Trailing whitespace is superfluous.")
-)
-
 test_that("marginfigure engine from tufte package doesn't cause problems", {
   expect_lint(
     file = "knitr_extended_formats/tufte.Rmd",
     checks = list(
-      list(regexes[["assign"]], line_number = 11L)
+      list(rex("Use <-, not =, for assignment."), line_number = 11L)
     ),
     default_linters,
     parse_settings = FALSE
@@ -24,7 +16,7 @@ test_that("engines from bookdown package cause no problems", {
   expect_lint(
     file = "knitr_extended_formats/bookdown.Rmd",
     checks = list(
-      list(regexes[["assign"]], line_number = 14L)
+      list(rex("Use <-, not =, for assignment."), line_number = 14L)
     ),
     default_linters,
     parse_settings = FALSE
