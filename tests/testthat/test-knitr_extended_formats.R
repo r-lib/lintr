cba39a1589d4375585bce6d2a8e3f18f8e1c9503
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-09-26 19:30:47 +0200

    Update test-knitr_extended_formats.R

--- tests/testthat/test-knitr_extended_formats.R
+++ tests/testthat/test-knitr_extended_formats.R
@@ -1,9 +1,7 @@
 test_that("marginfigure engine from tufte package doesn't cause problems", {
   expect_lint(
     file = "knitr_extended_formats/tufte.Rmd",
-    checks = list(
-      list(rex("Use <-, not =, for assignment."), line_number = 11L)
-    ),
+    checks = list(rex("Use <-, not =, for assignment."), line_number = 11L),
     default_linters,
     parse_settings = FALSE
   )
@@ -15,9 +13,7 @@ test_that("engines from bookdown package cause no problems", {
 
   expect_lint(
     file = "knitr_extended_formats/bookdown.Rmd",
-    checks = list(
-      list(rex("Use <-, not =, for assignment."), line_number = 14L)
-    ),
+    checks = list(rex("Use <-, not =, for assignment."), line_number = 14L),
     default_linters,
     parse_settings = FALSE
   )
