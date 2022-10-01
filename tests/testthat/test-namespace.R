5464f5a65e0da71b96c475d655435942def6a179
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-10-01 14:36:03 +0200

    add test

--- tests/testthat/test-namespace.R
+++ tests/testthat/test-namespace.R
@@ -15,3 +15,23 @@ test_that("is_s3_generic doesn't error for namespace-qualified calls", {
   expect_warning(result <- is_s3_generic(func), NA)
   expect_false(result)
 })
+
+
+test_that("namespace_imports respects except directives", {
+  skip_if_not_installed("callr")
+
+  withr::with_dir(
+    test_path("dummy_packages", "importsexcept"),
+    {
+      callr::rcmd("INSTALL", ".", echo = FALSE, show = FALSE)
+      df <- namespace_imports(test_path("dummy_packages", "importsexcept"))
+
+      expect_equal(unique(df$pkg), "glue")
+      expect_true(any("glue_sql_collapse" == df$fun))
+
+      # excluded functions from imports not present
+      expect_false(any("glue_collapse" == df$fun))
+      expect_false(any("glue_data" == df$fun))
+    }
+  )
+})
