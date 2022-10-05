b8d03e09908d5cea19a25c3796922d25271c394b
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-10-05 23:21:02 +0200

    Provide better error message
    Closes #1622

--- tests/testthat/test-sarif_output.R
+++ tests/testthat/test-sarif_output.R
@@ -1,10 +1,14 @@
-test_that("`sarif_output` writes lints to JSON", {
+test_that("`sarif_output` produces expected warning", {
+  l <- lint(text = "x = 1", linters = assignment_linter())
+  expect_error(sarif_output(l), "Package path needs to be a relative path")
+})
+
+test_that("`sarif_output` writes expected files", {
   l <- lint_package(
     test_path("dummy_packages", "missing_dep"),
     linters = object_length_linter(),
     parse_settings = FALSE
   )
-  attributes(l) <- list(path = "./missing_dep")
 
   withr::with_tempdir({
     sarif_output(l)
