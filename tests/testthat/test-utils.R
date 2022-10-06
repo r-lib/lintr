94182c366a2f87b406d9540ef90ab7f48fe5dc84
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-10-06 13:10:06 +0200

    tests for lang utilities

---
+++ tests/testthat/test-utils.R
@@ -0,0 +1,10 @@
+test_that("set_lang and reset_lang work as expected", {
+  old_lang <- Sys.getenv("LANGUAGE")
+  new_lang <- "en"
+
+  set_lang(new_lang)
+  expect_identical(Sys.getenv("LANGUAGE"), new_lang)
+
+  reset_lang(old_lang)
+  expect_identical(Sys.getenv("LANGUAGE"), old_lang)
+})
