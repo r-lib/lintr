0ee6de719967521fb716c7d93daf59f472b660a9
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-10-07 15:52:27 +0200

    Use parametrized unit tests for equals_na_linter

--- tests/testthat/test-equals_na_linter.R
+++ tests/testthat/test-equals_na_linter.R
@@ -5,8 +5,10 @@ test_that("equals_na_linter skips allowed usages", {
   expect_lint("  blah", NULL, linter)
   expect_lint("  blah", NULL, linter)
   expect_lint("x=NA", NULL, linter)
+  expect_lint("x = NaN", NULL, linter)
   expect_lint("x = NA_real_", NULL, linter)
   expect_lint("is.na(x)", NULL, linter)
+  expect_lint("is.nan(x)", NULL, linter)
   expect_lint("x[!is.na(x)]", NULL, linter)
 
   # equals_na_linter should ignore strings and comments
@@ -18,38 +20,38 @@ test_that("equals_na_linter skips allowed usages", {
 
 })
 
-test_that("equals_na_linter blocks disallowed usages", {
+skip_if_not_installed("tibble")
+skip_if_not_installed("patrick")
+patrick::with_parameters_test_that(
+  "equals_na_linter blocks disallowed usages for all combinations of operators and types of NAs",
+  expect_lint(
+    sprintf("x %s %s", operation, type_na),
+    rex::rex("Use is.na for comparisons to NA (not == or !=)"),
+    equals_na_linter()
+  ),
+  .cases = tibble::tribble(
+    ~.test_name,                ~operation, ~type_na,
+    "equality, logical NA",     "==",       "NA",
+    "equality, integer NA",     "==",       "NA_integer_",
+    "equality, real NA",        "==",       "NA_real_",
+    "equality, complex NA",     "==",       "NA_complex_",
+    "equality, character NA",   "==",       "NA_character_",
+    "inequality, logical NA",   "!=",       "NA",
+    "inequality, integer NA",   "!=",       "NA_integer_",
+    "inequality, real NA",      "!=",       "NA_real_",
+    "inequality, complex NA",   "!=",       "NA_complex_",
+    "inequality, character NA", "!=",       "NA_character_",
+  )
+)
+
+test_that("equals_na_linter blocks disallowed usages in edge cases", {
   linter <- equals_na_linter()
   msg <- rex::rex("Use is.na for comparisons to NA (not == or !=)")
 
-  expect_lint(
-    "x == NA",
-    list(message = msg, line_number = 1L, column_number = 1L),
-    linter
-  )
-
-  expect_lint(
-    "x != NA",
-    list(message = msg, line_number = 1L, column_number = 1L),
-    linter
-  )
-
-  expect_lint(
-    "x != NA_integer_",
-    list(message = msg, line_number = 1L, column_number = 1L),
-    linter
-  )
-
-  expect_lint(
-    "x==NA",
-    list(message = msg, line_number = 1L, column_number = 1L),
-    linter
-  )
+  # missing spaces around operators
+  expect_lint("x==NA", list(message = msg, line_number = 1L, column_number = 1L), linter)
+  expect_lint("x!=NA", list(message = msg, line_number = 1L, column_number = 1L), linter)
 
   # correct line number for multiline code
-  expect_lint(
-    "x ==\nNA_character_",
-    list(line_number = 1L, column_number = 1L, ranges = list(c(1L, 4L))),
-    linter
-  )
+  expect_lint("x ==\nNA", list(line_number = 1L, column_number = 1L, ranges = list(c(1L, 4L))), linter)
 })
