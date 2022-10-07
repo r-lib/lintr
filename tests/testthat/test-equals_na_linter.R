912b6f05b6461a40777f57b416ab850652f42cb2
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-10-07 15:37:43 +0200

    Examples (and tests) for `equals_na_linter()`

--- tests/testthat/test-equals_na_linter.R
+++ tests/testthat/test-equals_na_linter.R
@@ -1,13 +1,41 @@
-test_that("returns the correct linting", {
+test_that("equals_na_linter skips allowed usages", {
   linter <- equals_na_linter()
-  msg <- rex::rex("Use is.na for comparisons to NA (not == or !=)")
+
   expect_lint("blah", NULL, linter)
   expect_lint("  blah", NULL, linter)
   expect_lint("  blah", NULL, linter)
   expect_lint("x=NA", NULL, linter)
+  expect_lint("x = NA_real_", NULL, linter)
+  expect_lint("is.na(x)", NULL, linter)
+  expect_lint("x[!is.na(x)]", NULL, linter)
+
+  # equals_na_linter should ignore strings and comments
+  expect_lint("is.na(x) # dont flag x == NA if inside a comment", NULL, linter)
+  expect_lint("msg <- 'dont flag x == NA if inside a string'", NULL, linter)
+
+  # nested NAs are okay
+  expect_lint("x==f(1, ignore = NA)", NULL, linter)
+
+})
+
+test_that("equals_na_linter blocks disallowed usages", {
+  linter <- equals_na_linter()
+  msg <- rex::rex("Use is.na for comparisons to NA (not == or !=)")
 
   expect_lint(
     "x == NA",
+    list(message = msg, line_number = 1L, column_number = 1L),
+    linter
+  )
+
+  expect_lint(
+    "x != NA",
+    list(message = msg, line_number = 1L, column_number = 1L),
+    linter
+  )
+
+  expect_lint(
+    "x != NA_integer_",
     list(message = msg, line_number = 1L, column_number = 1L),
     linter
   )
@@ -18,27 +46,9 @@ test_that("returns the correct linting", {
     linter
   )
 
-  expect_lint(
-    "x==f(1, ignore = NA)",
-    NULL,
-    linter
-  )
-
-  # equals_na_linter should ignore strings and comments
-  expect_lint(
-    "is.na(x) # dont flag x == NA if inside a comment",
-    NULL,
-    linter
-  )
-  expect_lint(
-    "msg <- 'dont flag x == NA if inside a string'",
-    NULL,
-    linter
-  )
-
   # correct line number for multiline code
   expect_lint(
-    "x ==\nNA",
+    "x ==\nNA_character_",
     list(line_number = 1L, column_number = 1L, ranges = list(c(1L, 4L))),
     linter
   )
