566db8a7df30737d92f49d4f088d6cd2141557c9
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-10-08 09:26:32 +0200

    Add examples to documentation: Part-3 (e) (#1631)
    * Examples (and tests) for `equals_na_linter()`
    
    * Use parametrized unit tests for equals_na_linter
    
    * Examples for `expect_comparison_linter()`
    
    * Examples for `expect_identical_linter()`
    
    * Examples for `expect_length_linter()`
    
    * Examples for `expect_length_linter()`
    
    * Examples for `expect_not_linter()`
    
    * test cleanup
    
    * Examples for `expect_null_linter()`
    
    * Add examples for `expect_s3_class_linter()`
    
    * Add examples for `expect_s4_class_linter()`
    
    * Add examples for `expect_true_false_linter()`
    
    * Examples for `expect_type_linter()`
    
    * Update test-equals_na_linter.R
    
    * Address some review comments
    
    * Use Matrix example for S4

--- tests/testthat/test-equals_na_linter.R
+++ tests/testthat/test-equals_na_linter.R
@@ -1,45 +1,59 @@
-test_that("returns the correct linting", {
+test_that("equals_na_linter skips allowed usages", {
   linter <- equals_na_linter()
-  msg <- rex::rex("Use is.na for comparisons to NA (not == or !=)")
+
   expect_lint("blah", NULL, linter)
   expect_lint("  blah", NULL, linter)
   expect_lint("  blah", NULL, linter)
   expect_lint("x=NA", NULL, linter)
-
-  expect_lint(
-    "x == NA",
-    list(message = msg, line_number = 1L, column_number = 1L),
-    linter
-  )
-
-  expect_lint(
-    "x==NA",
-    list(message = msg, line_number = 1L, column_number = 1L),
-    linter
-  )
-
-  expect_lint(
-    "x==f(1, ignore = NA)",
-    NULL,
-    linter
-  )
+  expect_lint("x = NaN", NULL, linter)
+  expect_lint("x = NA_real_", NULL, linter)
+  expect_lint("is.na(x)", NULL, linter)
+  expect_lint("is.nan(x)", NULL, linter)
+  expect_lint("x[!is.na(x)]", NULL, linter)
 
   # equals_na_linter should ignore strings and comments
+  expect_lint("is.na(x) # dont flag x == NA if inside a comment", NULL, linter)
+  expect_lint("msg <- 'dont flag x == NA if inside a string'", NULL, linter)
+
+  # nested NAs are okay
+  expect_lint("x==f(1, ignore = NA)", NULL, linter)
+})
+
+skip_if_not_installed("tibble")
+skip_if_not_installed("patrick")
+patrick::with_parameters_test_that(
+  "equals_na_linter blocks disallowed usages for all combinations of operators and types of NAs",
   expect_lint(
-    "is.na(x) # dont flag x == NA if inside a comment",
-    NULL,
-    linter
+    paste("x", operation, type_na),
+    rex::rex("Use is.na for comparisons to NA (not == or !=)"),
+    equals_na_linter()
+  ),
+  .cases = tibble::tribble(
+    ~.test_name,                ~operation, ~type_na,
+    "equality, logical NA",     "==",       "NA",
+    "equality, integer NA",     "==",       "NA_integer_",
+    "equality, real NA",        "==",       "NA_real_",
+    "equality, complex NA",     "==",       "NA_complex_",
+    "equality, character NA",   "==",       "NA_character_",
+    "inequality, logical NA",   "!=",       "NA",
+    "inequality, integer NA",   "!=",       "NA_integer_",
+    "inequality, real NA",      "!=",       "NA_real_",
+    "inequality, complex NA",   "!=",       "NA_complex_",
+    "inequality, character NA", "!=",       "NA_character_"
   )
-  expect_lint(
-    "msg <- 'dont flag x == NA if inside a string'",
-    NULL,
-    linter
-  )
+)
+
+test_that("equals_na_linter blocks disallowed usages in edge cases", {
+  linter <- equals_na_linter()
+  msg <- rex::rex("Use is.na for comparisons to NA (not == or !=)")
+
+  # missing spaces around operators
+  expect_lint("x==NA", list(message = msg, line_number = 1L, column_number = 1L), linter)
+  expect_lint("x!=NA", list(message = msg, line_number = 1L, column_number = 1L), linter)
+
+  # order doesn't matter
+  expect_lint("NA == x", list(message = msg, line_number = 1L, column_number = 1L), linter)
 
   # correct line number for multiline code
-  expect_lint(
-    "x ==\nNA",
-    list(line_number = 1L, column_number = 1L, ranges = list(c(1L, 4L))),
-    linter
-  )
+  expect_lint("x ==\nNA", list(line_number = 1L, column_number = 1L, ranges = list(c(1L, 4L))), linter)
 })
