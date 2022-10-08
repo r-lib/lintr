c07123b2e2e933eb5b20108a2611943d6d88ee22
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-10-08 07:51:02 +0200

    Update tests/testthat/test-methods.R

--- tests/testthat/test-methods.R
+++ tests/testthat/test-methods.R
@@ -152,5 +152,5 @@ test_that("split.lint works as intended", {
 test_that("within.list is dispatched", {
   l <- lint(text = "a=1\nb=2", linters = infix_spaces_linter())
   expect_silent(l <- lapply(l, within, line_number <- line_number + 1L))
-  expect_identical(vapply(l, `[[`, integer(1L), "line_number"), 2:3)
+  expect_identical(vapply(l, `[[`, integer(1L), "line_number"), 2L:3L)
 })
