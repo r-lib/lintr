c8483b7d47c730159132dc52fa64b2481ff191d3
Michael Chirico <chiricom@google.com>
2022-10-10 05:12:33 +0000

    add examples, links

--- R/unnecessary_placeholder_linter.R
+++ R/unnecessary_placeholder_linter.R
@@ -4,6 +4,31 @@
 #'   passed as the first positional argument; using it can cause confusion
 #'   and impacts readability.
 #'
+#' @examples
+#' # will produce lints
+#' lint(
+#'   text = "x %>% sum(., na.rm = TRUE)",
+#'   linters = unnecessary_placeholder_linter()
+#' )
+#'
+#' # okay
+#' lint(
+#'   text = "x %>% sum(na.rm = TRUE)",
+#'   linters = unnecessary_placeholder_linter()
+#' )
+#'
+#' lint(
+#'   text = "x %>% lm(data = ., y ~ z)",
+#'   linters = unnecessary_placeholder_linter()
+#' )
+#'
+#' lint(
+#'   text = "x %>% outer(., .)",
+#'   linters = unnecessary_placeholder_linter()
+#' )
+#'
+#' @evalRd rd_tags("unnecessary_placeholder_linter")
+#' @seealso [linters] for a complete list of linters available in lintr.
 #' @export
 unnecessary_placeholder_linter <- function() {
   # TODO(michaelchirico): handle R4.2.0 native placeholder _ as well
