bc78d6867ca0a044cc6090add0647a52090b95ed
Michael Chirico <chiricom@google.com>
2022-10-10 05:16:55 +0000

    remove TODO (posted as issue comment)

--- tests/testthat/test-unnecessary_placeholder_linter.R
+++ tests/testthat/test-unnecessary_placeholder_linter.R
@@ -7,15 +7,6 @@ test_that("unnecessary_placeholder_linter skips allowed usages", {
   expect_lint("x %>% foo(., .)", NULL, unnecessary_placeholder_linter())
   # . used as a keyword argument --> ok
   expect_lint("x %>% foo(arg = .)", NULL, unnecessary_placeholder_linter())
-  # TODO(chiricom): currently, the linter marks
-  #  ' x %>% ... %>% length(.) > 0
-  #  this _technically_ works as intended because it's parsed as
-  #  ' `>`(x %>% ... %>% length(), 0)
-  #  but I think the code is not quite behaving as intended.
-  #  ' x %>% ... %>% (length(.) > 0)
-  #  is probably closer to what was intended. So for now we mark the code as a
-  #  lint, but that also doesn't feel quite right (we're using this linter
-  #  as a signal of a different code quality issue).
 })
 
 test_that("unnecessary_placeholder_linter blocks simple disallowed usages", {
