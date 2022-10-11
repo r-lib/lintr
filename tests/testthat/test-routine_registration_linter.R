c66abbc5fcf621a9b0b1e5d48ae9f8aaf2ae02e6
Michael Chirico <chiricom@google.com>
2022-10-11 07:56:20 +0000

    fully parameterize

--- tests/testthat/test-routine_registration_linter.R
+++ tests/testthat/test-routine_registration_linter.R
@@ -1,16 +1,15 @@
-test_that("routine_registration_linter skips allowed usages", {
-  expect_lint(".Call(ROUTINE, 1)", NULL, routine_registration_linter())
-  expect_lint(".Fortran(f_ROUTINE, 2)", NULL, routine_registration_linter())
-})
-
 skip_if_not_installed("patrick")
 patrick::with_parameters_test_that(
-  "unregistered routines lint",
-  expect_lint(
-    sprintf("%s('ROUTINE', PACKAGE = 'foo')", caller),
-    "Register your native code routines with useDynLib",
-    routine_registration_linter()
-  ),
+  "lints correctly",
+  {
+    linter <- routine_registration_linter()
+    expect_lint(sprintf("%s(ROUTINE, 1)", caller), NULL, linter)
+    expect_lint(
+      sprintf("%s('ROUTINE', PACKAGE = 'foo')", caller),
+      "Register your native code routines with useDynLib",
+      linter
+    )
+  },
   .test_name = c(".C", ".Call", ".External", ".Fortran"),
   caller = c(".C", ".Call", ".External", ".Fortran")
 )
