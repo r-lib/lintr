1d4769020398a6ff2cd3527ca5b32fd71fcf86e4
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-10-12 19:58:12 +0200

    Further reading for no tabs linter
    Closes #1672

--- R/no_tab_linter.R
+++ R/no_tab_linter.R
@@ -1,6 +1,9 @@
 #' No tab linter
 #'
-#' Check that only spaces are used for indentation, not tabs.
+#' Check that only spaces are used for indentation, not tabs. Much ink has been
+#' spilt on this topic, and we encourage you to check out references for more
+#' information.
+#'
 #' @include make_linter_from_regex.R
 #'
 #' @examples
@@ -18,6 +21,11 @@
 #'
 #' @evalRd rd_tags("no_tab_linter")
 #' @seealso [linters] for a complete list of linters available in lintr.
+#'
+#' @references
+#' - https://softwareengineering.stackexchange.com/questions/57/tabs-versus-spaces-what-is-the-proper-indentation-character-for-everything-in-e
+#' - https://www.jwz.org/doc/tabs-vs-spaces.html
+#' - https://blog.codinghorror.com/death-to-the-space-infidels/
 #' @export
 no_tab_linter <- make_linter_from_regex(
   regex = rex(start, zero_or_more(regex("\\s")), one_or_more("\t")),
