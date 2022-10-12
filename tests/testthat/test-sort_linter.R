7c49b3160da305e6101bd288053451a2c0572b43
Hugo Gruson <Bisaloo@users.noreply.github.com>
2022-10-12 10:54:48 +0200

    Assign linter to var in tests

--- tests/testthat/test-sort_linter.R
+++ tests/testthat/test-sort_linter.R
@@ -1,51 +1,55 @@
 test_that("sort_linter skips allowed usages", {
-  expect_lint("order(y)", NULL, sort_linter())
+  linter <- sort_linter()
 
-  expect_lint("y[order(x)]", NULL, sort_linter())
+  expect_lint("order(y)", NULL, linter)
+
+  expect_lint("y[order(x)]", NULL, linter)
 
   # If another function is intercalated, don't fail
-  expect_lint("x[c(order(x))]", NULL, sort_linter())
+  expect_lint("x[c(order(x))]", NULL, linter)
 })
 
 
 test_that("sort_linter blocks simple disallowed usages", {
+  linter <- sort_linter()
   lint_message <- rex::rex("sort(", anything, ") is better than")
 
-  expect_lint("x[order(x)]", lint_message, sort_linter())
+  expect_lint("x[order(x)]", lint_message, linter)
 
   # Works with extra args in order()
-  expect_lint("x[order(x, decreasing = TRUE)]", lint_message, sort_linter())
+  expect_lint("x[order(x, decreasing = TRUE)]", lint_message, linter)
 
   # ...even in disorder
-  expect_lint("x[order(decreasing = TRUE, x)]", lint_message, sort_linter())
+  expect_lint("x[order(decreasing = TRUE, x)]", lint_message, linter)
 })
 
 test_that("sort_linter produces customized warning message", {
+  linter <- sort_linter()
 
   expect_lint(
     "y[order(y)]",
     rex::rex("sort(y, na.last = TRUE) is better than y[order(y)]."),
-    sort_linter()
+    linter
   )
 
   # We capture the correct variable symbol
   expect_lint(
     "y + x[order(x)]",
     rex::rex("sort(x, na.last = TRUE) is better than x[order(x)]."),
-    sort_linter()
+    linter
   )
 
   # Default na.last = TRUE is overwritten if na.last is already provided
   expect_lint(
     "x[order(x, na.last = FALSE)]",
     rex::rex("sort(x, na.last = FALSE) is better than x[order(x, na.last = FALSE)]."),
-    sort_linter()
+    linter
   )
 
   expect_lint(
     "x[order(x, decreasing = FALSE)]",
     rex::rex("sort(x, decreasing = FALSE, na.last = TRUE) is better than x[order(x, decreasing = FALSE)]."),
-    sort_linter()
+    linter
   )
 
 
