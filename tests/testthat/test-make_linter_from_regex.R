d25d1f2e472663c38abcdf5f5f5ad2927808823c
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-10-16 21:39:29 +0200

    Delete test-make_linter_from_regex.R

--- tests/testthat/test-make_linter_from_regex.R
+++
@@ -1,5 +0,0 @@
-test_that("test make_linter_from_regex works", {
-  linter <- lintr:::make_linter_from_regex("-", "style", "Silly lint.")()
-  expect_lint("a <- 2L", "Silly lint.", linter)
-  expect_lint("a = '2-3'", NULL, linter)
-})
