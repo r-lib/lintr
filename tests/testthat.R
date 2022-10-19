e1f51e6a4fdaeb9e80963e8b01c0c8be6577ea86
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-10-19 19:29:43 +0200

    Remove unnecessary `loadNamespace()` call for `{rex}`
    cf. https://github.com/r-lib/lintr/pull/1739#discussion_r999745186

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,10 +1,10 @@
 library(testthat)
 library(lintr)
+
 # suppress printing environment name (noisy)
 invisible({
-  loadNamespace("rex")
-  loadNamespace("withr")
   loadNamespace("patrick")
+  loadNamespace("withr")
 })
 
 test_check("lintr")
