266ddb65ec2ec90552f07b0f49f078fe0b48dc2b
Hugo Gruson <Bisaloo@users.noreply.github.com>
2022-10-22 16:41:50 +0200

    Add failing test for multiple lints

--- tests/testthat/test-sort_linter.R
+++ tests/testthat/test-sort_linter.R
@@ -58,3 +58,20 @@ test_that("sort_linter produces customized warning message", {
     linter
   )
 })
+
+test_that("sort_linter works with multiple lints in a single expression", {
+  linter <- sort_linter()
+
+  expect_lint(
+    "c(
+      x[order(x)],
+      y[order(y, decreasing = TRUE, na.last = FALSE)]
+    )",
+    list(
+      rex::rex("sort(x, na.last = TRUE) is better than x[order(x)]."),
+      rex::rex("sort(y, decreasing = TRUE, na.last = FALSE) is better than x[order(x, decreasing = TRUE, na.last = FALSE)].")
+    ),
+    linter
+  )
+
+})
