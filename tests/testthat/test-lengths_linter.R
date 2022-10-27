907873ed2d85a5ce2c0e058505a96db02b060cbf
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-10-27 08:38:00 +0200

    Update test-lengths_linter.R

--- tests/testthat/test-lengths_linter.R
+++ tests/testthat/test-lengths_linter.R
@@ -4,6 +4,7 @@ test_that("lengths_linter skips allowed usages", {
   expect_lint("length(x)", NULL, linter)
   expect_lint("function(x) length(x) + 1L", NULL, linter)
   expect_lint("vapply(x, fun, integer(length(y)))", NULL, linter)
+  expect_lint("sapply(x, sqrt, simplify = length(x))", NULL, linter)
 
   # TODO(#1570): also throw a lint here, and for map(x, length)
   expect_lint("lapply(x, length)", NULL, linter)
