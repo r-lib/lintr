890a4aa1c1795a1d926e87534d2c99ed6630e474
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-11-09 19:43:24 +0100

    undo formatted if else

--- R/utils.R
+++ R/utils.R
@@ -145,16 +145,12 @@ quote_wrap <- function(x, q) paste0(q, x, q)
 # interface to work like options() or setwd() -- returns the old value for convenience
 set_lang <- function(new_lang) {
   old_lang <- Sys.getenv("LANGUAGE", unset = NA)
-  Sys.setenv(LANGUAGE = new_lang) 
+  Sys.setenv(LANGUAGE = new_lang)
   old_lang
 }
 # handle the logic of either unsetting if it was previously unset, or resetting
 reset_lang <- function(old_lang) {
-  if (is.na(old_lang)) {
-    Sys.unsetenv("LANGUAGE")
-  } else {
-    Sys.setenv(LANGUAGE = old_lang) 
-  }
+  if (is.na(old_lang)) Sys.unsetenv("LANGUAGE") else Sys.setenv(LANGUAGE = old_lang)
 }
 
 #' Create a `linter` closure
