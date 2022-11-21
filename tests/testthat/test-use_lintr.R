2864d74de515b1332b01ee043a8190af871bf06f
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-11-21 09:56:58 +0100

    Use public interface to test internal functions: Part-4
    Part of #1692

--- tests/testthat/test-use_lintr.R
+++ tests/testthat/test-use_lintr.R
@@ -7,21 +7,19 @@ test_that("use_lintr works as expected", {
   # can't generate if a .lintr already exists
   expect_error(use_lintr(path = tmp), "Found an existing configuration")
 
-  # read_settings() works with the generated file
-  expect_silent(lintr:::read_settings(tmp))
-  lintr:::read_settings(NULL)
-
-  expect_identical(
-    normalizePath(lintr:::find_config(tmp)),
-    normalizePath(lintr_file)
-  )
+  # check that `read_settings()` works with the generated file
+  # this can be checked by checking lintr runs successfully
+  lints <- lint_dir(tmp)
+  expect_identical(length(lints), 0L)
 })
 
 test_that("use_lintr with type = full also works", {
   tmp <- withr::local_tempdir()
 
   # type = "full" also works with read_settings()
-  use_lintr(path = tmp, type = "full")
-  expect_silent(lintr:::read_settings(tmp))
-  lintr:::read_settings(NULL)
+  lintr_file <- use_lintr(path = tmp, type = "full")
+  expect_true(file.exists(lintr_file))
+
+  lints <- lint_dir(tmp)
+  expect_identical(length(lints), 0L)
 })
