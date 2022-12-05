70568e444e4cf442d40ffc3ef112f6d450df2ce0
Alexander Rosenstock <alexander.rosenstock@web.de>
2022-12-05 07:49:27 +0100

    skip on R < 4.1, add tests for infix_spaces_linter()

--- tests/testthat/test-indentation_linter.R
+++ tests/testthat/test-indentation_linter.R
@@ -111,25 +111,6 @@ test_that("indentation linter flags unindented expressions", {
     linter
   )
 
-  # native pipe indents
-  expect_lint(
-    trim_some("
-      a |>
-        foo()
-    "),
-    NULL,
-    linter
-  )
-
-  expect_lint(
-    trim_some("
-      b <- a |>
-        foo()
-    "),
-    NULL,
-    linter
-  )
-
   expect_lint(
     trim_some("
       if (cond)
@@ -684,5 +665,28 @@ test_that("consecutive same-level lints are suppressed", {
       list(line_number = 15L, message = "Hanging indent")
     ),
     indentation_linter()
+  )
+})
+
+test_that("native pipe is supported", {
+  skip_if_not_r_version("4.1")
+  linter <- indentation_linter()
+
+  expect_lint(
+    trim_some("
+      a |>
+        foo()
+    "),
+    NULL,
+    linter
+  )
+
+  expect_lint(
+    trim_some("
+      b <- a |>
+        foo()
+    "),
+    NULL,
+    linter
   )
 })
