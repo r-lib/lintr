c833f8a93460be8c6fdcb1466c09061292cefd1d
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-12-05 13:14:23 +0100

    Add tests for `no_tab_linter()` with pipes (#1795)

--- tests/testthat/test-no_tab_linter.R
+++ tests/testthat/test-no_tab_linter.R
@@ -7,6 +7,22 @@ test_that("no_tab_linter skips allowed usages", {
   expect_lint("#\tblah", NULL, linter)
 })
 
+test_that("no_tab_linter skips allowed tab usages inside strings", {
+  linter <- no_tab_linter()
+
+  expect_lint(
+    'lint_msg <- "dont flag tabs if\tthey are inside a string."',
+    NULL,
+    linter
+  )
+
+  expect_lint(
+    'lint_msg <- "dont flag tabs if\n\tthey are inside multiline strings."',
+    NULL,
+    linter
+  )
+})
+
 test_that("no_tab_linter blocks disallowed usages", {
   linter <- no_tab_linter()
   lint_msg <- rex::rex("Use spaces to indent, not tabs.")
@@ -22,17 +38,23 @@ test_that("no_tab_linter blocks disallowed usages", {
     list(message = lint_msg, line_number = 2L, column_number = 1L),
     linter
   )
+})
 
-  # ignore strings
+test_that("no_tab_linter blocks disallowed usages with a pipe", {
+  skip_if_not_r_version("4.1.0")
+
+  linter <- no_tab_linter()
+  lint_msg <- rex::rex("Use spaces to indent, not tabs.")
+
   expect_lint(
-    'lint_msg <- "dont flag tabs if\tthey are inside a string."',
-    NULL,
+    "a %>%\n\tb()",
+    list(message = lint_msg, line_number = 2L, column_number = 1L, ranges = list(c(1L, 1L))),
     linter
   )
-  # ignore multi-line strings
+
   expect_lint(
-    'lint_msg <- "dont flag tabs if\n\tthey are inside multiline strings."',
-    NULL,
+    "a |>\n\tb()",
+    list(message = lint_msg, line_number = 2L, column_number = 1L, ranges = list(c(1L, 1L))),
     linter
   )
 })
