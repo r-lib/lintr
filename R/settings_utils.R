6e0a4c1cb309efd7797f28bc5cfeda13a25a00d9
Michael Chirico <michaelchirico4@gmail.com>
2022-12-16 00:25:35 -0800

    too many ')'

--- R/settings_utils.R
+++ R/settings_utils.R
@@ -10,7 +10,7 @@ has_rproj <- function(path) {
 find_package <- function(path, allow_rproj = FALSE, max_depth = 2L) {
   path <- normalizePath(path, mustWork = !allow_rproj)
   if (allow_rproj) {
-    found <- function(path) has_description(path) || has_rproj(path))
+    found <- function(path) has_description(path) || has_rproj(path)
   } else {
     found <- function(path) has_description(path)
   }
