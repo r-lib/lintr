f22da54cf0fbc7887a14eabaf2cdb57cb1ebae9b
Hugo Gruson <Bisaloo@users.noreply.github.com>
2022-12-21 11:24:25 +0100

    Add basic detection pattern and tests

---
+++ R/colsums_rowsums_linter.R
@@ -0,0 +1,78 @@
+#' Require usage of `colSums(x)` or `rowSums(x)` over `apply(x, ., sum)`
+#'
+#' [colSums()] and [rowSums()] are clearer and more performant alternatives to
+#' `apply(x, 2, sum)` and `apply(x, 1, sum)` respectively in the case of 2D
+#' arrays, or matrices
+#'
+#' @examples
+#' # will produce lints
+#' lint(
+#'   text = "apply(x, 1, sum)",
+#'   linters = colsums_rowsums_linter()
+#' )
+#'
+#' lint(
+#'   text = "apply(x, 2, sum)",
+#'   linters = colsums_rowsums_linter()
+#' )
+#'
+#' lint(
+#'   text = "apply(x, 2, sum, na.rm = TRUE)",
+#'   linters = colsums_rowsums_linter()
+#' )
+#'
+#' @evalRd rd_tags("expect_named_linter")
+#' @seealso [linters] for a complete list of linters available in lintr.
+#' @export
+colsums_rowsums_linter <- function() {
+
+  # mean() and sum() have very different signatures so we treat them separately.
+  # sum() takes values to sum over via ..., has just one extra argument and is not a generic
+  # mean() is a generic, takes values to average via a single argument, and can have extra arguments
+  sums_xpath <- "
+  //SYMBOL_FUNCTION_CALL[text() = 'apply']
+    /parent::expr
+    /following-sibling::expr[
+      SYMBOL[text() = 'sum']
+      and (position() = 3)
+    ]
+  "
+
+  means_xpath <- "
+  //SYMBOL_FUNCTION_CALL[text() = 'apply']
+    /parent::expr
+    /following-sibling::expr[
+      SYMBOL[text() = 'mean']
+      and (position() = 3)
+    ]
+  "
+
+  Linter(function(source_expression) {
+    if (!is_lint_level(source_expression, "expression")) {
+      return(list())
+    }
+
+    xml <- source_expression$xml_parsed_content
+
+    bad_sums_expr <- xml2::xml_find_all(xml, sums_xpath)
+    sums_lint_message <- sprintf("colSums()")
+    sums_lint <- xml_nodes_to_lints(
+      bad_sums_expr,
+      source_expression = source_expression,
+      sums_lint_message,
+      type = "warning"
+    )
+
+    bad_means_expr <- xml2::xml_find_all(xml, means_xpath)
+    means_lint_message <- sprintf("colMeans()")
+    means_lint <- xml_nodes_to_lints(
+      bad_means_expr,
+      source_expression = source_expression,
+      means_lint_message,
+      type = "warning"
+    )
+
+    c(sums_lint, means_lint)
+
+  })
+}
---
+++ tests/testthat/test-colsums_rowsums_linter.R
@@ -0,0 +1,62 @@
+test_that("colsums_rowsums_linter skips allowed usages", {
+  linter <- colsums_rowsums_linter()
+
+  expect_lint("apply(x, 1, prod)", NULL, linter)
+
+  expect_lint("apply(x, 1, function(i) sum(i[i > 0]))", NULL, linter)
+
+  # sum as FUN argument
+  expect_lint("apply(x, 1, f, sum)", NULL, linter)
+
+  # mean() with named arguments other than na.rm is skipped because they are not
+  # implemented in colMeans() or rowMeans()
+  expect_lint("apply(x, 1, mean, trim = 0.2)", NULL, linter)
+})
+
+
+test_that("colsums_rowsums_linter simple disallowed usages", {
+  linter <- colsums_rowsums_linter()
+  lint_message <- rex::rex("colSums")
+
+  expect_lint("apply(x, 1, sum)", lint_message, linter)
+
+  expect_lint("apply(x, MARGIN = 1, FUN = sum)", lint_message, linter)
+
+  expect_lint("apply(x, 2, sum)", lint_message, linter)
+
+  expect_lint("apply(x, 2:4, sum)", lint_message, linter)
+
+  # Works with extra args in sum()
+  expect_lint("apply(x, 1, sum, na.rm = TRUE)", lint_message, linter)
+
+  lint_message <- rex::rex("colMeans")
+
+  expect_lint("apply(x, 1, mean)", lint_message, linter)
+
+  expect_lint("apply(x, MARGIN = 1, FUN = mean)", lint_message, linter)
+
+  expect_lint("apply(x, 2, mean)", lint_message, linter)
+
+  expect_lint("apply(x, 2:4, mean)", lint_message, linter)
+
+  # Works with extra args in mean()
+  expect_lint("apply(x, 1, mean, na.rm = TRUE)", lint_message, linter)
+
+})
+
+test_that("colsums_rowsums_linterr works with multiple lints in a single expression", {
+  linter <- colsums_rowsums_linter()
+
+  expect_lint(
+    "rbind(
+      apply(x, 1, sum),
+      apply(y, 1, sum)
+    )",
+    list(
+      rex::rex("colSums"),
+      rex::rex("colSums")
+    ),
+    linter
+  )
+
+})
