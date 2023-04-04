c6099d55a6e64dacdc4fb22fe2df5e2d2b5cd9d8
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-04-04 10:05:37 +0200

    Avoid implicit assignments in expectations (#1879)
    * getting started
    
    * use magrittr pipe instead
    
    * handle a few more
    
    * a few more
    
    * and some more
    
    * Update test-xml_nodes_to_lints.R
    
    * fix indentation lints
    
    * remove magrittr brace syntax in favour of tempvar
    
    * don't gain dependency on magrittr

--- tests/testthat/test-lint_package.R
+++ tests/testthat/test-lint_package.R
@@ -132,7 +132,13 @@ test_that(
 test_that("lint_package returns early if no package is found", {
   temp_pkg <- withr::local_tempdir("dir")
 
-  expect_warning(l <- lint_package(temp_pkg), "Didn't find any R package", fixed = TRUE)
+  expect_warning(
+    {
+      l <- lint_package(temp_pkg)
+    },
+    "Didn't find any R package",
+    fixed = TRUE
+  )
   expect_null(l)
 
   # ignore a folder named DESCRIPTION, #702
