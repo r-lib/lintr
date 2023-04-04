e7002a2187dfe74c4a1daa3215e4e32998260005
Michael Chirico <michaelchirico4@gmail.com>
2023-04-03 20:11:35 -0700

    use local variables in tests

--- tests/testthat/test-consecutive_assertion_linter.R
+++ tests/testthat/test-consecutive_assertion_linter.R
@@ -1,58 +1,62 @@
 test_that("consecutive_assertion_linter skips allowed usages", {
-  expect_lint("stopifnot(x)", NULL, consecutive_assertion_linter())
-  expect_lint("stopifnot(x, y, z)", NULL, consecutive_assertion_linter())
+  linter <- consecutive_assertion_linter()
+  expect_lint("stopifnot(x)", NULL, linter)
+  expect_lint("stopifnot(x, y, z)", NULL, linter)
 
   # intervening expression
-  expect_lint("stopifnot(x); y; stopifnot(z)", NULL, consecutive_assertion_linter())
+  expect_lint("stopifnot(x); y; stopifnot(z)", NULL, linter)
 
   # inline or potentially with gaps don't matter
-  lines <- trim_some("
-    stopifnot(x)
-    y
-
-    stopifnot(z)
-  ")
-  expect_lint(lines, NULL, consecutive_assertion_linter())
+  expect_lint(
+    trim_some("
+      stopifnot(x)
+      y
+  
+      stopifnot(z)
+    "),
+    NULL,
+    linter
+  )
 })
 
 test_that("consecutive_assertion_linter blocks simple disallowed usages", {
+  linter <- consecutive_assertion_linter()
+  lint_msg <- rex::rex("Unify consecutive calls to stopifnot().")
+
   # one test of inline usage
   expect_lint(
     "stopifnot(x); stopifnot(y)",
-    rex::rex("Unify consecutive calls to stopifnot()."),
-    consecutive_assertion_linter()
+    lint_msg,
+    linter
   )
 
-  lines_gap <- trim_some("
-    stopifnot(x)
-
-    stopifnot(y, z)
-  ")
   expect_lint(
-    lines_gap,
-    rex::rex("Unify consecutive calls to stopifnot()."),
-    consecutive_assertion_linter()
+    trim_some("
+      stopifnot(x)
+  
+      stopifnot(y, z)
+    "),
+    lint_msg,
+    linter
   )
 
-  lines_consecutive <- trim_some("
-    stopifnot(x)
-    stopifnot(y)
-  ")
   expect_lint(
-    lines_consecutive,
-    rex::rex("Unify consecutive calls to stopifnot()."),
-    consecutive_assertion_linter()
+    trim_some("
+      stopifnot(x)
+      stopifnot(y)
+    "),
+    lint_msg,
+    linter
   )
 
-  lines_comment <- trim_some("
-    stopifnot(x)
-    # a comment on y
-    stopifnot(y)
-  ")
   expect_lint(
-    lines_comment,
-    rex::rex("Unify consecutive calls to stopifnot()."),
-    consecutive_assertion_linter()
+    trim_some("
+      stopifnot(x)
+      # a comment on y
+      stopifnot(y)
+    "),
+    lint_msg,
+    linter
   )
 })
 
@@ -60,21 +64,21 @@ test_that("assert_that usages are handled correctly too", {
   linter <- consecutive_assertion_linter()
   lint_msg <- rex::rex("Unify consecutive calls to assert_that().")
 
-  expect_lint("assert_that(x)", NULL, consecutive_assertion_linter())
-  expect_lint("assertthat::assert_that(x, y, z)", NULL, consecutive_assertion_linter())
+  expect_lint("assert_that(x)", NULL, linter)
+  expect_lint("assertthat::assert_that(x, y, z)", NULL, linter)
 
   # if msg= is used, can't necessarily combine
   lines <- trim_some("
     assert_that(x, msg = 'bad x')
     assert_that(y, msg = 'bad y')
   ")
-  expect_lint(lines, NULL, consecutive_assertion_linter())
+  expect_lint(lines, NULL, linter)
 
   # one test of inline usage
   expect_lint(
     "assert_that(x); assert_that(y)",
     lint_msg,
-    consecutive_assertion_linter()
+    linter
   )
 
   lines_gap <- trim_some("
@@ -82,7 +86,7 @@ test_that("assert_that usages are handled correctly too", {
 
     assertthat::assert_that(y, z)
   ")
-  expect_lint(lines_gap, lint_msg, consecutive_assertion_linter())
+  expect_lint(lines_gap, lint_msg, linter)
 })
 
 test_that("Mixing test functions is fine", {
