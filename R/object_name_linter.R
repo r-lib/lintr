3b03bc94301ad27379077e9eb4633db714110017
Hedley <git@heds.nz>
2023-07-05 15:49:30 +1200

    document "symbols" in object_name_linter (#1924)

--- R/object_name_linter.R
+++ R/object_name_linter.R
@@ -45,7 +45,9 @@ object_name_xpath <- local({
 #'
 #' @param styles A subset of
 #'   \Sexpr[stage=render, results=rd]{lintr:::regexes_rd}. A name should
-#'   match at least one of these styles.
+#'   match at least one of these styles. The `"symbols"` style refers to
+#'   names containing only non-alphanumeric characters, e.g., `%+%` from
+#'   ggplot2 or `%>%` from magrittr.
 #' @param regexes A (possibly named) character vector specifying a custom naming convention.
 #'   If named, the names will be used in the lint message. Otherwise, the regexes enclosed by `/` will be used in the
 #'   lint message.
