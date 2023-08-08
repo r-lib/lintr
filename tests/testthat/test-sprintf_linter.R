92f75fdd9097cc80a223822fdd156ce947ca6fba
Michael Chirico <chiricom@google.com>
2023-08-08 18:38:36 +0000

    new tests with nesting, all pipes

--- tests/testthat/test-sprintf_linter.R
+++ tests/testthat/test-sprintf_linter.R
@@ -92,17 +92,41 @@ test_that("edge cases are detected correctly", {
   )
 })
 
-test_that("piping into sprintf works", {
+local({
   linter <- sprintf_linter()
+  unused_fmt_msg <- "too few arguments"
+  unused_arg_msg <- "one argument not used by format"
+  pipes <- pipes(exclude = "%$%")
+  patrick::with_parameters_test_that(
+    "piping into sprintf works",
+    {
+      expect_lint(paste("x", pipe, "sprintf(fmt = '%s')"), NULL, linter)
+      # no fmt= specified -> this is just 'sprintf("%s", "%s%s")', which won't lint
+      expect_lint(paste('"%s"', pipe, 'sprintf("%s%s")'), NULL, linter)
+      expect_lint(paste("x", pipe, "sprintf(fmt = '%s%s')"), unused_fmt_msg, linter)
 
-  expect_lint("x %>% sprintf(fmt = '%s')", NULL, linter)
-  # no fmt= specified -> this is just 'sprintf("%s", "%s%s")', which won't lint
-  expect_lint('"%s" %>% sprintf("%s%s")', NULL, linter)
-  expect_lint("x %>% sprintf(fmt = '%s%s')", "too few arguments", linter)
-
-  skip_if_not_r_version("4.1.0")
-  # Cannot evaluate statically --> skip
-  expect_lint('x |> sprintf("a")', NULL, linter)
-  # We don't handle nested piping, so don't test exact behavior, just ensure no error
-  expect_no_error(lint("'%%sb' |> sprintf('%s') |> sprintf('a')\n", linter))
+      # Cannot evaluate statically --> skip
+      expect_lint(paste("x", pipe, 'sprintf("a")'), NULL, linter)
+      # Nested pipes
+      pipe_width <- nchar(pipe)
+      expect_lint(
+        paste("'%%sb'", pipe, "sprintf('%s')", pipe, "sprintf('a')"),
+        list(column_number = nchar(paste("'%%sb'", pipe, "x")), message = unused_arg_msg),
+        linter
+      )
+      expect_lint(
+        paste("x", pipe, 'sprintf(fmt = "%s")', pipe, 'sprintf(fmt = "%s%s")'),
+        list(column_number = nchar(paste("x", pipe, 'sprintf(fmt = "%s")', pipe, "x")), message = unused_fmt_msg),
+        linter
+      )
+      expect_lint(
+        paste("x", pipe, 'sprintf(fmt = "%s%s")', pipe, 'sprintf(fmt = "%s")'),
+        list(column_number = nchar(paste("x", pipe, "x")), message = unused_fmt_msg),
+        linter
+      )
+    },
+    pipe = pipes,
+    .test_name = names(pipes)
+  )
 })
+
