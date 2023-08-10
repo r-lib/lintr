86ef6b4b83d58755c8fd197334375611aa5e30f1
Michael Chirico <chiricom@google.com>
2023-08-09 22:48:46 -0700

    include line length in lint message (#2066)
    * include line length in lint message
    
    * slight refactor, test multiple lints
    
    * rm vestigial
    
    * adjust incidental test
    
    * NEWS grammar
    
    * trailing ws
    
    * explicit integer

--- tests/testthat/test-with.R
+++ tests/testthat/test-with.R
@@ -50,7 +50,10 @@ test_that("all default linters are tagged default", {
   skip_if_not_r_version("4.1.0") # Desired all.equal behavior only available in >= 4.1
   expect_identical(
     all.equal(linters_with_tags("default"), linters_with_defaults(line_length_linter(120L))),
-    'Component "line_length_linter": Component "length": Mean relative difference: 0.5'
+    c(
+      'Component "line_length_linter": Component "general_msg": 1 string mismatch',
+      'Component "line_length_linter": Component "length": Mean relative difference: 0.5'
+    )
   )
 })
 
