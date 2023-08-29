caaa4018d310ef4fddae54a86327b4631d09c2b1
Michael Chirico <chiricom@google.com>
2023-08-29 12:40:36 +0000

    fix NEWS, typo, document expr

--- R/xp_utils.R
+++ R/xp_utils.R
@@ -46,11 +46,11 @@ xp_or <- function(...) paren_wrap(..., sep = "or")
 
 #' Get the name of the function matched by an XPath
 #'
-#' Often, it is mroe helpful to custom-tailer the `message` of a lint to record
+#' Often, it is more helpful to custom-tailer the `message` of a lint to record
 #'   which function was matched by the lint logic. This function encapsualtes
 #'   the logic to pull out the matched call in common situations.
 #'
-#' @param expr An
+#' @param expr An `xml_node` or `xml_nodeset`, e.g. from [xml2::xml_find_all()].
 #' @param depth Integer, default `1L`. How deep in the AST represented by `expr`
 #'   should we look to find the call? By default, we assume `expr` is matched
 #'   to an `<expr>` node under which the corresponding `<SYMBOL_FUNCTION_CALL>`
