d112eec2ff01b63cc51611ccdd44c5d8b86de598
Matthias Ollech <wertz265@web.de>
2023-09-05 21:56:46 +0200

    Add more tests

--- tests/testthat/test-repeat_linter.R
+++ tests/testthat/test-repeat_linter.R
@@ -6,8 +6,11 @@ test_that("test repeat_linter", {
   expect_lint("repeat { }", NULL, linter)
   expect_lint("while (FALSE) { }", NULL, linter)
   expect_lint("while (i < 5) { }", NULL, linter)
+  expect_lint("while (j < 5) TRUE", NULL, linter)
+  expect_lint("while (TRUE && j < 5) { ... }", NULL, linter)
 
   expect_lint("while (TRUE) { }", msg, linter)
+  expect_lint("`while`(TRUE)", msg, linter)
   expect_lint("for (i in 1:10) { while (TRUE) { if (i == 5) { break } } }", msg, linter)
   expect_lint("while (TRUE) { while (TRUE) { } }", list(msg, msg), linter)
 })
