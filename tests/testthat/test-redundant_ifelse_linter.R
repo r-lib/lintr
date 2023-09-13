b245e032620410edd8559f6425fae127c997b500
Michael Chirico <chiricom@google.com>
2023-09-13 16:25:38 +0000

    add new test cases

--- tests/testthat/test-redundant_ifelse_linter.R
+++ tests/testthat/test-redundant_ifelse_linter.R
@@ -11,107 +11,134 @@ test_that("redundant_ifelse_linter skips allowed usages", {
 })
 
 test_that("redundant_ifelse_linter blocks simple disallowed usages", {
+  linter <- redundant_ifelse_linter()
+
   expect_lint(
     "ifelse(x > 5, TRUE, FALSE)",
     rex::rex("Just use the logical condition (or its negation) directly"),
-    redundant_ifelse_linter()
+    linter
   )
   expect_lint(
     "ifelse(x > 5, FALSE, TRUE)",
     rex::rex("Just use the logical condition (or its negation) directly"),
-    redundant_ifelse_linter()
+    linter
   )
 
   # other ifelse equivalents from common packages
   expect_lint(
     "if_else(x > 5, TRUE, FALSE)",
     rex::rex("Just use the logical condition (or its negation) directly"),
-    redundant_ifelse_linter()
+    linter
   )
   expect_lint(
     "fifelse(x > 5, FALSE, TRUE)",
     rex::rex("Just use the logical condition (or its negation) directly"),
-    redundant_ifelse_linter()
+    linter
   )
 })
 
 test_that("redundant_ifelse_linter blocks usages equivalent to as.numeric, optionally", {
+  linter <- redundant_ifelse_linter()
+
   expect_lint(
     "ifelse(x > 5, 1L, 0L)",
     rex::rex("Prefer as.integer(x) to ifelse(x, 1L, 0L)"),
-    redundant_ifelse_linter()
+    linter
   )
   expect_lint(
     "ifelse(x > 5, 0L, 1L)",
     rex::rex("Prefer as.integer(!x) to ifelse(x, 0L, 1L)"),
-    redundant_ifelse_linter()
+    linter
   )
 
   expect_lint(
     "ifelse(x > 5, 1, 0)",
     rex::rex("Prefer as.numeric(x) to ifelse(x, 1, 0)"),
-    redundant_ifelse_linter()
+    linter
   )
   expect_lint(
     "ifelse(x > 5, 0, 1)",
     rex::rex("Prefer as.numeric(!x) to ifelse(x, 0, 1)"),
-    redundant_ifelse_linter()
+    linter
   )
 
   # mixing int and num
   expect_lint(
     "ifelse(x > 5, 0, 1L)",
     rex::rex("Prefer as.numeric(!x) to ifelse(x, 0, 1L)"),
-    redundant_ifelse_linter()
+    linter
   )
   expect_lint(
     "ifelse(x > 5, 0L, 1)",
     rex::rex("Prefer as.numeric(!x) to ifelse(x, 0L, 1)"),
-    redundant_ifelse_linter()
+    linter
   )
   expect_lint(
     "ifelse(x > 5, 1, 0L)",
     rex::rex("Prefer as.numeric(x) to ifelse(x, 1, 0L)"),
-    redundant_ifelse_linter()
+    linter
   )
   expect_lint(
     "ifelse(x > 5, 1L, 0)",
     rex::rex("Prefer as.numeric(x) to ifelse(x, 1L, 0)"),
-    redundant_ifelse_linter()
+    linter
   )
 
   # data.table/dplyr equivalents
   expect_lint(
     "dplyr::if_else(x > 5, 1L, 0L)",
     rex::rex("Prefer as.integer(x) to if_else(x, 1L, 0L)"),
-    redundant_ifelse_linter()
+    linter
   )
   expect_lint(
     "data.table::fifelse(x > 5, 0L, 1L)",
     rex::rex("Prefer as.integer(!x) to fifelse(x, 0L, 1L)"),
-    redundant_ifelse_linter()
+    linter
   )
 
   expect_lint(
     "if_else(x > 5, 1, 0)",
     rex::rex("Prefer as.numeric(x) to if_else(x, 1, 0)"),
-    redundant_ifelse_linter()
+    linter
   )
   expect_lint(
     "fifelse(x > 5, 0, 1)",
     rex::rex("Prefer as.numeric(!x) to fifelse(x, 0, 1)"),
-    redundant_ifelse_linter()
+    linter
   )
+})
 
-  expect_lint("ifelse(x > 5, 1L, 0L)", NULL, redundant_ifelse_linter(allow10 = TRUE))
-  expect_lint("ifelse(x > 5, 0L, 1L)", NULL, redundant_ifelse_linter(allow10 = TRUE))
+test_that("allow10 works as intended", {
+  linter <- redundant_ifelse_linter(allow10 = TRUE)
 
-  expect_lint("ifelse(x > 5, 1, 0)", NULL, redundant_ifelse_linter(allow10 = TRUE))
-  expect_lint("ifelse(x > 5, 0, 1)", NULL, redundant_ifelse_linter(allow10 = TRUE))
+  expect_lint("ifelse(x > 5, 1L, 0L)", NULL, linter)
+  expect_lint("ifelse(x > 5, 0L, 1L)", NULL, linter)
 
-  expect_lint("dplyr::if_else(x > 5, 1L, 0L)", NULL, redundant_ifelse_linter(allow10 = TRUE))
-  expect_lint("data.table::fifelse(x > 5, 0L, 1L)", NULL, redundant_ifelse_linter(allow10 = TRUE))
+  expect_lint("ifelse(x > 5, 1, 0)", NULL, linter)
+  expect_lint("ifelse(x > 5, 0, 1)", NULL, linter)
 
-  expect_lint("if_else(x > 5, 1, 0)", NULL, redundant_ifelse_linter(allow10 = TRUE))
-  expect_lint("fifelse(x > 5, 0, 1)", NULL, redundant_ifelse_linter(allow10 = TRUE))
+  expect_lint("dplyr::if_else(x > 5, 1L, 0L)", NULL, linter)
+  expect_lint("data.table::fifelse(x > 5, 0L, 1L)", NULL, linter)
+
+  expect_lint("if_else(x > 5, 1, 0)", NULL, linter)
+  expect_lint("fifelse(x > 5, 0, 1)", NULL, linter)
+})
+
+test_that("ifelse(missing = ) gives correct lints", {
+  linter <- redundant_ifelse_linter()
+
+  expect_lint("if_else(x > 5, TRUE, FALSE, NA)", "xxx", linter)
+  expect_lint("if_else(x > 5, TRUE, FALSE, NA_integer_)", "xxx", linter)
+  expect_lint("if_else(x > 5, TRUE, FALSE, TRUE)", "xxx", linter)
+  expect_lint("if_else(x > 5, TRUE, FALSE, 5L)", "xxx", linter)
+
+  expect_lint("if_else(x > 5, 1L, 0L, NA_integer_)", "xxx", linter)
+  expect_lint("if_else(x > 5, 1L, 0L, NA_real_)", "xxx", linter)
+  expect_lint("if_else(x > 5, 1L, 0L, 2L)", "xxx", linter)
+  expect_lint("if_else(x > 5, 1L, 0L, 5)", "xxx", linter)
+
+  expect_lint("if_else(x > 5, 1, 0, NA_real_)", "xxx", linter)
+  expect_lint("if_else(x > 5, 1, 0, NA_character_)", "xxx", linter)
+  expect_lint("if_else(x > 5, 1, 0, 2)", "xxx", linter)
+  expect_lint("if_else(x > 5, 1, 0, '5')", "xxx", linter)
 })
