3c50582dddff434cfc2b863198b04d58444cc510
Michael Chirico <chiricom@google.com>
2023-09-18 00:17:23 -0700

    we don't allow that here

--- R/settings.R
+++ R/settings.R
@@ -72,8 +72,8 @@ get_setting <- function(setting, config, defaults) {
   option <- getOption(paste(sep = ".", "lintr", setting))
   if (!is.null(option)) {
     option
-  } else if (!is.null(value <- config[[setting]])) {
-    value
+  } else if (!is.null(config[[setting]])) {
+    config[[setting]]
   } else {
     defaults[[setting]]
   }
