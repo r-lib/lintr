4d6b238d8870cc719c38bee18ba173f4d091e951
Michael Chirico <chiricom@google.com>
2023-09-18 22:09:00 +0000

    revert bad simplification

--- R/unnecessary_lambda_linter.R
+++ R/unnecessary_lambda_linter.R
@@ -61,9 +61,9 @@ unnecessary_lambda_linter <- function() {
   #     d. the function argument doesn't appear elsewhere in the call
   # TODO(#1703): handle explicit returns too: function(x) return(x)
   symbol_tests_fmt <- "
-    {call_expr_path}/expr[2][not(preceding-sibling::*[1][self::EQ_SUB])]/SYMBOL
-    and SYMBOL_FORMALS = {call_expr_path}/expr[2]/SYMBOL
-    and not(SYMBOL_FORMALS = {call_expr_path}/expr[position() > 2]//SYMBOL)
+    {call_expr_path}/OP-LEFT-PAREN/following-sibling::expr[1][not(preceding-sibling::*[1][self::EQ_SUB])]/SYMBOL
+    and SYMBOL_FORMALS = {call_expr_path}/OP-LEFT-PAREN/following-sibling::expr[1]/SYMBOL
+    and not(SYMBOL_FORMALS = {call_expr_path}/OP-LEFT-PAREN/following-sibling::expr[position() > 1]//SYMBOL)
   "
   default_fun_xpath <- glue("
   //SYMBOL_FUNCTION_CALL[ {apply_funs} ]
