c83640edbf5c93ad9c99e79f48c947a903fa2ba9
Michael Chirico <chiricom@google.com>
2023-09-18 20:50:11 +0000

    condense XPath

--- R/unnecessary_lambda_linter.R
+++ R/unnecessary_lambda_linter.R
@@ -60,22 +60,23 @@ unnecessary_lambda_linter <- function() {
   #       - and it has to be passed positionally (not as a keyword)
   #     d. the function argument doesn't appear elsewhere in the call
   # TODO(#1703): handle explicit returns too: function(x) return(x)
-  default_fun_xpath_fmt <- "
+  symbol_loc_fmt <- "
+    {paren_path}/OP-LEFT-PAREN/following-sibling::expr[1][not(preceding-sibling::*[1][self::EQ_SUB])]/SYMBOL
+    and SYMBOL_FORMALS = {paren_path}/OP-LEFT-PAREN/following-sibling::expr[1]/SYMBOL
+    and not(SYMBOL_FORMALS = {paren_path}/OP-LEFT-PAREN/following-sibling::expr[position() > 1]//SYMBOL)
+  "
+  default_fun_xpath <- glue("
   //SYMBOL_FUNCTION_CALL[ {apply_funs} ]
     /parent::expr
     /following-sibling::expr[
       FUNCTION
       and count(SYMBOL_FORMALS) = 1
-      and {paren_path}/OP-LEFT-PAREN/following-sibling::expr[1][not(preceding-sibling::*[1][self::EQ_SUB])]/SYMBOL
-      and SYMBOL_FORMALS = {paren_path}/OP-LEFT-PAREN/following-sibling::expr[1]/SYMBOL
-      and not(SYMBOL_FORMALS = {paren_path}/OP-LEFT-PAREN/following-sibling::expr[position() > 1]//SYMBOL)
+      and (
+        ({ glue(symbol_loc_fmt, paren_path = 'expr') })
+        or ({ glue(symbol_loc_fmt, paren_path = 'expr[OP-LEFT-BRACE and count(expr) = 1]/expr[1]') })
+      )
     ]
-  "
-  default_fun_xpath <- paste(
-    sep = "|",
-    glue(default_fun_xpath_fmt, paren_path = "expr"),
-    glue(default_fun_xpath_fmt, paren_path = "expr[OP-LEFT-BRACE and count(expr) = 1]/expr[1]")
-  )
+  ")
 
   # purrr-style inline formulas-as-functions, e.g. ~foo(.x)
   # logic is basically the same as that above, except we need
