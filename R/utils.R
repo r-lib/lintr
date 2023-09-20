af0077a2a43bbaa21831cd159328354b6ac887d9
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-09-20 15:59:20 +0200

    Extract XML-related utilities into a separate file
    Since the `utils.R` is getting quite large.
    
    Similar to `xp_utils.R`.

--- R/utils.R
+++ R/utils.R
@@ -87,17 +87,6 @@ names2 <- function(x) {
   names(x) %||% rep("", length(x))
 }
 
-safe_parse_to_xml <- function(parsed_content) {
-  if (is.null(parsed_content)) {
-    return(xml2::xml_missing())
-  }
-  tryCatch(
-    xml2::read_xml(xmlparsedata::xml_parse_data(parsed_content)),
-    # use xml_missing so that code doesn't always need to condition on XML existing
-    error = function(e) xml2::xml_missing()
-  )
-}
-
 get_content <- function(lines, info) {
   lines[is.na(lines)] <- ""
 
@@ -246,18 +235,6 @@ get_r_string <- function(s, xpath = NULL) {
   out <- as.character(parse(text = s, keep.source = FALSE))
   is.na(out) <- is.na(s)
   out
-}
-
-#' str2lang, but for xml children.
-#'
-#' [xml2::xml_text()] is deceptively close to obviating this helper, but it collapses
-#'   text across lines. R is _mostly_ whitespace-agnostic, so this only matters in some edge cases,
-#'   in particular when there are comments within an expression (`<expr>` node). See #1919.
-#'
-#' @noRd
-xml2lang <- function(x) {
-  x_strip_comments <- xml_find_all(x, ".//*[not(self::COMMENT or self::expr)]")
-  str2lang(paste(xml_text(x_strip_comments), collapse = " "))
 }
 
 is_linter <- function(x) inherits(x, "linter")
---
+++ R/xml_utils.R
@@ -0,0 +1,25 @@
+# utils for working with XML
+
+#' str2lang, but for xml children.
+#'
+#' [xml2::xml_text()] is deceptively close to obviating this helper, but it collapses
+#'   text across lines. R is _mostly_ whitespace-agnostic, so this only matters in some edge cases,
+#'   in particular when there are comments within an expression (`<expr>` node). See #1919.
+#'
+#' @noRd
+xml2lang <- function(x) {
+  x_strip_comments <- xml_find_all(x, ".//*[not(self::COMMENT or self::expr)]")
+  str2lang(paste(xml_text(x_strip_comments), collapse = " "))
+}
+
+
+safe_parse_to_xml <- function(parsed_content) {
+  if (is.null(parsed_content)) {
+    return(xml2::xml_missing())
+  }
+  tryCatch(
+    xml2::read_xml(xmlparsedata::xml_parse_data(parsed_content)),
+    # use xml_missing so that code doesn't always need to condition on XML existing
+    error = function(e) xml2::xml_missing()
+  )
+}
