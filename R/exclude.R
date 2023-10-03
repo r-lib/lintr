898fecfb9e22ddee595e5e77deaeb3577ffd8305
Michael Chirico <chiricom@google.com>
2023-10-03 20:48:41 +0000

    grammar

--- R/exclude.R
+++ R/exclude.R
@@ -19,7 +19,7 @@
 #'   "   and the listing must be terminated with a full stop (`.`) for the linter list to be respected.",
 #'   "2. Line range in the source file. default: `# nolint start`, `# nolint end`. `# nolint start` accepts linter",
 #'   "   lists in the same form as `# nolint`.",
-#'   "3. Exclusions parameter, a list named and/or unnamed entries. If present, the name is a path relative to ",
+#'   "3. Exclusions parameter, a list with named and/or unnamed entries. If present, the name is a path relative to ",
 #'   "   the config. Moreover, elements have the following characteristics:",
 #'   "   1. Unnamed elements specify filenames or directories.",
 #'   "   2. Named elements are numeric or a list. If present, the name is a linter.",
