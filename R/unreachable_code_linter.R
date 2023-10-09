78952cfe0f9829ab800fef0bb73314e0389932f8
Michael Chirico <michaelchirico4@gmail.com>
2023-10-08 18:32:58 -0700

    switch order

--- R/unreachable_code_linter.R
+++ R/unreachable_code_linter.R
@@ -62,8 +62,8 @@ unreachable_code_linter <- function() {
   # NB: use not(OP-DOLLAR) to prevent matching process$stop(), #1051
   xpath_return_stop <- glue("
   (
-    (//FUNCTION | //OP-LAMBDA)/following-sibling::expr
-    | {expr_after_control}
+    {expr_after_control}
+    | (//FUNCTION | //OP-LAMBDA)/following-sibling::expr
   )
     /expr[expr[1][
       not(OP-DOLLAR or OP-AT)
