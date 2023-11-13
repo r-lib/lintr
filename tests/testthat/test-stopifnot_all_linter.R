530bc9229cae1d6eff131c32dcacb8ef4f2417d8
Michael Chirico <chiricom@google.com>
2023-11-13 15:18:02 +0000

    vectorized test

--- tests/testthat/test-stopifnot_all_linter.R
+++ tests/testthat/test-stopifnot_all_linter.R
@@ -3,15 +3,25 @@ test_that("stopifnot_all_linter skips allowed usages", {
 })
 
 test_that("stopifnot_all_linter blocks simple disallowed usages", {
-  expect_lint(
-    "stopifnot(all(A))",
-    list(rex::rex("stopifnot(x) runs all() 'under the hood'"), column_number = 11L),
-    stopifnot_all_linter()
-  )
+  linter <- stopifnot_all_linter()
+  lint_msg <- rex::rex("stopifnot(x) runs all() 'under the hood'")
+
+  expect_lint("stopifnot(all(A))", list(lint_msg, column_number = 11L), linter)
+  expect_lint("stopifnot(x, y, all(z))", list(lint_msg, column_number = 17L), linter)
 
   expect_lint(
-    "stopifnot(x, y, all(z))",
-    list(rex::rex("stopifnot(x) runs all() 'under the hood'"), column_number = 17L),
-    stopifnot_all_linter()
+    trim_some("{
+      stopifnot(all(x), all(y),
+        all(z)
+      )
+      stopifnot(a > 0, b < 0, all(c == 0))
+    }"),
+    list(
+      list(lint_msg, line_number = 2L, column_number = 13L),
+      list(lint_msg, line_number = 2L, column_number = 21L),
+      list(lint_msg, line_number = 3L, column_number = 5L),
+      list(lint_msg, line_number = 5L, column_number = 27L)
+    ),
+    linter
   )
 })
