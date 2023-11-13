ffff6777097a30a68468787dc41588ead3c1dbae
Michael Chirico <chiricom@google.com>
2023-11-13 17:41:50 +0000

    improve handling of operators inside lambdas

--- R/unnecessary_lambda_linter.R
+++ R/unnecessary_lambda_linter.R
@@ -76,8 +76,8 @@ unnecessary_lambda_linter <- function() {
           position() = 2
           and preceding-sibling::expr/SYMBOL_FUNCTION_CALL
           and not(preceding-sibling::*[1][self::EQ_SUB])
+          and not(parent::expr/following-sibling::*[not(self::OP-RIGHT-PAREN or self::OP-RIGHT-BRACE)])
         ]/SYMBOL
-      and count(OP-LEFT-PAREN) + count(OP-LEFT-BRACE/following-sibling::expr/OP-LEFT-PAREN) = 1
     ]
     /parent::expr
   ")
