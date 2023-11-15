7bac1434f389b9186fc4313c4fed76b03bdaf0fb
Michael Chirico <chiricom@google.com>
2023-11-15 06:22:10 +0000

    metadata test

--- tests/testthat/test-print_linter.R
+++ tests/testthat/test-print_linter.R
@@ -16,4 +16,20 @@ test_that("print_linter blocks disallowed usages", {
   expect_lint('print(paste(x, "b", y))', lint_msg, linter)
   expect_lint('print(paste0(x, "c", y))', lint_msg, linter)
   expect_lint('print(sprintf("a %s", x))', lint_msg, linter)
+
+  # vectorization + metadata
+  expect_lint(
+    trim_some("{
+      print('a')
+      print(paste('x', y))
+      print(z)
+      print(sprintf('%s', b))
+    }"),
+    list(
+      list(lint_msg, line_number = 2L, column_number = 3L),
+      list(lint_msg, line_number = 3L, column_number = 3L),
+      list(lint_msg, line_number = 5L, column_number = 3L)
+    ),
+    linter
+  )
 })
