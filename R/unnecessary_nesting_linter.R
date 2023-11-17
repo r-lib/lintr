c703315101501980235cb071f1f8b056f26f7032
Michael Chirico <chiricom@google.com>
2023-11-17 21:20:27 +0000

    New unnecessary_nesting_linter

---
+++ R/unnecessary_nesting_linter.R
@@ -0,0 +1,137 @@
+#' Block instances of excessive nesting
+#'
+#' Excessive nesting harms readability. Use helper functions or early returns
+#'   to reduce nesting wherever possible.
+#'
+#' @evalRd rd_tags("unnecessary_nesting_linter")
+#' @seealso
+#'  - [cyclocomp_linter()] for another linter that penalizes overly complexcode.
+#'  - [linters] for a complete list of linters available in lintr.
+#' @export
+unnecessary_nesting_linter <- function() {
+  exit_calls <- c("stop", "return", "abort", "quit", "q")
+  # These calls can be called in the sibling branch and not trigger a lint,
+  #   allowing for cleanly parallel code, where breaking it would often harm readability:
+  #   if (A) {
+  #     stop()
+  #   } else {
+  #     warning()
+  #   }
+  # NB: print() is intentionally excluded since its usage is usually a mistake (?print_linter)
+  signal_calls <- c(
+    exit_calls,
+    "warning", "warn", "message", "cat", "LOG", "stopifnot"
+  )
+  exit_call_expr <- glue("
+  expr[SYMBOL_FUNCTION_CALL[{xp_text_in_table(exit_calls)}]]
+  ")
+  # block IF here for cases where a nested if/else is entirely within
+  #   one of the branches.
+  # TODO(michaelchirico): we could try and make the parallel exits requirement
+  #   more recursive, but it's a pain to do so.
+  no_signal_call_expr <- glue("
+  expr[
+    OP-LEFT-BRACE
+    and expr[
+      position() = last()
+      and not(IF)
+      and not(expr[SYMBOL_FUNCTION_CALL[{xp_text_in_table(signal_calls)}]])
+    ]
+  ]
+  ")
+  # condition for ELSE should be redundant, but include for robustness
+  # condition on parent::expr[IF] ensures we're at the first `if` of a sequence of if/else statements
+  # condition on expr uses following-sibling or preceding-sibling to ensure
+  #   that the other expr falls on a different branch (earlier used separate
+  #   conditions on two expr[], but seems to allow any branch with >1 statement
+  #   to lead to a lint.
+  # use position() = last() to ignore any expr but the last one in any branch.
+  if_else_exit_xpath <- glue("
+  //expr[
+    IF
+    and ELSE
+    and not(parent::expr[IF])
+    and expr[
+      OP-LEFT-BRACE
+      and expr[position() = last() and {exit_call_expr}]
+      and (
+        following-sibling::{no_signal_call_expr}
+        or preceding-sibling::{no_signal_call_expr}
+      )
+    ]
+  ]
+  ")
+
+  # several carve-outs of common cases where single-expression braces are OK
+  #   - control flow statements: if, for, while, repeat, switch()
+  #       + switch() is unique in being a function, not a language element
+  #       + include foreach() as a common package-based for loop extension
+  #   - function definitions
+  #       + includes purrr-like anonymous functions as ~ {...}
+  #   - rlang's double-brace expressions like {{ var }}
+  #       + NB: both braces would trigger here, so we must exclude both of them
+  #   - any expression ending like `})` or `}]`
+  #       + note that nesting is not improved by "fixing" such cases,
+  #         and could also be worsened
+  #       + motivated by the most common cases:
+  #          * test_that("test", { expr })
+  #          * with(x, { expr }) / within(x, { expr })
+  #          * suppressWarnings({ expr })
+  #          * DataTable[, { expr }]
+  #          * DataTable[, col := { expr }] <- requires carve-out for `:=`
+  unnecessary_brace_xpath <- "
+  //OP-LEFT-BRACE
+    /parent::expr[
+      count(expr) = 1
+      and not(preceding-sibling::*[
+        self::FUNCTION
+        or self::FOR
+        or self::IF
+        or self::WHILE
+        or self::REPEAT
+        or self::expr/SYMBOL_FUNCTION_CALL[text() = 'switch']
+        or self::expr/expr/SYMBOL_FUNCTION_CALL[text() = 'foreach']
+        or self::OP-TILDE
+        or self::LEFT_ASSIGN[text() = ':=']
+      ])
+      and not(expr/OP-LEFT-BRACE)
+      and not(preceding-sibling::OP-LEFT-BRACE)
+      and not(
+        OP-RIGHT-BRACE/@end + 1 = following-sibling::OP-RIGHT-PAREN/@end
+        or OP-RIGHT-BRACE/@end + 1 = following-sibling::OP-RIGHT-BRACKET/@end
+      )
+    ]
+  "
+
+  Linter(function(source_expression) {
+    if (!is_lint_level(source_expression, "expression")) {
+      return(list())
+    }
+
+    xml <- source_expression$xml_parsed_content
+
+    if_else_exit_expr <- xml_find_all(xml, if_else_exit_xpath)
+    # TODO(michaelchirico): customize the error message to the exit clause used
+    if_else_exit_lints <- xml_nodes_to_lints(
+      if_else_exit_expr,
+      source_expression = source_expression,
+      lint_message = paste0(
+        "Reduce the nesting of this if/else statement by unnesting the ",
+        "portion without an exit clause (i.e., ",
+        paste0(exit_calls, "()", collapse = ", "),
+        ")."
+      ),
+      type = "warning"
+    )
+
+    unnecessary_brace_expr <- xml_find_all(xml, unnecessary_brace_xpath)
+    unnecessary_brace_lints <- xml_nodes_to_lints(
+      unnecessary_brace_expr,
+      source_expression = source_expression,
+      lint_message = "Reduce the nesting of this statement by removing the braces {}.",
+      type = "warning"
+    )
+
+    c(if_else_exit_lints, unnecessary_brace_lints)
+  })
+}
---
+++ tests/testthat/test-unnecessary_nesting_linter.R
@@ -0,0 +1,285 @@
+test_that("unnecessary_nesting_linter skips allowed usages", {
+  linter <- unnecessary_nesting_linter()
+
+  # parallel stops() and return()s are OK
+  double_stop_lines <- c(
+    "if (A) {",
+    " stop()",
+    "} else {",
+    " stop()",
+    "}"
+  )
+  expect_lint(double_stop_lines, NULL, linter)
+
+  double_return_lines <- c(
+    "if (A) {",
+    " return()",
+    "} else {",
+    " return()",
+    "}"
+  )
+  expect_lint(double_return_lines, NULL, linter)
+})
+
+test_that("parallel stop()/warning() branches are OK", {
+  stop_warning_lines <- c(
+    "if (i == force.iter) {",
+    "  stop(msg, call. = FALSE)",
+    "} else {",
+    "  warning(attempt, call. = FALSE)",
+    "}"
+  )
+  expect_lint(stop_warning_lines, NULL, unnecessary_nesting_linter())
+})
+
+# TODO(michaelchirico): consider if there's a nice easy pattern to enforce for
+#   multiple if/else cases. This test in particular would be easy to un-nest,
+#   but it's not true in general.
+test_that("Multiple if/else statements don't require unnesting", {
+  # with further branches, reducing nesting might be less readable
+  if_else_if_else_lines <- c(
+    "if (x == 'a') {",
+    "  stop()",
+    "} else if (x == 'b') {",
+    "  do_b()",
+    "} else {",
+    "  stop()",
+    "}"
+  )
+  expect_lint(if_else_if_else_lines, NULL, unnecessary_nesting_linter())
+})
+
+test_that("else-less if statements don't lint", {
+  multi_statement_if_lines <- c(
+    "if (x == 4) {",
+    "  msg <- 'failed'",
+    "  stop(msg)",
+    "}"
+  )
+  expect_lint(multi_statement_if_lines, NULL, unnecessary_nesting_linter())
+})
+
+test_that("non-terminal expressions are not considered for the logic", {
+  multi_statement_if_lines <- c(
+    "if (x == 4) {",
+    "  x <- 5",
+    "  return(x)",
+    "} else {",
+    "  return(x)",
+    "}"
+  )
+  expect_lint(multi_statement_if_lines, NULL, unnecessary_nesting_linter())
+})
+
+test_that("parallels in further nesting are skipped", {
+  terminal_if_else_lines <- c(
+    "if (length(bucket) > 1) {",
+    "  return(age)",
+    "} else {",
+    "  if (grepl('[0-9]', age)) {",
+    "    return(age)",
+    "  } else {",
+    "    return('unknown')",
+    "  }",
+    "}"
+  )
+  expect_lint(terminal_if_else_lines, NULL, unnecessary_nesting_linter())
+})
+
+test_that("unnecessary_nesting_linter blocks if/else with one exit branch", {
+  linter <- unnecessary_nesting_linter()
+
+  if_stop_lines <- c(
+    "if (A) {",
+    "  stop()",
+    "} else {",
+    "  B",
+    "}"
+  )
+  expect_lint(
+    if_stop_lines,
+    rex::rex("Reduce the nesting of this if/else statement by unnesting the portion"),
+    linter
+  )
+
+  if_return_lines <- c(
+    "if (A) {",
+    "  return()",
+    "} else {",
+    "  B",
+    "}"
+  )
+  expect_lint(
+    if_return_lines,
+    rex::rex("Reduce the nesting of this if/else statement by unnesting the portion"),
+    linter
+  )
+
+  # also find exits in the later branch
+  else_stop_lines <- c(
+    "if (A) {",
+    "  B",
+    "} else {",
+    "  stop()",
+    "}"
+  )
+  expect_lint(
+    else_stop_lines,
+    rex::rex("Reduce the nesting of this if/else statement by unnesting the portion"),
+    linter
+  )
+
+  else_return_lines <- c(
+    "if (A) {",
+    "  B",
+    "} else {",
+    "  return()",
+    "}"
+  )
+  expect_lint(
+    else_return_lines,
+    rex::rex("Reduce the nesting of this if/else statement by unnesting the portion"),
+    linter
+  )
+})
+
+test_that("unnecessary_nesting_linter skips one-line functions", {
+  linter <- unnecessary_nesting_linter()
+
+  anonymous_function_lines <- c(
+    "foo <- function(x) {",
+    "  return(x)",
+    "}"
+  )
+  expect_lint(anonymous_function_lines, NULL, linter)
+
+  # purrr anonymous functions also get skipped
+  purrr_function_lines <- c(
+    "purrr::map(x, ~ {",
+    "  .x",
+    "})"
+  )
+  expect_lint(purrr_function_lines, NULL, linter)
+})
+
+test_that("unnecessary_nesting_linter skips one-expression for loops", {
+  linter <- unnecessary_nesting_linter()
+
+  for_lines <- c(
+    "for (i in 1:10) {",
+    "  print(i)",
+    "}"
+  )
+  expect_lint(for_lines, NULL, linter)
+
+  # also for extended control flow functionality from packages
+  foreach_lines <- c(
+    "foreach (i = 1:10) %dopar% {",
+    "  print(i)",
+    "}"
+  )
+  expect_lint(foreach_lines, NULL, linter)
+})
+
+test_that("unnecessary_nesting_linter skips one-expression if and else clauses", {
+  lines <- c(
+    "if (TRUE) {",
+    "  x",
+    "} else {",
+    "  y",
+    "}"
+  )
+  expect_lint(lines, NULL, unnecessary_nesting_linter())
+})
+
+test_that("unnecessary_nesting_linter skips one-expression while loops", {
+  lines <- c(
+    "while (x < 10) {",
+    "  x <- x + 1",
+    "}"
+  )
+  expect_lint(lines, NULL, unnecessary_nesting_linter())
+})
+
+test_that("unnecessary_nesting_linter skips one-expression repeat loops", {
+  lines <- c(
+    "repeat {",
+    "  x <- x + 1",
+    "}"
+  )
+  expect_lint(lines, NULL, unnecessary_nesting_linter())
+})
+
+test_that("unnecessary_nesting_linter skips one-expression switch statements", {
+  lines <- c(
+    "switch(x,",
+    "  a = {",
+    "    do_a()",
+    "  },",
+    "  b = {",
+    "    do_b()",
+    "  }",
+    ")"
+  )
+  expect_lint(lines, NULL, unnecessary_nesting_linter())
+})
+
+test_that("unnecessary_nesting_linter passes for multi-line braced expressions", {
+  lines <- c(
+    "tryCatch(",
+    "  {",
+    "    foo(x)",
+    "    bar(x)",
+    "  },",
+    "  error = identity",
+    ")"
+  )
+  expect_lint(lines, NULL, unnecessary_nesting_linter())
+})
+
+test_that("unnecessary_nesting_linter skips if unbracing won't reduce nesting", {
+  linter <- unnecessary_nesting_linter()
+
+  test_that_lines <- c(
+    "test_that('this works', {",
+    "  expect_true(TRUE)",
+    "})"
+  )
+  expect_lint(test_that_lines, NULL, linter)
+  data_table_lines <- c(
+    "DT[, {",
+    "  plot(x, y)",
+    "}]"
+  )
+  expect_lint(data_table_lines, NULL, linter)
+  data_table_assign_lines <- c(
+    "DT[, x := {",
+    "  foo(x, y)",
+    "}]"
+  )
+  expect_lint(data_table_assign_lines, NULL, linter)
+})
+
+test_that("rlang's double-brace operator is skipped", {
+  expect_lint(
+    "rename(DF, col = {{ val }})",
+    NULL,
+    unnecessary_nesting_linter()
+  )
+})
+
+test_that("unnecessary_nesting_linter blocks one-expression braced expressions", {
+  lines <- c(
+    "tryCatch(",
+    "  {",
+    "    foo(x)",
+    "  },",
+    "  error = identity",
+    ")"
+  )
+  expect_lint(
+    lines,
+    R"(Reduce the nesting of this statement by removing the braces \{\}\.)",
+    unnecessary_nesting_linter()
+  )
+})
