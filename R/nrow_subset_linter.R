afc20d675eee1251a326c027d69dee93ccc1836a
Michael Chirico <michaelchirico4@gmail.com>
2023-11-18 12:04:38 -0800

    documentation feedback

--- R/nrow_subset_linter.R
+++ R/nrow_subset_linter.R
@@ -6,9 +6,6 @@
 #' There are a number of equivalent expressions that don't require the full
 #'   subset, e.g. `with(x, sum(condition))` (or, more generically,
 #'   `with(x, sum(condition, na.rm = TRUE))`).
-#' The same can be said of other versions of this like
-#'   `nrow(DT[(condition)])` for subsetting a `data.table` or
-#'   `DT %>% filter(condition) %>% nrow()`.
 #'
 #' @examples
 #' # will produce lints
@@ -35,7 +32,7 @@ nrow_subset_linter <- make_linter_from_xpath(
   ",
   lint_message = paste(
     "Use arithmetic to count the number of rows satisfying a condition,",
-    "rather than fully subsetting the table and counting the resulting rows.",
+    "rather than fully subsetting the data.frame and counting the resulting rows.",
     "For example, replace nrow(subset(x, is_treatment))",
     "with sum(x$is_treatment). NB: use na.rm = TRUE if `is_treatment` has",
     "missing values."
