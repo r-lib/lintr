445800aeec78ff7f60aa4bbfc17f99b041e7bdd7
Michael Chirico <michaelchirico4@gmail.com>
2023-11-18 17:35:57 -0800

    use trim_some() in test

--- tests/testthat/test-pipe_return_linter.R
+++ tests/testthat/test-pipe_return_linter.R
@@ -1,44 +1,44 @@
 test_that("pipe_return_linter skips allowed usages", {
   linter <- pipe_return_linter()
 
-  normal_pipe_lines <- c(
-    "x %>%",
-    "  filter(str > 5) %>%",
-    "  summarize(str = sum(str))"
-  )
+  normal_pipe_lines <- trim_some("
+    x %>%
+      filter(str > 5) %>%
+      summarize(str = sum(str))
+  ")
   expect_lint(normal_pipe_lines, NULL, linter)
 
-  normal_function_lines <- c(
-    "pipeline <- function(x) {",
-    "  out <- x %>%",
-    "    filter(str > 5) %>%",
-    "    summarize(str = sum(str))",
-    "  return(out)",
-    "}"
-  )
+  normal_function_lines <- trim_some("
+    pipeline <- function(x) {
+      out <- x %>%
+        filter(str > 5) %>%
+        summarize(str = sum(str))
+      return(out)
+    }
+  ")
   expect_lint(normal_function_lines, NULL, linter)
 
-  nested_return_lines <- c(
-    "pipeline <- function(x) {",
-    "  x_squared <- x %>%",
-    "    sapply(function(xi) {",
-    "      return(xi ** 2)",
-    "    })",
-    "  return(x_squared)",
-    "}"
-  )
+  nested_return_lines <- trim_some("
+    pipeline <- function(x) {
+      x_squared <- x %>%
+        sapply(function(xi) {
+          return(xi ** 2)
+        })
+      return(x_squared)
+    }
+  ")
   expect_lint(nested_return_lines, NULL, linter)
 })
 
 test_that("pipe_return_linter blocks simple disallowed usages", {
-  lines <- c(
-    "pipeline <- function(x) {",
-    "  out <- x %>%",
-    "    filter(str > 5) %>%",
-    "    summarize(str = sum(str)) %>%",
-    "    return()",
-    "}"
-  )
+  lines <- trim_some("
+    pipeline <- function(x) {
+      out <- x %>%
+        filter(str > 5) %>%
+        summarize(str = sum(str)) %>%
+        return()
+    }
+  ")
   expect_lint(
     lines,
     rex::rex("Using return() as the final step of a magrittr pipeline"),
