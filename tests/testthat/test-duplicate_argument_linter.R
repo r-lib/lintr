3f1b81c4a0f13b3c2499680805f855e4f352f554
Michael Chirico <chiricom@google.com>
2023-12-06 15:04:36 +0000

    fix for #2402

--- tests/testthat/test-duplicate_argument_linter.R
+++ tests/testthat/test-duplicate_argument_linter.R
@@ -21,10 +21,12 @@ test_that("duplicate_argument_linter blocks disallowed usages", {
   expect_lint("dt[i = 1, i = 2]", lint_msg, linter)
 
   expect_lint(
-    "list(
-      var = 1,
-      var = 2
-    )",
+    trim_some("
+      list(
+        var = 1,
+        var = 2
+      )
+    "),
     lint_msg,
     linter
   )
@@ -93,5 +95,67 @@ test_that("doesn't lint duplicated arguments in allowed functions", {
     )",
     NULL,
     linter
+  )
+})
+
+test_that("interceding comments don't trip up logic", {
+  linter <- duplicate_argument_linter()
+  lint_msg <- rex::rex("Avoid duplicate arguments")
+
+  # actually this case "just works" even before #2402 since
+  #   get_r_string() returns NA for both argument names
+  expect_lint(
+    trim_some("
+      fun(
+        arg # xxx
+        = 1,
+        arg # yyy
+        = 2
+      )
+    "),
+    list(lint_msg, line_number = 4L),
+    linter
+  )
+
+  expect_lint(
+    trim_some("
+      fun(
+        arg # xxx
+        = 1,
+        arg = 2
+      )
+    "),
+    list(lint_msg, line_number = 4L),
+    linter
+  )
+
+  expect_lint(
+    trim_some("
+      fun(
+        arg = 1,
+        arg # yyy
+        = 2
+      )
+    "),
+    list(lint_msg, line_number = 3L),
+    linter
+  )
+})
+
+test_that("lints vectorize", {
+  lint_msg <- rex::rex("Avoid duplicate arguments")
+
+  expect_lint(
+    trim_some("{
+      c(a = 1, a = 2, a = 3)
+      list(b = 1, b = 2, b = 3)
+    }"),
+    list(
+      list(lint_msg, line_number = 2L, column_number = 12L),
+      list(lint_msg, line_number = 2L, column_number = 19L),
+      list(lint_msg, line_number = 3L, column_number = 15L),
+      list(lint_msg, line_number = 3L, column_number = 22L)
+    ),
+    duplicate_argument_linter()
   )
 })
