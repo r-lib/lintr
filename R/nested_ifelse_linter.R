f63cd18984f194dd5a164dc10227ce4b4744b6c6
Michael Chirico <chiricom@google.com>
2023-12-06 01:28:03 +0000

    one more revert

--- R/nested_ifelse_linter.R
+++ R/nested_ifelse_linter.R
@@ -94,8 +94,8 @@ nested_ifelse_linter <- function() {
 
     matched_call <- xp_call_name(bad_expr)
     lint_message <- paste(
-      sprintf("Avoid nested %s() calls.", matched_call),
-      "Instead, try (1) data.table::fcase(); (2) dplyr::case_when(); or (3) using a lookup table."
+      sprintf("Don't use nested %s() calls;", matched_call),
+      "instead, try (1) data.table::fcase; (2) dplyr::case_when; or (3) using a lookup table."
     )
     xml_nodes_to_lints(bad_expr, source_expression, lint_message, type = "warning")
   })
--- tests/testthat/test-nested_ifelse_linter.R
+++ tests/testthat/test-nested_ifelse_linter.R
@@ -11,28 +11,51 @@ test_that("nested_ifelse_linter skips allowed usages", {
   expect_lint("case_when(l1 ~ v1, l2 ~ v2)", NULL, linter)
 })
 
-local({
-  ifelse_calls <- c("ifelse", "if_else", "fifelse")
+test_that("nested_ifelse_linter blocks simple disallowed usages", {
+  expect_lint(
+    "ifelse(l1, v1, ifelse(l2, v2, v3))",
+    rex::rex("Don't use nested ifelse() calls"),
+    nested_ifelse_linter()
+  )
 
-  linter <- nested_ifelse_linter()
-
-  patrick::with_parameters_test_that(
-    "nested_ifelse_linter blocks simple disallowed usages",
-    {
-      lint_msg <- rex::rex("Avoid nested ", ifelse_call, "() calls")
-
-      expect_lint(glue::glue("{ifelse_call}(l1, v1, {ifelse_call}(l2, v2, v3))"), lint_msg, linter)
-      expect_lint(glue::glue("{ifelse_call}(l1, {ifelse_call}(l2, v1, v2), v3)"), lint_msg, linter)
-    },
-    ifelse_call = ifelse_calls
+  expect_lint(
+    "ifelse(l1, ifelse(l2, v1, v2), v3)",
+    rex::rex("Don't use nested ifelse() calls"),
+    nested_ifelse_linter()
   )
 })
 
-test_that("nested_ifelse_linter also catches mixed usage", {
+test_that("nested_ifelse_linter also catches dplyr::if_else", {
+  expect_lint(
+    "if_else(l1, v1, if_else(l2, v2, v3))",
+    rex::rex("Don't use nested if_else() calls"),
+    nested_ifelse_linter()
+  )
+
+  expect_lint(
+    "dplyr::if_else(l1, dplyr::if_else(l2, v1, v2), v3)",
+    rex::rex("Don't use nested if_else() calls"),
+    nested_ifelse_linter()
+  )
+})
+
+test_that("nested_ifelse_linter also catches data.table::fifelse", {
+  expect_lint(
+    "fifelse(l1, v1, fifelse(l2, v2, v3))",
+    rex::rex("Don't use nested fifelse() calls"),
+    nested_ifelse_linter()
+  )
+
+  expect_lint(
+    "data.table::fifelse(l1, v1, data.table::fifelse(l2, v2, v3))",
+    rex::rex("Don't use nested fifelse() calls"),
+    nested_ifelse_linter()
+  )
+
   # not sure why anyone would do this, but the readability still argument holds
   expect_lint(
     "data.table::fifelse(l1, dplyr::if_else(l2, v1, v2), v3)",
-    rex::rex("Avoid nested if_else() calls"),
+    rex::rex("Don't use nested if_else() calls"),
     nested_ifelse_linter()
   )
 })
