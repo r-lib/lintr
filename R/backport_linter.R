a680b56f37cb9e80ea14eae19458b6d46f6f6567
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-12-11 08:14:37 +0100

    start with backport linter

--- R/backport_linter.R
+++ R/backport_linter.R
@@ -90,7 +90,7 @@ normalize_r_version <- function(r_version) {
     version_names <- c("devel", "release", paste0("oldrel-", seq_len(length(minor_versions) - 2L)))
     if (!r_version %in% version_names) {
       # This can only trip if e.g. oldrel-99 is requested
-      stop("`r_version` must be a version number or one of ", toString(sQuote(version_names)), call. = FALSE)
+      cli::cli_abort("{.var r_version} must be a version number or one of {toString(sQuote(version_names))}")
     }
     requested_version <- minor_versions[match(r_version, table = version_names)]
     available_patches <- all_versions[startsWith(all_versions, requested_version)]
@@ -102,13 +102,10 @@ normalize_r_version <- function(r_version) {
   } else if (is.character(r_version)) {
     r_version <- R_system_version(r_version, strict = TRUE)
   } else if (!inherits(r_version, "R_system_version")) {
-    stop("`r_version` must be a R version number, returned by R_system_version(), or a string.", call. = FALSE)
+    cli::cli_abort("{.var r_version} must be an R version number, returned by {.fun R_system_version}, or a string.")
   }
   if (r_version < "3.0.0") {
-    warning(
-      "It is not recommended to depend on an R version older than 3.0.0. Resetting 'r_version' to 3.0.0.",
-      call. = FALSE
-    )
+    cli::cli_warn("Depending on an R version older than 3.0.0 is not recommended. Resetting {.var r_version} to 3.0.0.")
     r_version <- R_system_version("3.0.0")
   }
   r_version
