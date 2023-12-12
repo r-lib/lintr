fa94235954c9cd42cfc06708589eb5ad964f7bc3
Michael Chirico <chiricom@google.com>
2023-12-12 15:35:56 +0000

    customize lint message

--- R/nzchar_linter.R
+++ R/nzchar_linter.R
@@ -55,7 +55,7 @@ nzchar_linter <- function() {
         ])
         or ancestor-or-self::expr[
           (
-            preceding-sibling::expr[SYMBOL_FUNCTION_CALL]
+            preceding-sibling::expr/SYMBOL_FUNCTION_CALL
             or preceding-sibling::OP-LEFT-BRACKET
           ) and not(
             descendant-or-self::expr[IF or WHILE]
@@ -65,6 +65,15 @@ nzchar_linter <- function() {
     ]
   ")
 
+  comparison_msg_map <- c(
+    GT = 'Use nzchar(x) instead of x > "". ',
+    NE = 'Use nzchar(x) instead of x != "". ',
+    LE = 'Use !nzchar(x) instead of x <= "". ',
+    EQ = 'Use !nzchar(x) instead of x == "". ',
+    GE = 'x >= "" is always true, maybe you want nzchar(x)? ',
+    LT = 'x < "" is always false, maybe you want !nzchar(x)? '
+  )
+
   # nchar(., type="width") not strictly compatible with nzchar
   # unsure allowNA compatible, so allow it just in case (see TODO in tests)
   nchar_xpath <- glue("
@@ -73,18 +82,27 @@ nzchar_linter <- function() {
     /parent::expr
     /parent::expr[
       ({ xp_or(comparator_nodes) })
-      and not(expr[SYMBOL_SUB[
+      and not(expr/SYMBOL_SUB[
         (
           text() = 'type'
-          and following-sibling::expr[1][STR_CONST[contains(text(), 'width')]]
+          and following-sibling::expr[1]/STR_CONST[contains(text(), 'width')]
         ) or (
           text() = 'allowNA'
-          and following-sibling::expr[1][NUM_CONST[text() = 'TRUE']]
+          and following-sibling::expr[1]/NUM_CONST[text() = 'TRUE']
         )
-      ]])
-      and expr[NUM_CONST[text() = '0' or text() = '0L' or text() = '0.0']]
+      ])
+      and expr/NUM_CONST[text() = '0' or text() = '0L' or text() = '0.0']
     ]
   ")
+
+  nchar_msg_map <- c(
+    GT = 'Use nzchar(x) instead of nchar(x) > 0. ',
+    NE = 'Use nzchar(x) instead of nchar(x) != 0. ',
+    LE = 'Use !nzchar(x) instead of nchar(x) <= 0. ',
+    EQ = 'Use !nzchar(x) instead of nchar(x) == 0. ',
+    GE = 'nchar(x) >= 0 is always true, maybe you want nzchar(x)? ',
+    LT = 'nchar(x) < 0 is always false, maybe you want !nzchar(x)? '
+  )
 
   keepna_note <- paste(
     "Whenever missing data is possible,",
@@ -92,30 +110,46 @@ nzchar_linter <- function() {
     "nzchar(NA) is TRUE by default."
   )
 
+  # For ordered operators like '>', we need to give the message for
+  #   its "opposite" (not inverse) if the bad usage is on the RHS,
+  #   e.g. 0 < nchar(x) has to be treated as nchar(x) > 0.
+  op_for_msg <- function(expr, const) {
+    op <- xml_name(xml_find_first(expr, "*[2]"))
+    maybe_needs_flip <- !is.na(xml_find_first(expr, sprintf("*[1][%s]", const)))
+
+    ordered_ops <- c("GT", "GE", "LE", "LT")
+    ordered_idx <- match(op, ordered_ops)
+
+    needs_flip <- maybe_needs_flip & !is.na(ordered_idx)
+    # un-benchmarked, but should be faster (though less readable) as
+    #   ordered_ops[5L - ordered_idx[needs_flip]]
+    op[needs_flip] <- rev(ordered_ops)[ordered_idx[needs_flip]]
+    op
+  }
+
   Linter(linter_level = "expression", function(source_expression) {
     xml <- source_expression$xml_parsed_content
 
     comparison_expr <- xml_find_all(xml, comparison_xpath)
+    comparison_op <- op_for_msg(comparison_expr, const = "STR_CONST")
     comparison_lints <- xml_nodes_to_lints(
       comparison_expr,
       source_expression = source_expression,
-      lint_message = paste(
-        'Use nzchar() instead of comparing strings to "".',
-        "Note that if x is a factor, you'll have use ",
-        'as.character() to replicate an implicit conversion that happens in x == "".',
+      lint_message = paste0(
+        comparison_msg_map[comparison_op],
+        "Note that unlike nzchar(), ", comparison_op, " coerces to character, ",
+        "so you'll have to use as.character() if x is a factor. ",
         keepna_note
       ),
       type = "warning"
     )
 
     nchar_expr <- xml_find_all(xml, nchar_xpath)
+    nchar_op <- op_for_msg(nchar_expr, const = "NUM_CONST")
     nchar_lints <- xml_nodes_to_lints(
       nchar_expr,
       source_expression = source_expression,
-      lint_message = paste(
-        "Use nzchar() instead of comparing nchar(x) to 0.",
-        keepna_note
-      ),
+      lint_message = paste0(nchar_msg_map[nchar_op], keepna_note),
       type = "warning"
     )
 
