11cd69cda58baef1642a96dde011e8265db902c4
Michael Chirico <chiricom@google.com>
2023-12-14 21:09:20 -0800

    expression level

--- R/todo_comment_linter.R
+++ R/todo_comment_linter.R
@@ -43,7 +43,7 @@
 todo_comment_linter <- function(todo = c("todo", "fixme")) {
   todo_comment_regex <- rex(one_or_more("#"), any_spaces, or(todo))
 
-  Linter(linter_level = "file", function(source_expression) {
+  Linter(linter_level = "expression", function(source_expression) {
     xml <- source_expression$full_xml_parsed_content
 
     comment_expr <- xml_find_all(xml, "//COMMENT")
