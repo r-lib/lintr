331f31ae80e2b82c1f4a3daa3122c61867a8010c
Michael Chirico <chiricom@google.com>
2023-12-17 17:15:52 -0800

    Run in several locales, print nicer failure

--- .dev/roxygen_test.R
+++ .dev/roxygen_test.R
@@ -5,23 +5,9 @@ library(roxygen2)
 old_dir <- file.path(tempdir(), "man")
 if (dir.exists(old_dir)) unlink(old_dir, recursive = TRUE)
 file.copy("man", tempdir(), recursive = TRUE)
+old_files <- list.files(old_dir, pattern = "\\.Rd$")
 new_dir <- "man"
 .Last <- function() unlink(old_dir, recursive = TRUE)
-
-roxygenize()
-
-old_files <- list.files(old_dir, pattern = "\\.Rd$")
-new_files <- list.files(new_dir, pattern = "\\.Rd$")
-
-old_not_new <- setdiff(old_files, new_files)
-if (length(old_not_new) > 0L) {
-  stop("Found saved .Rd files gone from a fresh run of roxygenize(): ", toString(old_not_new))
-}
-
-new_not_old <- setdiff(new_files, old_files)
-if (length(new_not_old) > 0L) {
-  stop("Found new .Rd files from a fresh run of roxygenize(): ", toString(new_not_old))
-}
 
 # Rd2txt() prints to its out= argument, so we'd have to compare file contents;
 #   plain parse_Rd() keeps srcref info that encodes the file path, which as.character() strips.
@@ -29,11 +15,40 @@ normalize_rd <- function(rd_file) as.character(parse_Rd(rd_file))
 
 rd_equal <- function(f1, f2) isTRUE(all.equal(normalize_rd(f1), normalize_rd(f2)))
 
-for (file in new_files) {
-  if (rd_equal(file.path(old_dir, file), file.path(new_dir, file))) {
-    next
+check_roxygenize_idempotent <- function() {
+  roxygenize()
+  
+  new_files <- list.files(new_dir, pattern = "\\.Rd$")
+  
+  old_not_new <- setdiff(old_files, new_files)
+  if (length(old_not_new) > 0L) {
+    stop("Found saved .Rd files gone from a fresh run of roxygenize(): ", toString(old_not_new))
   }
-  stop("roxygenize() output differs from saved output for ", file, ".")
+  
+  new_not_old <- setdiff(new_files, old_files)
+  if (length(new_not_old) > 0L) {
+    stop("Found new .Rd files from a fresh run of roxygenize(): ", toString(new_not_old))
+  }
+    
+  for (file in new_files) {
+    old_file <- file.path(old_dir, file)
+    new_file <- file.path(new_dir, file)
+    if (rd_equal(old_file, new_file)) {
+      next
+    }
+    cat(sprintf("roxygenize() output differs from saved output for %s.\n", file))
+    cat("Here's the Rd2txt() output for what's saved:\n")
+    Rd2txt(old_file)
+    cat("Here's the Rd2txt() output for roxygenize():\n")
+    Rd2txt(new_file)
+    stop(call. = FALSE)
+  }
+}
+
+# Run the check in a few locales to ensure there's no idempotency issues w.r.t. sorting, too
+for (LOCALE in c("C", "en_US", "hu_HU", "ja_JP")) {
+  Sys.setlocale("LC_COLLATE", LOCALE)
+  check_roxygenize_idempotent()
 }
 
 unlink(old_dir, recursive = TRUE)
