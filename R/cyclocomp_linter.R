10945c2c164e1bcf556ffcaebd3c52664ee43868
Indrajeet Patil <patilindrajeet.science@gmail.com>
2024-04-14 07:31:42 +0200

    error if cyclocomp is not installed

--- R/cyclocomp_linter.R
+++ R/cyclocomp_linter.R
@@ -22,6 +22,12 @@
 #' @seealso [linters] for a complete list of linters available in lintr.
 #' @export
 cyclocomp_linter <- function(complexity_limit = 15L) {
+  # nocov start
+  if (!requireNamespace("cyclocomp", quietly = TRUE)) {
+    cli::cli_abort("'cyclocomp' package needs to be installed for this linter.")
+  }
+  # nocov end
+
   Linter(linter_level = "expression", function(source_expression) {
     complexity <- try_silently(
       cyclocomp::cyclocomp(parse(text = source_expression$content))
