b0a24238de9c1593db333652aec9b6c9d17693cf
Indrajeet Patil <patilindrajeet.science@gmail.com>
2024-08-04 23:32:48 +0200

    more inlining

--- R/methods.R
+++ R/methods.R
@@ -98,15 +98,12 @@ print.lints <- function(x, ...) {
       lapply(x, print, ...)
     }
 
-    if (isTRUE(settings$error_on_lint)) {
-      quit("no", 31L, FALSE) # nocov
-    }
+    if (isTRUE(settings$error_on_lint)) quit("no", 31L, FALSE) # nocov
   }
 
   if (length(x) == 0L) {
-    cli_inform(c(i = "No lints found."))
-    # Empty lints: clear RStudio source markers
-    if (use_rstudio_source_markers) rstudio_source_markers(x)
+    cli_inform(c("i" = "No lints found."))
+    if (use_rstudio_source_markers) rstudio_source_markers(x) # clear RStudio source markers
   }
 
   invisible(x)
