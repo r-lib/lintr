02bb1e557ba343855e51063c9387b36597110c37
Indrajeet Patil <patilindrajeet.science@gmail.com>
2024-11-05 22:39:23 +0100

    combine linters

--- R/which_linter.R
+++ R/which_linter.R
@@ -1,16 +1,42 @@
-#' Require usage of grep over which(grepl(.))
+#' Find suboptimal usages involving `which()`
 #'
-#' `which(grepl(pattern, x))` is the same as `grep(pattern, x)`, but harder
-#'   to read and requires two passes over the vector.
+#' @description
+#'
+#' - `which.min(x)` and `which.max(x)` are optimized functions that directly return
+#' the index of the minimum or maximum value in a vector. They are more
+#' efficient and cleaner than using `which(min(x))` or `which(max(x))`.
+#'
+#' - `which(grepl(pattern, x))` is the same as `grep(pattern, x)`, but harder
+#' to read and requires two passes over the vector.
 #'
 #' @examples
 #' # will produce lints
+#' lint(
+#'   text = "which(x == max(x))",
+#'   linters = unnecessary_which_linter()
+#' )
+#'
+#' lint(
+#'   text = "which(x == min(x))",
+#'   linters = unnecessary_which_linter()
+#' )
+#'
 #' lint(
 #'   text = "which(grepl('^a', x))",
 #'   linters = which_linter()
 #' )
 #'
 #' # okay
+#' lint(
+#'   text = "which.max(x))",
+#'   linters = unnecessary_which_linter()
+#' )
+#'
+#' lint(
+#'   text = "which.min(x))",
+#'   linters = unnecessary_which_linter()
+#' )
+#'
 #' lint(
 #'   text = "which(grepl('^a', x) | grepl('^b', x))",
 #'   linters = which_linter()
@@ -19,12 +45,55 @@
 #' @evalRd rd_tags("which_linter")
 #' @seealso [linters] for a complete list of linters available in lintr.
 #' @export
-which_linter <- make_linter_from_function_xpath(
-  function_names = "grepl",
-  xpath = "
-  parent::expr
-    /parent::expr
-    /parent::expr[expr/SYMBOL_FUNCTION_CALL[text() = 'which']]
-  ",
-  lint_message = "grep(pattern, x) is better than which(grepl(pattern, x))."
-)
+which_linter <- function() {
+  which_function_xpath <- function(func_name) {
+    glue("
+    //expr[
+      expr[SYMBOL_FUNCTION_CALL[text()='which']]
+      and expr/expr[
+        expr[SYMBOL_FUNCTION_CALL[text()='{func_name}']]
+      ]
+    ]
+  ")
+  }
+
+  which_min_xpath <- which_function_xpath("min")
+  which_max_xpath <- which_function_xpath("max")
+  which_grepl_xpath <- glue("
+    //expr[
+      expr[SYMBOL_FUNCTION_CALL[text()='which']]
+      and
+      expr/expr[SYMBOL_FUNCTION_CALL[text()='grepl']]
+    ]
+  ")
+
+  Linter(linter_level = "expression", function(source_expression) {
+    xml <- source_expression$xml_parsed_content
+
+    which_min_expr <- xml_find_all(xml, which_min_xpath)
+    which_min_lints <- xml_nodes_to_lints(
+      which_min_expr,
+      source_expression = source_expression,
+      lint_message = "which.min(x) is more efficient than which(x == min(x)).",
+      type = "warning"
+    )
+
+    which_max_expr <- xml_find_all(xml, which_max_xpath)
+    which_max_lints <- xml_nodes_to_lints(
+      which_max_expr,
+      source_expression = source_expression,
+      lint_message = "which.max(x) is more efficient than which(x == max(x)).",
+      type = "warning"
+    )
+
+    which_grepl_expr <- xml_find_all(xml, which_grepl_xpath)
+    which_grepl_lints <- xml_nodes_to_lints(
+      which_grepl_expr,
+      source_expression = source_expression,
+      lint_message = "grep(pattern, x) is better than which(grepl(pattern, x)).",
+      type = "warning"
+    )
+
+    c(which_min_lints, which_max_lints, which_grepl_lints)
+  })
+}
---
+++ tests/testthat/test-which_linter.R
@@ -0,0 +1,77 @@
+test_that("which_linter skips allowed usages", {
+  # this _could_ be combined as p1|p2, but often it's cleaner to read this way
+  expect_lint("which(grepl(p1, x) | grepl(p2, x))", NULL, which_linter())
+})
+
+test_that("which_linter blocks simple disallowed usages", {
+  linter <- which_linter()
+  lint_msg <- rex::rex("grep(pattern, x) is better than which(grepl(pattern, x)).")
+
+  expect_lint("which(grepl('^a', x))", lint_msg, linter)
+  # options also don't matter (grep has more arguments: value, invert)
+  expect_lint("which(grepl('^a', x, perl = TRUE, fixed = TRUE))", lint_msg, linter)
+
+  expect_lint(
+    trim_some('{
+      which(x)
+      grepl(y)
+      which(grepl("pt1", x))
+      which(grepl("pt2", y))
+    }'),
+    list(
+      list(lint_msg, line_number = 4L, column_number = 3L),
+      list(lint_msg, line_number = 5L, column_number = 3L)
+    ),
+    linter
+  )
+})
+
+test_that("which_linter ignores valid which min/max usages", {
+  linter <- which_linter()
+
+  expect_lint("which.min(x)", NULL, linter)
+  expect_lint("which.max(x)", NULL, linter)
+  expect_lint("min(x)", NULL, linter)
+  expect_lint("max(x)", NULL, linter)
+  expect_lint("which(x > 0L)", NULL, linter)
+  expect_lint("min(which(x > 0L))", NULL, linter)
+  expect_lint("max(which(x > 0L))", NULL, linter)
+  expect_lint("# which(x == min(x))", NULL, linter)
+
+})
+
+test_that("which_linter identifies which(x == min(x)) pattern", {
+  linter <- which_linter()
+  lint_msg <- rex("which.min(x) is more efficient than which(x == min(x))")
+
+  expect_lint("which(x == min(x))", lint_msg, linter)
+  expect_lint("result <- which(values == min(values))", lint_msg, linter)
+})
+
+test_that("which_linter identifies which(x == max(x)) pattern", {
+  linter <- which_linter()
+  lint_msg <- rex("which.max(x) is more efficient than which(x == max(x))")
+
+  expect_lint("which(x == max(x))", lint_msg, linter)
+  expect_lint("result <- which(values == max(values))", lint_msg, linter)
+})
+
+test_that("lints vectorize", {
+  max_message <- rex::rex("which(x == max(x))")
+  min_message <- rex::rex("which(x == min(x))")
+
+  expect_lint(
+    trim_some("{
+      which.max(x)
+      which(x == max(x))
+      which.min(x)
+      which(x == min(x))
+    }"),
+    list(
+      list(max_message, line_number = 3L),
+      list(min_message, line_number = 5L)
+    ),
+    which_linter()
+  )
+})
+
