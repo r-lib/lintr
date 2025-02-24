0dd07f9f51219e1e87aa3497c372da1d881b4659
Michael Chirico <michaelchirico4@gmail.com>
2025-02-24 09:38:42 -0800

    another
    Co-authored-by: AshesITR <alexander.rosenstock@web.de>

--- R/null_coalescing_linter.R
+++ R/null_coalescing_linter.R
@@ -73,7 +73,6 @@ null_coalescing_linter <- function() {
 
   Linter(linter_level = "expression", function(source_expression) {
     null_calls <- source_expression$xml_find_function_calls("is.null")
-
     bad_expr <- xml_find_all(null_calls, xpath)
     is_negation <- !is.na(xml_find_first(bad_expr, "expr/OP-EXCLAMATION"))
     observed <- ifelse(is_negation, "if (!is.null(x)) x else y", "if (is.null(x)) y else x")
