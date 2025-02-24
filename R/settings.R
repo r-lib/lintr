a585a8242b70a5d2d7675e7a969acd379e5295c2
Michael Chirico <chiricom@google.com>
2025-02-24 18:17:36 +0000

    typo

--- R/settings.R
+++ R/settings.R
@@ -178,8 +178,8 @@ validate_config_file <- function(config, config_file, defaults) {
   validate_regex(config,
     c("exclude", "exclude_next", "exclude_start", "exclude_end", "exclude_linter", "exclude_linter_sep")
   )
-  validate_character_string(config, c("encoding", "cache_directory")
-  validate_true_false(config, c("comment_bot", "error_on_lint"))
+  validate_character_string(config, c("encoding", "cache_directory"))
+  validate_true_false(config, "error_on_lint")
   validate_linters(config$linters)
   validate_exclusions(config$exclusions)
 }
