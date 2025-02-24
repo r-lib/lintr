32e323cd97e87f3af6913b6dc13b60b982e8bd9c
Michael Chirico <chiricom@google.com>
2025-02-24 18:55:02 +0000

    extra ')'

--- tests/testthat/test-cyclocomp_linter.R
+++ tests/testthat/test-cyclocomp_linter.R
@@ -69,5 +69,5 @@ test_that("a null linter is returned, with warning, if cyclocomp is unavailable"
     linter <- cyclocomp_linter(1L)
   })
 
-  expect_error(lint(text = "if (TRUE) 1 else 2"), linters = linter), "disabled", fixed = TRUE)
+  expect_error(lint(text = "if (TRUE) 1 else 2", linters = linter), "disabled", fixed = TRUE)
 })
