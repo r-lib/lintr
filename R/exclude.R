b37fd7f2878cd8fca8b3236d9057ada0a50cd4cb
Michael Chirico <michaelchirico4@gmail.com>
2025-02-24 23:02:56 -0800

    better wording
    Co-authored-by: AshesITR <alexander.rosenstock@web.de>

--- R/exclude.R
+++ R/exclude.R
@@ -31,7 +31,7 @@
 #'   "",
 #'   "   Note also that:",
 #'   "   1. All paths are interpreted as globs ([Sys.glob()]), so that e.g. `*` does pattern expansion.",
-#'   "   2. For `exclusions` provided _via_ config, all paths are taken as relative _to the config file itself_,",
+#'   "   2. For `exclusions` provided _via_ a config file, all paths are taken as relative _to the config file itself_,",
 #'   "      not the path where `lint()` is run.",
 #'   NULL
 #' )
