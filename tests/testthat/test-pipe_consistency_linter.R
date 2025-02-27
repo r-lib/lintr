d272066760a512e7495e4666ff0751ef2da740fb
Michael Chirico <chiricom@google.com>
2025-02-27 17:56:17 +0000

    there are two lints now

--- tests/testthat/test-pipe_consistency_linter.R
+++ tests/testthat/test-pipe_consistency_linter.R
@@ -98,7 +98,11 @@ test_that("pipe_consistency_linter works with |> argument", {
     linter
   )
 
-  expect_lint("1:3 %>% mean() %T>% print()", lint_message, linter)
+  expect_lint(
+    "1:3 %>% mean() %T>% print()",
+    list("%>%, "%T>%"),
+    linter
+  )
 
   expect_lint(
     "1:3 |> mean() %T>% print()",
