360901aa4e5e104ec8083207561543f8af67db63
Michael Chirico <chiricom@google.com>
2025-03-03 21:17:31 +0000

    Just delete more tests for now

--- tests/testthat/test-settings.R
+++ tests/testthat/test-settings.R
@@ -1,65 +1,3 @@
-test_that("it uses default settings if none provided", {
-  lintr:::read_settings(NULL)
-
-  lapply(ls(settings), function(setting) {
-    expect_identical(settings[[setting]], default_settings[[setting]])
-  })
-})
-
-test_that("it uses option settings if provided", {
-  withr::local_options(list(lintr.exclude = "test"))
-
-  lintr:::read_settings(NULL)
-
-  expect_identical(settings$exclude, "test")
-})
-
-test_that("it uses config settings in same directory if provided", {
-  test_dir <- tempdir()
-  file <- withr::local_tempfile(tmpdir = test_dir)
-  local_config('exclude: "test"', test_dir)
-
-  lintr:::read_settings(file)
-
-  lapply(setdiff(ls(settings), "exclude"), function(setting) {
-    expect_identical(settings[[setting]], default_settings[[setting]])
-  })
-
-  expect_identical(settings$exclude, "test")
-})
-
-test_that("it uses config home directory settings if provided", {
-  path <- withr::local_tempdir()
-  home_path <- withr::local_tempdir()
-  file <- withr::local_tempfile(tmpdir = path)
-  local_config('exclude: "test"', home_path)
-
-  withr::with_envvar(c(HOME = home_path), lintr:::read_settings(file))
-
-  lapply(setdiff(ls(settings), "exclude"), function(setting) {
-    expect_identical(settings[[setting]], default_settings[[setting]])
-  })
-
-  expect_identical(settings$exclude, "test")
-})
-
-test_that("it uses system config directory settings if provided", {
-  path <- withr::local_tempdir()
-  config_parent_path <- withr::local_tempdir("config")
-  config_path <- file.path(config_parent_path, "R", "lintr")
-  dir.create(config_path, recursive = TRUE)
-  file <- withr::local_tempfile(tmpdir = path)
-  local_config('exclude: "test"', config_path, filename = "config")
-
-  withr::with_envvar(c(R_USER_CONFIG_DIR = config_parent_path), lintr:::read_settings(file))
-
-  lapply(setdiff(ls(settings), "exclude"), function(setting) {
-    expect_identical(settings[[setting]], default_settings[[setting]])
-  })
-
-  expect_identical(settings$exclude, "test")
-})
-
 test_that("read_config_file() warns if the config file does not end in a newline", {
   .lintr <- withr::local_tempfile()
   withr::local_options(lintr.linter_file = .lintr)
@@ -103,32 +41,6 @@ test_that("linters_with_defaults doesn't break on very long input", {
     ),
     "undesirable_function_linter"
   )
-})
-
-test_that("it has a smart default for encodings", {
-  lintr:::read_settings(NULL)
-  expect_identical(settings$encoding, "UTF-8")
-
-  proj_file <- test_path("dummy_projects", "project", "cp1252.R")
-  pkg_file <- test_path("dummy_packages", "cp1252", "R", "cp1252.R")
-
-  expect_identical(
-    normalize_path(find_rproj_at(find_package(proj_file, allow_rproj = TRUE))),
-    normalize_path(test_path("dummy_projects", "project", "project.Rproj"))
-  )
-  expect_identical(
-    normalize_path(find_package(pkg_file)),
-    normalize_path(test_path("dummy_packages", "cp1252"))
-  )
-
-  expect_identical(lintr:::find_default_encoding(proj_file), "ISO8859-1")
-  expect_identical(lintr:::find_default_encoding(pkg_file), "ISO8859-1")
-
-  lintr:::read_settings(proj_file)
-  expect_identical(settings$encoding, "ISO8859-1")
-
-  lintr:::read_settings(pkg_file)
-  expect_identical(settings$encoding, "ISO8859-1")
 })
 
 test_that("validate_config_file() detects improperly-formed settings", {
