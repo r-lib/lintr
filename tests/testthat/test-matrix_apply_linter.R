06b20473f0c5a6cf5616837de95e615c691684e9
Michael Chirico <michaelchirico4@gmail.com>
2025-03-08 14:40:41 -0800

    the easier fixes continue

--- tests/testthat/test-matrix_apply_linter.R
+++ tests/testthat/test-matrix_apply_linter.R
@@ -26,53 +26,49 @@ test_that("matrix_apply_linter is not implemented for complex MARGIN values", {
   expect_lint("apply(x, m, sum)", NULL, linter)
 
   expect_lint("apply(x, 1 + 2:4, sum)", NULL, linter)
-
 })
 
 
 test_that("matrix_apply_linter simple disallowed usages", {
   linter <- matrix_apply_linter()
+
   lint_message <- rex::rex("rowSums(x)")
-
   expect_lint("apply(x, 1, sum)", lint_message, linter)
-
   expect_lint("apply(x, MARGIN = 1, FUN = sum)", lint_message, linter)
-
   expect_lint("apply(x, 1L, sum)", lint_message, linter)
-
   expect_lint("apply(x, 1:4, sum)", rex::rex("rowSums(x, dims = 4)"), linter)
-
   expect_lint("apply(x, 2, sum)", rex::rex("rowSums(colSums(x))"), linter)
-
   expect_lint("apply(x, 2:4, sum)", rex::rex("rowSums(colSums(x), dims = 3)"), linter)
 
   lint_message <- rex::rex("rowMeans")
-
   expect_lint("apply(x, 1, mean)", lint_message, linter)
-
   expect_lint("apply(x, MARGIN = 1, FUN = mean)", lint_message, linter)
 
   # Works with extra args in mean()
   expect_lint("apply(x, 1, mean, na.rm = TRUE)", lint_message, linter)
 
   lint_message <- rex::rex("colMeans")
-
   expect_lint("apply(x, 2, mean)", lint_message, linter)
-
   expect_lint("apply(x, 2:4, mean)", lint_message, linter)
 
+  # adversarial comments
+  expect_lint(
+    trim_some("
+      apply(x, 2, #comment
+      mean)
+    "),
+    lint_message,
+    linter
+  )
 })
 
 test_that("matrix_apply_linter recommendation includes na.rm if present in original call", {
   linter <- matrix_apply_linter()
+
   lint_message <- rex::rex("na.rm = TRUE")
-
   expect_lint("apply(x, 1, sum, na.rm = TRUE)", lint_message, linter)
-
   expect_lint("apply(x, 2, sum, na.rm = TRUE)", lint_message, linter)
-
   expect_lint("apply(x, 1, mean, na.rm = TRUE)", lint_message, linter)
-
   expect_lint("apply(x, 2, mean, na.rm = TRUE)", lint_message, linter)
 
   lint_message <- rex::rex("rowSums(x)")
@@ -80,7 +76,6 @@ test_that("matrix_apply_linter recommendation includes na.rm if present in origi
 
   lint_message <- rex::rex("na.rm = foo")
   expect_lint("apply(x, 1, sum, na.rm = foo)", lint_message, linter)
-
 })
 
 test_that("matrix_apply_linter works with multiple lints in a single expression", {
