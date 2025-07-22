a5e900bb49e03dc8ae5dd316d3474fbe7e2b07ca
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-07-22 10:11:38 +0200

    Add lint message and vectorize test

--- tests/testthat/test-all_equal_linter.R
+++ tests/testthat/test-all_equal_linter.R
@@ -9,8 +9,26 @@ test_that("all_equal_linter() skips allowed usages", {
 
 test_that("all_equal_linter() blocks simple disallowed usages", {
   linter <- all_equal_linter()
-  lint_message <- rex::rex(anything)
+  lint_message <- rex::rex("Wrap all.equal() in isTRUE()")
 
   expect_lint("if (all.equal(a, b)) message('equal')", lint_message, linter)
   expect_lint("!all.equal(a, b)", lint_message, linter)
+})
+
+test_that("lints vectorize", {
+  lint_message <- rex::rex("Wrap all.equal() in isTRUE()")
+
+  expect_lint(
+    trim_some("{
+      all.equal(a, b)
+      !all.equal(a, b)
+      !isTRUE(all.equal(a, b))
+      if (all.equal(a, b)) message('equal')
+    }"),
+    list(
+      list(lint_message, line_number = 3L),
+      list(lint_message, line_number = 5L)
+    ),
+    all_equal_linter()
+  )
 })
