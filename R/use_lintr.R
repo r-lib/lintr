6ae1b470826ebe5756b3db5154ee3fa7cbcfc0bc
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-08-23 11:44:59 +0200

    Add message with pkg root location
    Mostly for debugging

--- R/use_lintr.R
+++ R/use_lintr.R
@@ -48,6 +48,7 @@ use_lintr <- function(path = ".", type = c("tidyverse", "full")) {
   if (is.null(pkg_path)) {
     return(invisible(config_file))
   }
+  cli_inform("Detected package root at {.path {pkg_path}}.")
 
   rbuildignore_path <- file.path(pkg_path, ".Rbuildignore")
   rel_path <- gsub(paste0(pkg_path, "/"), "", config_file, fixed = TRUE)
