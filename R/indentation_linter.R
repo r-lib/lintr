e222edbbce1ed00fbcf97959bc1c330d3ab65d17
Michael Chirico <chiricom@google.com>
2026-07-14 16:15:47 +0000

    finally something I'm happier with

--- R/indentation_linter.R
+++ R/indentation_linter.R
@@ -337,25 +337,19 @@ find_new_indent <- function(current_indent, change_type, indent, hanging_indent)
 
 find_bad_lines <- function(line_metadata) {
   is_bad <-
-    with(line_metadata, indent_level != expected_level & nzchar(trimws(line, "left"), keepNA = TRUE) & !in_str_const)
-  if (!any(is_bad, na.rm = TRUE)) {
-    is_bad[is.na(is_bad)] <- FALSE
+    with(line_metadata, !is.na(line) & indent_level != expected_level & nzchar(trimws(line, "left")) & !in_str_const)
+  if (!any(is_bad)) {
     return(is_bad)
   }
 
   # Suppress consecutive lints with the same indentation difference, to not generate an excessive number of lints
+  indent_diff <- line_metadata$expected_level - line_metadata$indent_level
+  indent_diff[is.na(indent_diff)] <- 0L
 
-  code_lines <- !is.na(is_bad)
-  indent_diff <- line_metadata$expected_level[code_lines] - line_metadata$indent_level[code_lines]
+  is_consecutive <- c(FALSE, diff(is_bad) == 0L)
+  is_same_diff <- c(FALSE, diff(indent_diff) == 0L)
 
-  is_adjacent <- c(FALSE, diff(line_metadata$number[code_lines]) == 1L)
-
-  is_consecutive <- c(FALSE, diff(is_bad[code_lines]) == 0L) & is_adjacent
-  is_same_diff <- c(FALSE, diff(indent_diff) == 0L) & is_adjacent
-
-  is_bad[code_lines] <- is_bad[code_lines] & !(is_consecutive & is_same_diff)
-  is_bad[is.na(is_bad)] <- FALSE
-  is_bad
+  is_bad & !(is_consecutive & is_same_diff)
 }
 
 incorporate_closing_lints <- function(bad_closing_block,
