76f7dc3cfa8e0daa124e567b9047a09ca8ab398a
Michael Chirico <chiricom@google.com>
2026-08-11 15:49:38 -0600

    seq_linter() finds seq(1, 10) (#3092)
    * Gemini: lint seq(1, n) and seq(from = 1, to = n) in seq_linter()
    
    * Clean up some lints
    
    * Gemini: Refactor seq_linter to eliminate nested if blocks and ifelse warnings
    
    * Simplify linter description (growing unwieldy)
    
    * fix examples structure/parallelism
    
    * shrink diff: use old name
    
    * restore helpful comment
    
    * hide 0-length edge case handling in helper
    
    * whitespace stlye in test
    
    * Gemini: Fully vectorize get_seq_metadata() and store properties in a metadata data.frame
    
    * stringsAsFactors=FALSE not needed for us
    
    * simplify helper locals
    
    * initial pass: dump everything in data.frame
    
    * Pull some vectors out for readability/simplicity
    
    * rename -> seq_metadata
    
    * Minimize data.frame usage
    
    * trimws() ostensibly unneeded
    
    * correct GitHub username
    
    Co-authored-by: Hugo Gruson <git@hugogruson.fr>
    
    * Review feedback: catch seq(10, from=1)
    
    * Gemini: adjust message to get rev() correct; add tests
    
    * fix-up XPath style
    
    * Review feedback: right message for seq(1,1)
    
    * Review feedback: dplyr::n() gives right message
    
    * Review feedback: dont cast to string only to cast back to integer
    
    * Review feedback: proper handling of .N XPath
    
    * Review feedback: unnecessary `,` in regex
    
    * Review feedback: more test coverage, including a new false negative
    
    * Gemini: handle seq(to=1, from=10)
    
    * Gemini: handle seq(0,1) correctly too
    
    * touch-up
    
    * Gemini: use patrick
    
    * Gemini: more readable test suite
    
    ---------
    
    Co-authored-by: Hugo Gruson <git@hugogruson.fr>

--- tests/testthat/test-seq_linter.R
+++ tests/testthat/test-seq_linter.R
@@ -15,107 +15,53 @@ test_that("seq_len(...) or seq_along(...) expressions are fine", {
   expect_no_lint("seq(length(x), 2)", linter)
 })
 
-test_that("finds seq(...) expressions", {
-  linter <- seq_linter()
-  lint_msg <- function(want, got) rex::rex("Use ", want, " instead of ", got)
-
+skip_if_not_installed("tibble")
+patrick::with_parameters_test_that(
+  "finds seq(...) expressions",
   expect_lint(
-    "seq(length(x))",
-    lint_msg("seq_along(...)", "seq(length(...))"),
-    linter
+    target_code,
+    rex::rex("Use ", good_usage, " instead of ", bad_usage),
+    seq_linter()
+  ),
+  .cases = tibble::tribble(
+    ~target_code,          ~good_usage,          ~bad_usage,
+    "seq(length(x))",      "seq_along(...)",     "seq(length(...))",
+    "seq(nrow(x))",        "seq_len(nrow(...))", "seq(nrow(...))",
+    "rev(seq(length(x)))", "seq_along(...)",     "seq(length(...))",
+    "rev(seq(nrow(x)))",   "seq_len(nrow(...))", "seq(nrow(...))"
   )
+)
 
+patrick::with_parameters_test_that(
+  "finds 1:x expressions",
   expect_lint(
-    "seq(nrow(x))",
-    lint_msg("seq_len(nrow(...))", "seq(nrow(...))"),
-    linter
+    target_code,
+    rex::rex("Use ", good_usage, " instead of ", bad_usage),
+    seq_linter()
+  ),
+  .cases = tibble::tribble(
+    ~target_code,                    ~good_usage,              ~bad_usage,
+    "1:length(x)",                   "seq_along(...)",         "1:length(...)",
+    "1L:length(x)",                  "seq_along(...)",         "1L:length(...)",
+    "1:nrow(x)",                     "seq_len(nrow(...))",     "1:nrow(...)",
+    "1:ncol(x)",                     "seq_len(ncol(...))",     "1:ncol(...)",
+    "1:NROW(x)",                     "seq_len(NROW(...))",     "1:NROW(...)",
+    "1:NCOL(x)",                     "seq_len(NCOL(...))",     "1:NCOL(...)",
+    "1:dim(x)[1L]",                  "seq_len(dim(...)[1L])",  "1:dim(...)[1L]",
+    "1L:dim(x)[[1]]",                "seq_len(dim(...)[[1]])", "1L:dim(...)[[1]]",
+    "mutate(x, .id = 1:n())",        "seq_len(n())",           "1:n()",
+    "mutate(x, .id = 1:dplyr::n())", "seq_len(dplyr::n())",    "1:dplyr::n()",
+    "x[, .id := 1:.N]",              "seq_len(.N)",            "1:.N"
   )
+)
 
-  expect_lint(
-    "rev(seq(length(x)))",
-    lint_msg("seq_along(...)", "seq(length(...))"),
-    linter
-  )
-
-  expect_lint(
-    "rev(seq(nrow(x)))",
-    lint_msg("seq_len(nrow(...))", "seq(nrow(...))"),
-    linter
-  )
-})
-
-test_that("finds 1:length(...) expressions", {
-  linter <- seq_linter()
-  lint_msg <- function(want, got) rex::rex("Use ", want, " instead of ", got)
-
-  expect_lint(
-    "1:length(x)",
-    lint_msg("seq_along(...)", "1:length(...)"),
-    linter
-  )
-
-  expect_lint(
-    "1:nrow(x)",
-    lint_msg("seq_len(nrow(...))", "1:nrow(...)"),
-    linter
-  )
-
-  expect_lint(
-    "1:ncol(x)",
-    lint_msg("seq_len(ncol(...))", "1:ncol(...)"),
-    linter
-  )
-
-  expect_lint(
-    "1:NROW(x)",
-    lint_msg("seq_len(NROW(...))", "1:NROW(...)"),
-    linter
-  )
-
-  expect_lint(
-    "1:NCOL(x)",
-    lint_msg("seq_len(NCOL(...))", "1:NCOL(...)"),
-    linter
-  )
-
-  expect_lint(
-    "1:dim(x)[1L]",
-    lint_msg("seq_len(dim(...)[1L])", "1:dim(...)[1L]"),
-    linter
-  )
-
-  expect_lint(
-    "1L:dim(x)[[1]]",
-    rex::rex("Use seq_len", anything, "dim(...)"),
-    linter
-  )
-
-  expect_lint(
-    "mutate(x, .id = 1:n())",
-    lint_msg("seq_len(n())", "1:n(),"),
-    linter
-  )
-
+test_that("adversarial comment is handled in 1:n()", {
   expect_lint(
     trim_some("
       mutate(x, .id = 1:n( # comment
       ))
     "),
-    lint_msg("seq_len(n())", "1:n(),"),
-    linter
-  )
-
-  expect_lint(
-    "x[, .id := 1:.N]",
-    lint_msg("seq_len(.N)", "1:.N,"),
-    linter
-  )
-})
-
-test_that("1L is also bad", {
-  expect_lint(
-    "1L:length(x)",
-    rex::rex("seq_along", anything, "1L:length(...)"),
+    rex::rex("Use seq_len(n()) instead of 1:n()"),
     seq_linter()
   )
 })
@@ -124,30 +70,23 @@ test_that("reverse seq is ok", {
   linter <- seq_linter()
   expect_no_lint("rev(seq_along(x))", linter)
   expect_no_lint("rev(seq_len(nrow(x)))", linter)
+})
 
+patrick::with_parameters_test_that(
+  "finds potential sequence() replacements",
   expect_lint(
-    "length(x):1",
-    rex::rex("rev(seq_along(...))", anything, "length(...):1"),
-    linter
+    sprintf("unlist(%s(x, %s))", map_fn, seq_fn),
+    rex::rex("Use sequence()"),
+    seq_linter()
+  ),
+  .cases = expand.grid(
+    map_fn = c("lapply", "sapply", "map", "purrr::map"),
+    seq_fn = c("seq_len", "seq")
   )
-})
-
-test_that("finds potential sequence() replacements", {
-  linter <- seq_linter()
-  lint_msg <- rex::rex("Use sequence()")
-
-  expect_lint("unlist(lapply(x, seq_len))", lint_msg, linter)
-
-  expect_lint("unlist(lapply(x, seq))", lint_msg, linter)
-
-  # Even for prefixed purrr:: calls
-  expect_lint("unlist(purrr::map(x, seq_len))", lint_msg, linter)
-})
+)
 
 test_that("sequence() is not recommended for complex seq() calls", {
-  linter <- seq_linter()
-
-  expect_no_lint("unlist(lapply(x, seq, from = 2))", linter)
+  expect_no_lint("unlist(lapply(x, seq, from = 2))", seq_linter())
 })
 
 test_that("Message vectorization works for multiple lints", {
@@ -157,10 +96,14 @@ test_that("Message vectorization works for multiple lints", {
     trim_some("{
       1:length(x)
       1:nrow(y)
+      seq(1, 10)
+      seq(to = 1, from = 10)
     }"),
     list(
       list(rex::rex("seq_along(...)", anything, "1:length(...)"), line_number = 2L),
-      list(rex::rex("seq_len(nrow(...))", anything, "1:nrow(...)"), line_number = 3L)
+      list(rex::rex("seq_len(nrow(...))", anything, "1:nrow(...)"), line_number = 3L),
+      list(rex::rex("seq_len(10)", anything, "seq(1, 10)"), line_number = 4L),
+      list(rex::rex("rev(seq_len(10))", anything, "seq(10, 1)"), line_number = 5L)
     ),
     linter
   )
@@ -214,14 +157,22 @@ test_that("Message vectorization works for multiple lints", {
   )
 })
 
-test_that("Message recommends rev() correctly", {
-  linter <- seq_linter()
-
-  expect_lint(".N:1", rex::rex("Use rev(seq_len(.N))"), linter)
-  expect_lint("n():1", rex::rex("Use rev(seq_len(n()))"), linter)
-  expect_lint("nrow(x):1", rex::rex("Use rev(seq_len(nrow(...)))"), linter)
-  expect_lint("length(x):1", rex::rex("Use rev(seq_along(...))"), linter)
-})
+patrick::with_parameters_test_that(
+  "Message recommends rev() correctly",
+  expect_lint(
+    target_code,
+    rex::rex("Use ", good_usage, " instead of ", bad_usage),
+    seq_linter()
+  ),
+  .cases = tibble::tribble(
+    ~target_code,   ~good_usage,                ~bad_usage,
+    ".N:1",         "rev(seq_len(.N))",         ".N:1",
+    "n():1",        "rev(seq_len(n()))",        "n():1",
+    "dplyr::n():1", "rev(seq_len(dplyr::n()))", "dplyr::n():1",
+    "nrow(x):1",    "rev(seq_len(nrow(...)))",  "nrow(...):1",
+    "length(x):1",  "rev(seq_along(...))",      "length(...):1"
+  )
+)
 
 test_that("seq_len(length(x)) should be seq_along(x)", {
   linter <- seq_linter()
@@ -232,3 +183,103 @@ test_that("seq_len(length(x)) should be seq_along(x)", {
   expect_lint("seq_len(length(x))", "seq_along", linter)
   expect_lint("seq_len(length(foo(x)))", "seq_along", linter)
 })
+
+patrick::with_parameters_test_that(
+  "finds seq(1, n) and seq(from = 1, to = n) expressions",
+  expect_lint(
+    target_code,
+    rex::rex("Use ", good_usage, " instead of ", bad_usage),
+    seq_linter()
+  ),
+  .cases = tibble::tribble(
+    ~target_code,                    ~good_usage,               ~bad_usage,
+    "seq(1, 10)",                     "seq_len(10)",             "seq(1, 10)",
+    "seq(1, 1)",                      "seq_len(1)",              "seq(1, 1)",
+    "base::seq(1, 10)",               "seq_len(10)",             "seq(1, 10)",
+    "seq(1L, 10L)",                   "seq_len(10L)",            "seq(1L, 10L)",
+    "seq(1, n)",                      "seq_len(n)",              "seq(1, n)",
+    "seq(1, length(x))",              "seq_along(...)",          "seq(1, length(...))",
+    "seq(1, nrow(x))",                "seq_len(nrow(...))",      "seq(1, nrow(...))",
+    "seq(from = 1, to = 10)",         "seq_len(10)",             "seq(1, 10)",
+    "seq(from = 1L, to = length(x))", "seq_along(...)",          "seq(1L, length(...))",
+    "seq(to = 10, from = 1)",         "seq_len(10)",             "seq(1, 10)",
+    "seq(to = length(x), from = 1L)", "seq_along(...)",          "seq(1L, length(...))",
+    "seq(1, to = 10)",                "seq_len(10)",             "seq(1, 10)",
+    "seq(to = 10, 1)",                "seq_len(10)",             "seq(1, 10)",
+    "seq(to = 10, 1L)",               "seq_len(10)",             "seq(1L, 10)",
+    "seq(10, from = 1)",              "seq_len(10)",             "seq(1, 10)",
+    "seq(10, from = 1L)",             "seq_len(10)",             "seq(1L, 10)",
+    "seq(from = 1, 10)",              "seq_len(10)",             "seq(1, 10)",
+    "seq(from = 1L, 10)",             "seq_len(10)",             "seq(1L, 10)",
+    "seq(length(x), from = 1)",       "seq_along(...)",          "seq(1, length(...))",
+    "seq(length(x), from = 1L)",      "seq_along(...)",          "seq(1L, length(...))",
+    "seq(from = 1, length(x))",       "seq_along(...)",          "seq(1, length(...))",
+    "seq(to = length(x), 1)",         "seq_along(...)",          "seq(1, length(...))",
+    # Decreasing seq() calls
+    "seq(10, 1)",                     "rev(seq_len(10))",        "seq(10, 1)",
+    "seq(10L, 1L)",                   "rev(seq_len(10L))",       "seq(10L, 1L)",
+    "seq(n, 1)",                      "rev(seq_len(n))",         "seq(n, 1)",
+    "seq(length(x), 1)",              "rev(seq_along(...))",     "seq(length(...), 1)",
+    "seq(nrow(x), 1)",                "rev(seq_len(nrow(...)))", "seq(nrow(...), 1)",
+    "seq(from = 10, to = 1)",         "rev(seq_len(10))",        "seq(10, 1)",
+    "seq(to = 1, from = 10)",         "rev(seq_len(10))",        "seq(10, 1)",
+    "seq(to = 1L, from = 10)",        "rev(seq_len(10))",        "seq(10, 1L)",
+    "seq(from = length(x), to = 1)",  "rev(seq_along(...))",     "seq(length(...), 1)",
+    "seq(from = length(x), to = 1L)", "rev(seq_along(...))",     "seq(length(...), 1L)",
+    "seq(to = 1, from = length(x))",  "rev(seq_along(...))",     "seq(length(...), 1)",
+    "seq(to = 1L, from = length(x))", "rev(seq_along(...))",     "seq(length(...), 1L)",
+    "seq(10, to = 1)",                "rev(seq_len(10))",        "seq(10, 1)",
+    "seq(to = 1, 10)",                "rev(seq_len(10))",        "seq(10, 1)",
+    "seq(from = 10, 1)",              "rev(seq_len(10))",        "seq(10, 1)",
+    "seq(from = 10, 1L)",             "rev(seq_len(10))",        "seq(10, 1L)",
+    "seq(1, from = 10)",              "rev(seq_len(10))",        "seq(10, 1)",
+    "seq(1L, from = 10)",             "rev(seq_len(10))",        "seq(10, 1L)"
+  )
+)
+
+patrick::with_parameters_test_that(
+  "complex seq() expressions or other arguments are fine",
+  expect_no_lint(target_code, seq_linter()),
+  target_code = c(
+    "seq(0, 1)",
+    "seq(0L, 1L)",
+    "seq(0, 1L)",
+    "seq(0L, 1)",
+    "seq(from = 0, to = 1)",
+    "seq(to = 1, from = 0)",
+    "seq(to = 1, 0)",
+    "seq(from = 0, 1)",
+    "seq(-1, 1)",
+    "seq(from = -1, 1)",
+    "seq(to = 1, from = -1)",
+    "seq(from = -1, to = 1)",
+    "seq(0, 10)",
+    "seq(2, 10)",
+    "seq(10, from = 2)",
+    "seq(from = 2, 10)",
+    "seq(10, to = 2)",
+    "seq(to = 2, from = 10)",
+    "seq(from = 10, to = 2)",
+    "seq(to = 2, 10)",
+    "seq(from = 10, 2)",
+    "seq(10, 2)",
+    "seq(1, 10, by = 2)",
+    "seq(10, from = 1, by = 2)",
+    "seq(from = 1, 10, by = 2)",
+    "seq(to = 10, 1, by = 2)",
+    "seq(to = 1, from = 10, by = -2)",
+    "seq(1, 10, length.out = 5)",
+    "seq(1, 10, along.with = x)",
+    "seq(1, 10, 2)",
+    "seq(from = 2, to = 10)",
+    "seq(from = 1, to = 10, by = 2)",
+    "seq(-1, 10)",
+    "seq(from = -1, 10)",
+    "seq(10, from = -1)",
+    "seq(from = -1, to = 10)",
+    "seq(10, -1)",
+    "seq(from = 10, -1)",
+    "seq(-1, from = 10)",
+    "seq(from = 10, to = -1)"
+  )
+)
